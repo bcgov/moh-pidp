281704dba8e00d445e5a74ebca78079a4068e258
Martin Pultz <mtpultz@gmail.com>
2021-11-29 16:33:29 -0800

    Updated requested config filename, Added unsupported page

--- workspace/apps/pidp/src/main.ts
+++ workspace/apps/pidp/src/main.ts
@@ -7,11 +7,11 @@ import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 import { APP_DI_CONFIG, AppConfig, APP_CONFIG } from './app/app-config.module';
 
-// The deployment pipeline provides the config map based on environment
-// without requiring a build by placing config-map.json within the public
-// assets folder, otherwise it will not exist in local development which
-// relies on the cascade of environment files.
-fetch('/assets/config-map.json')
+// The deployment pipeline provides the production environment configuration
+// without requiring a rebuild by placing `environment.json` within the public
+// assets folder, otherwise local development in/outside of a container relies
+// on the local environment files.
+fetch('/assets/environment.json')
   .then((response) => response.json())
   .then((configMap: ConfigMap) => {
     let appConfig = APP_DI_CONFIG;
@@ -30,7 +30,12 @@ fetch('/assets/config-map.json')
       enableProdMode();
     }
 
-    platformBrowserDynamic([{ provide: APP_CONFIG, useValue: appConfig }])
+    platformBrowserDynamic([
+      {
+        provide: APP_CONFIG,
+        useValue: appConfig,
+      },
+    ])
       .bootstrapModule(AppModule)
       .catch((err) => console.error(err));
   });
