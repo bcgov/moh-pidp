75b811ca1845207d096015ba11f05ed824236d19
Martin Pultz <mtpultz@gmail.com>
2021-11-30 20:54:19 -0800

    Updated eslint rules, and ran formatting over the entire project

--- workspace/apps/pidp/src/app/libs/data-access/interceptors/maintenance.interceptor.spec.ts
+++ workspace/apps/pidp/src/app/libs/data-access/interceptors/maintenance.interceptor.spec.ts
@@ -3,14 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { MaintenanceInterceptor } from './maintenance.interceptor';
 
 describe('MaintenanceInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      MaintenanceInterceptor
-      ]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [MaintenanceInterceptor],
+    })
+  );
 
   it('should be created', () => {
-    const interceptor: MaintenanceInterceptor = TestBed.inject(MaintenanceInterceptor);
+    const interceptor: MaintenanceInterceptor = TestBed.inject(
+      MaintenanceInterceptor
+    );
     expect(interceptor).toBeTruthy();
   });
 });
--- workspace/apps/pidp/src/app/libs/data-access/interceptors/unauthorized.interceptor.spec.ts
+++ workspace/apps/pidp/src/app/libs/data-access/interceptors/unauthorized.interceptor.spec.ts
@@ -3,14 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { UnauthorizedInterceptor } from './unauthorized.interceptor';
 
 describe('UnauthorizedInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      UnauthorizedInterceptor
-      ]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [UnauthorizedInterceptor],
+    })
+  );
 
   it('should be created', () => {
-    const interceptor: UnauthorizedInterceptor = TestBed.inject(UnauthorizedInterceptor);
+    const interceptor: UnauthorizedInterceptor = TestBed.inject(
+      UnauthorizedInterceptor
+    );
     expect(interceptor).toBeTruthy();
   });
 });
