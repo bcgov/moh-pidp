c66ea9775d21c0e64752da33e68993d9f5540177
Martin Pultz <mtpultz@gmail.com>
2021-12-10 12:30:42 -0800

    Added rough in of form elements for other pages

--- workspace/apps/pidp/src/app/features/work-and-role-information/work-and-role-information.component.ts
+++ workspace/apps/pidp/src/app/features/work-and-role-information/work-and-role-information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,10 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WorkAndRoleInformationComponent implements OnInit {
   public title: string;
+  public form!: FormGroup;
 
-  public constructor(private route: ActivatedRoute) {
+  public constructor(private fb: FormBuilder, private route: ActivatedRoute) {
     this.title = this.route.snapshot.data.title;
   }
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.form = this.fb.group({
+      physicalAddress: this.fb.group({
+        countryCode: [{ value: null, disabled: false }, []],
+        provinceCode: [{ value: null, disabled: false }, []],
+        street: [{ value: null, disabled: false }, []],
+        city: [{ value: null, disabled: false }, []],
+        postal: [{ value: null, disabled: false }, []],
+      }),
+    });
+  }
 }
