d11a0e6b8212cea5f4f80eae4d83a11cbeb7bcab
Martin Pultz <mtpultz@gmail.com>
2021-12-14 14:40:37 -0800

    Updated page width classes

--- workspace/libs/shared/ui/src/lib/components/page/page.component.ts
+++ workspace/libs/shared/ui/src/lib/components/page/page.component.ts
@@ -11,8 +11,13 @@ export class PageComponent {
    * @description
    * Contraints applied to the maximum width of the
    * view content container to improve reability.
+   *
+   * "Page" provides readability for a single column of text within
+   * a view, and enough room for 2 column forms. While the other sizes
+   * provide a bit of flexibility in width when needed for views that
+   * are not typical pages with forms found within a specific workflow.
    */
-  @Input() public mode: 'page' | 'moderate' | 'full';
+  @Input() public mode: 'page' | 'medium' | 'large' | 'full';
 
   public constructor() {
     this.mode = 'page';
