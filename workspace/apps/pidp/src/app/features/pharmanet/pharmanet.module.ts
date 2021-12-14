382bb33b4b2cca2ed771b3c7656237d774ac8cd3
Martin Pultz <mtpultz@gmail.com>
2021-12-14 00:31:44 -0800

    Updated pharmanet next steps, Added info graphic component and assets

--- workspace/apps/pidp/src/app/features/pharmanet/pharmanet.module.ts
+++ workspace/apps/pidp/src/app/features/pharmanet/pharmanet.module.ts
@@ -6,12 +6,14 @@ import { NextStepsComponent } from './pages/next-steps/next-steps.component';
 import { SelfDeclarationComponent } from './pages/self-declaration/self-declaration.component';
 import { TermsOfAccessComponent } from './pages/terms-of-access/terms-of-access.component';
 import { PharmanetRoutingModule } from './pharmanet-routing.module';
+import { InfoGraphicComponent } from './shared/components/info-graphic/info-graphic.component';
 
 @NgModule({
   declarations: [
     SelfDeclarationComponent,
     TermsOfAccessComponent,
     NextStepsComponent,
+    InfoGraphicComponent,
   ],
   imports: [PharmanetRoutingModule, SharedModule],
 })
