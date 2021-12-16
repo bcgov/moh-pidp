75fb9aa00e69052483f256f3575bc9a4b2197c28
Martin Pultz <mtpultz@gmail.com>
2021-12-16 11:47:52 -0800

    Added demo service

--- workspace/apps/pidp/src/app/features/training/pages/compliance-training/compliance-training.component.ts
+++ workspace/apps/pidp/src/app/features/training/pages/compliance-training/compliance-training.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { DemoService } from '@core/services/demo.service';
+
 @Component({
   selector: 'app-compliance-training',
   templateUrl: './compliance-training.component.html',
@@ -9,8 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 export class ComplianceTrainingComponent implements OnInit {
   public title: string;
 
-  public constructor(private route: ActivatedRoute) {
+  public constructor(
+    private route: ActivatedRoute,
+    private demoService: DemoService
+  ) {
     this.title = this.route.snapshot.data.title;
+  }
+
+  public onSubmit(): void {
+    this.demoService.state.trainingSections =
+      this.demoService.state.trainingSections.map((section) => {
+        if (section.type === 'compliance-training') {
+          return {
+            ...section,
+            statusType: 'success',
+            status: 'completed',
+          };
+        }
+        return section;
+      });
   }
 
   public ngOnInit(): void {}
