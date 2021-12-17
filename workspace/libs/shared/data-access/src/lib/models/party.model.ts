90a486fb4383a2a7a61a7f793bda654f0d13b069
Martin Pultz <mtpultz@gmail.com>
2021-12-17 11:36:13 -0800

    Comment out address from party for now

--- workspace/libs/shared/data-access/src/lib/models/party.model.ts
+++ workspace/libs/shared/data-access/src/lib/models/party.model.ts
@@ -2,5 +2,5 @@ import { Address } from './address.model';
 import { User } from './user.model';
 
 export interface Party extends User {
-  physicalAddress: Address;
+  // mailingAddress: Address;
 }
