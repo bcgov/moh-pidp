2daec1215792cfd2b292821d916120322d329fdc
Martin Pultz <mtpultz@gmail.com>
2021-12-20 22:20:55 -0800

    Added college lookup

---
+++ backend/webapi/Data/Lookups/CollegeConfiguration.cs
@@ -0,0 +1,7 @@
+namespace Pidp.Data.Lookups;
+
+using Pidp.Models.Lookups;
+
+public class CollegeConfiguration : LookupTableConfiguration<College, CollegeDataGenerator>
+{
+}
---
+++ backend/webapi/Migrations/20211221062151_AddedCollegeLookup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pidp.Migrations
+{
+    public partial class AddedCollegeLookup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CollegeLookup",
+                columns: table => new
+                {
+                    Code = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollegeLookup", x => x.Code);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CollegeLookup",
+                columns: new[] { "Code", "Name" },
+                values: new object[,]
+                {
+                    { 1, "College of Physicians and Surgeons of BC (CPSBC)" },
+                    { 2, "College of Pharmacists of BC (CPBC)" },
+                    { 3, "BC College of Nurses and Midwives (BCCNM)" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollegeLookup");
+        }
+    }
+}
