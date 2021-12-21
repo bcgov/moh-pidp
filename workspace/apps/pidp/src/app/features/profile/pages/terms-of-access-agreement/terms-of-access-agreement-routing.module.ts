9c7ceeb69f89003c48a0193b2c1e2d6612a3a1af
Martin Pultz <mtpultz@gmail.com>
2021-12-20 17:53:21 -0800

    Added root route to routing config for user access agreement

--- workspace/apps/pidp/src/app/features/profile/pages/terms-of-access-agreement/terms-of-access-agreement-routing.module.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/terms-of-access-agreement/terms-of-access-agreement-routing.module.ts
@@ -9,6 +9,9 @@ const routes: Routes = [
     component: TermsOfAccessAgreementComponent,
     data: {
       title: 'Provider Identity Portal',
+      routes: {
+        root: '../../',
+      },
     },
   },
 ];
