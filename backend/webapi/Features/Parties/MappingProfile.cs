cd31d290769b1e1ed1244fcacba7a2018cc00a31
Martin Pultz <mtpultz@gmail.com>
2022-01-06 12:24:24 -0800

    Updated work setting mapping

--- backend/webapi/Features/Parties/MappingProfile.cs
+++ backend/webapi/Features/Parties/MappingProfile.cs
@@ -11,7 +11,17 @@ public class MappingProfile : Profile
         this.CreateMap<Party, CollegeCertification.Command>();
         this.CreateMap<PartyCertification, CollegeCertification.Command>();
         this.CreateMap<Party, Demographics.Command>();
-        this.CreateMap<Party, WorkSetting.Command>();
+        this.CreateMap<Party, WorkSetting.Command>()
+            .ForMember(dest => dest.FacilityName, opt =>
+            {
+                opt.PreCondition(src => src.Facility != null);
+                opt.MapFrom(src => src.Facility.FacilityName);
+            })
+            .ForMember(dest => dest.PhysicalAddress, opt =>
+            {
+                opt.PreCondition(src => src.Facility != null);
+                opt.MapFrom(src => src.Facility.PhysicalAddress);
+            });
         this.CreateMap<Facility, WorkSetting.Command>();
         this.CreateMap<FacilityAddress, WorkSetting.Command.Address>();
     }
