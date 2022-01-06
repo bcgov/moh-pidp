88b4ae4e081307366799c60f60f7a14872e56739
James <jameshollinger.ee@gmail.com>
2022-01-06 00:12:17 -0800

    first pass of auth

--- backend/webapi/Extensions/ObjectExtensions.cs
+++ backend/webapi/Extensions/ObjectExtensions.cs
@@ -6,7 +6,7 @@ public static class ObjectExtensions
     /// Throws an ArgumentNullException if the given data item is null.
     /// </summary>
     /// <param name="data">The item to check for nullity.</param>
-    /// <param name="name">The name to use when throwing an exception, if necessary</param>
+    /// <param name="name">The name to use when throwing an exception</param>
     public static void ThrowIfNull<T>(this T data, string name) where T : class
     {
         if (data == null)
