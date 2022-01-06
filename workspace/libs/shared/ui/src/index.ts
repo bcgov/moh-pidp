4ccb67291b7cddd0683e991bba9eb7aa1e27ebe2
Martin Pultz <mtpultz@gmail.com>
2022-01-06 13:14:57 -0800

    Added bc gov svg component

--- workspace/libs/shared/ui/src/index.ts
+++ workspace/libs/shared/ui/src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/classes';
 export * from './lib/material';
 export * from './lib/modules';
 export * from './lib/pipes';
+export * from './lib/components';
 export * from './lib/shared-ui.module';
 export * from './lib/validators';
 
---
+++ workspace/libs/shared/ui/src/lib/components/index.ts
@@ -0,0 +1,1 @@
+export * from './bc-gov-logo/bc-gov-logo.component';
