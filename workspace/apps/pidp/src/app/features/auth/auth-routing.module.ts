3c7d35da8cad9d94dc72b621120dc490304335ab
Martin Pultz <mtpultz@gmail.com>
2022-01-11 13:25:06 -0800

    Updated auth routes

--- workspace/apps/pidp/src/app/features/auth/auth-routing.module.ts
+++ workspace/apps/pidp/src/app/features/auth/auth-routing.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthRoutes } from './auth.routes';
+
+
 
 import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+
+const routes: Routes = [
+  {
+    path: AuthRoutes.LOGIN,
+    component: LoginComponent,
+    data: {
+      title: 'Provider Identity Portal',
+    },
+  },
+  {
+    path: '',
+    redirectTo: AuthRoutes.LOGIN,
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- workspace/apps/pidp/src/app/features/auth/auth.routes.ts
+++ workspace/apps/pidp/src/app/features/auth/auth.routes.ts
@@ -1,6 +1,8 @@
 export class AuthRoutes {
   public static MODULE_PATH = 'auth';
 
+  public static LOGIN = 'login';
+
   /**
    * @description
    * Useful for redirecting to module root-level routes.
