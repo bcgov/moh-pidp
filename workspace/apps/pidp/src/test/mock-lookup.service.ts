3249c0801b01a60bbe924800846bb2928cd614e7
Martin Pultz <mtpultz@gmail.com>
2022-01-10 16:07:16 -0800

    Added test and mocks for lookup service

--- workspace/apps/pidp/src/test/mock-lookup.service.ts
+++ workspace/apps/pidp/src/test/mock-lookup.service.ts
@@ -4,7 +4,10 @@ import { Observable } from 'rxjs';
 
 import { LookupResource } from '@app/modules/lookup/lookup-resource.service';
 import { LookupConfig } from '@app/modules/lookup/lookup.model';
-import { LookupService } from '@app/modules/lookup/lookup.service';
+import {
+  ILookupService,
+  LookupService,
+} from '@app/modules/lookup/lookup.service';
 
 import { UtilsService } from '@core/services/utils.service';
 
@@ -13,7 +16,7 @@ import { MockLookup } from './mock-lookup';
 @Injectable({
   providedIn: 'root',
 })
-export class MockLookupService extends LookupService {
+export class MockLookupService extends LookupService implements ILookupService {
   public constructor(
     protected lookupResource: LookupResource,
     protected utilsService: UtilsService
