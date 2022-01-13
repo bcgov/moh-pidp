85094137b6e2435c127c3a9fb8b1232977c6a8c1
Martin Pultz <mtpultz@gmail.com>
2022-01-12 17:53:37 -0800

    Updated model for parsed access token

--- workspace/apps/pidp/src/app/features/auth/services/access-token.service.ts
+++ workspace/apps/pidp/src/app/features/auth/services/access-token.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, from, map } from 'rxjs';
+
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { KeycloakService } from 'keycloak-angular';
 
@@ -15,23 +17,22 @@ export class AccessTokenService {
     this.jwtHelper = new JwtHelperService();
   }
 
-  public async token(): Promise<string> {
-    return await this.keycloakService.getToken();
+  public token$(): Observable<string> {
+    return from(this.keycloakService.getToken());
   }
 
   public isTokenExpired(): boolean {
     return this.keycloakService.isTokenExpired();
   }
 
-  // public async identityProvider(): Promise<IdentityProviderEnum | null> {
-  //   return await this.decodeToken().then(
-  //     (token: AccessTokenParsed | null) => token?.identity_provider
-  //   );
-  // }
+  public decodeToken$(): Observable<AccessTokenParsed | null> {
+    return this.token$().pipe(
+      map((token: string) => (token ? this.jwtHelper.decodeToken(token) : null))
+    );
+  }
 
-  public async decodeToken(): Promise<AccessTokenParsed | null> {
-    const token = await this.token();
-    return token ? this.jwtHelper.decodeToken(token) : null;
+  public roles(): string[] {
+    return this.keycloakService.getUserRoles();
   }
 
   public clearToken(): void {
