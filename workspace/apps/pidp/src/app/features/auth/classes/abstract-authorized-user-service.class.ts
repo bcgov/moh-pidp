ca4d5784c22ebb8d0effaa5fdd179d8a2f898621
Martin Pultz <mtpultz@gmail.com>
2022-01-15 18:44:59 -0800

    Added authorized user services

---
+++ workspace/apps/pidp/src/app/features/auth/classes/abstract-authorized-user-service.class.ts
@@ -0,0 +1,47 @@
+import { Observable, OperatorFunction, combineLatest } from 'rxjs';
+
+import { AccessTokenParsed } from '../models/access-token-parsed.model';
+import { BrokerProfile } from '../models/broker-profile.model';
+import { AccessTokenService } from '../services/access-token.service';
+
+export interface UserIdentity {
+  accessTokenParsed: AccessTokenParsed | null;
+  brokerProfile: BrokerProfile;
+}
+
+/**
+ * @description
+ * Abstract service for creating a type of authorized
+ * user from access token claims (user identity).
+ */
+export abstract class AbstractAuthorizedUserService<T> {
+  public constructor(private accessTokenService: AccessTokenService) {}
+
+  /**
+   * @description
+   * Get the authorized user mapped from the access token.
+   */
+  public get user$(): Observable<T | null> {
+    return this.getUserIdentity().pipe(this.fromUserIdentity());
+  }
+
+  /**
+   * @description
+   * Get the claims (user identity) from the access token.
+   */
+  protected getUserIdentity(): Observable<UserIdentity> {
+    return combineLatest({
+      accessTokenParsed: this.accessTokenService.decodeToken(),
+      brokerProfile: this.accessTokenService.loadBrokerProfile(),
+    });
+  }
+
+  /**
+   * @description
+   * Map the user identity to a generic model.
+   */
+  protected abstract fromUserIdentity(): OperatorFunction<
+    UserIdentity,
+    T | null
+  >;
+}
