df0fead84fe8d0f142a62174d82cc0f9557750e6
Martin Pultz <mtpultz@gmail.com>
2022-01-15 18:11:32 -0800

    Added admin controller and GET endpoint for parties

---
+++ backend/webapi/Features/Admin/AdminController.cs
@@ -0,0 +1,15 @@
+namespace Pidp.Features.Admin;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Produces("application/json")]
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<Index.Model>>> GetParties([FromServices] IQueryHandler<Index.Query, List<Index.Model>> handler,
+                                                                  [FromQuery] Index.Query query)
+        => await handler.HandleAsync(query);
+}
