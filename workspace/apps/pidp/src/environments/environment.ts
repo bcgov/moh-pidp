1f8607a8c12069b2247a80d053e312a0b7065a6c
Martin Pultz <mtpultz@gmail.com>
2022-01-17 18:36:39 -0800

    Removed event handler

--- workspace/apps/pidp/src/environments/environment.ts
+++ workspace/apps/pidp/src/environments/environment.ts
@@ -1,7 +1,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 import { AppEnvironment, EnvironmentName } from './environment.model';
 import { environment as defaultEnvironment } from './environment.prod';
 
