9655ac12a45e9d00d149991031ac1a781c0e945d
James <jameshollinger.ee@gmail.com>
2022-01-19 15:45:04 -0800

    finish profile mapping

--- backend/webapi/Features/Parties/MappingProfile.cs
+++ backend/webapi/Features/Parties/MappingProfile.cs
@@ -10,7 +10,10 @@ public class MappingProfile : Profile
     {
         this.CreateMap<Party, Demographics.Command>();
         this.CreateMap<Party, EnrolmentStatus.Model>();
-        this.CreateMap<Party, ProfileStatus.Model>();
+        this.CreateMap<Party, ProfileStatus.Model>()
+            .ForMember(dest => dest.CollegeCode, opt => opt.MapFrom(src => src.PartyCertification!.CollegeCode))
+            .ForMember(dest => dest.LicenceNumber, opt => opt.MapFrom(src => src.PartyCertification!.LicenceNumber))
+            .ForMember(dest => dest.FacilityStreet, opt => opt.MapFrom(src => src.Facility!.PhysicalAddress!.Street));
         this.CreateMap<Party, WorkSetting.Command>()
             .ForMember(dest => dest.PhysicalAddress, opt => opt.MapFrom(src => src.Facility!.PhysicalAddress));
         this.CreateMap<FacilityAddress, WorkSetting.Command.Address>();
