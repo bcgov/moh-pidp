76a8483ddaeff9d9866841e263ff2b31be1ef44f
James <jameshollinger.ee@gmail.com>
2022-01-19 12:21:13 -0800

    client wip

--- backend/webapi/Infrastructure/HttpClients/Plr/PlrClient.cs
+++ backend/webapi/Infrastructure/HttpClients/Plr/PlrClient.cs
@@ -1,5 +1,7 @@
 namespace Pidp.Infrastructure.HttpClients.Plr;
 
+using Flurl;
+using NodaTime;
 using System.Threading.Tasks;
 
 using Pidp.Models.Lookups;
@@ -8,26 +10,29 @@ public class PlrClient : BaseClient, IPlrClient
 {
     public PlrClient(HttpClient client, ILogger<PlrClient> logger) : base(client, logger) { }
 
-    public async Task<PlrRecord?> GetPlrRecord(string licenceNumber, CollegeCode collegeCode)
+    public async Task<string?> GetPlrRecord(string licenceNumber, CollegeCode collegeCode, LocalDate birthdate)
     {
         throw new NotImplementedException();
-        var response = await this.Client.GetAsync("");
+        var url = "records".SetQueryParams(new
+        {
+            CollegeId = licenceNumber,
+            Birthdate = birthdate.ToString()
+        });
+
+        var response = await this.Client.GetAsync(url);
         if (!response.IsSuccessStatusCode)
         {
+            this.Logger.LogError($"Error when retrieving PLR Record with CollegeId = {licenceNumber} and Birthdate = {birthdate}.");
             return null;
         }
 
-        var records = await response.Content.ReadFromJsonAsync<IEnumerable<PlrIntake.Features.Records.Index.Model>>();
+        var records = await response.Content.ReadFromJsonAsync<IEnumerable<PlrRecord>>();
 
     }
-}
 
-public class PlrRecord
-{
-    /// <summary>
-    /// Internal Party Code, PLRs unique identifier for each record.
-    /// </summary>
-    public string Ipc { get; set; } = string.Empty;
-
-    public string
+    private class PlrRecord
+    {
+        public string Ipc { get; set; } = string.Empty;
+        public string? IdentifierType { get; set; }
+    }
 }
