c09f3aaae15f4b75fa23c0d9f4607a98ea778ed4
Martin Pultz <mtpultz@gmail.com>
2022-01-19 10:01:47 -0800

    Added logo to portal dashboard

--- workspace/apps/pidp/src/app/features/shell/components/portal-dashboard/portal-dashboard.component.ts
+++ workspace/apps/pidp/src/app/features/shell/components/portal-dashboard/portal-dashboard.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 
 import { Observable, map } from 'rxjs';
 
-import { IDashboard } from '@bcgov/shared/ui';
+import { DashboardHeaderTheme, IDashboard } from '@bcgov/shared/ui';
 
 import { APP_CONFIG, AppConfig } from '@app/app.config';
 import { AccessTokenService } from '@app/features/auth/services/access-token.service';
@@ -17,6 +17,13 @@ import { AuthService } from '@app/features/auth/services/auth.service';
 export class PortalDashboardComponent implements IDashboard {
   public logoutRedirectUrl: string;
   public username: Observable<string>;
+  public headerConfig: {
+    theme: DashboardHeaderTheme;
+    allowMobileToggle: boolean;
+  };
+  public brandConfig: { imgSrc: string; imgAlt: string };
+  public showMenuItemIcons: boolean;
+  public responsiveMenuItems: boolean;
 
   public constructor(
     @Inject(APP_CONFIG) private config: AppConfig,
@@ -27,6 +34,13 @@ export class PortalDashboardComponent implements IDashboard {
     this.username = accessTokenService
       .decodeToken()
       .pipe(map((token) => token?.name ?? ''));
+    this.headerConfig = { theme: 'dark', allowMobileToggle: false };
+    this.brandConfig = {
+      imgSrc: '/assets/images/pidp-logo-white.svg',
+      imgAlt: 'Provider Identity Portal Logo',
+    };
+    this.showMenuItemIcons = true;
+    this.responsiveMenuItems = true;
   }
 
   public onLogout(): void {
