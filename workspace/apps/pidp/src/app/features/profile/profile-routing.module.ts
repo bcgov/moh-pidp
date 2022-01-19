1ae801e72cb34584fde697786e5feb37ea7e56c9
Martin Pultz <mtpultz@gmail.com>
2022-01-19 10:00:37 -0800

    Added feature flags to work setting, and transactions

--- workspace/apps/pidp/src/app/features/profile/profile-routing.module.ts
+++ workspace/apps/pidp/src/app/features/profile/profile-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FeatureFlagGuard } from '@app/modules/feature-flag/feature-flag.guard';
+import { Role } from '@app/shared/enums/roles.enum';
+
 import { CollegeLicenceInformationModule } from './pages/college-licence-information/college-licence-information.module';
 import { PersonalInformationModule } from './pages/personal-information/personal-information.module';
 import { UserAccessAgreementModule } from './pages/user-access-agreement/user-access-agreement.module';
@@ -24,6 +27,10 @@ const routes: Routes = [
   },
   {
     path: ProfileRoutes.WORK_AND_ROLE_INFO_PAGE,
+    canLoad: [FeatureFlagGuard],
+    data: {
+      features: [Role.FEATURE_PIDP_DEMO],
+    },
     loadChildren: (): Promise<WorkAndRoleInformationModule> =>
       import(
         './pages/work-and-role-information/work-and-role-information.module'
@@ -31,6 +38,10 @@ const routes: Routes = [
   },
   {
     path: ProfileRoutes.USER_ACCESS_AGREEMENT_PAGE,
+    canLoad: [FeatureFlagGuard],
+    data: {
+      features: [Role.FEATURE_PIDP_DEMO],
+    },
     loadChildren: (): Promise<UserAccessAgreementModule> =>
       import('./pages/user-access-agreement/user-access-agreement.module').then(
         (m) => m.UserAccessAgreementModule
