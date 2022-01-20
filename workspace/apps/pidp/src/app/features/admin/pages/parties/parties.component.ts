f482ae1b11b800623db35b2959866130d0acf1db
Martin Pultz <mtpultz@gmail.com>
2022-01-19 19:10:08 -0800

    Updated parties to populate a table with data

--- workspace/apps/pidp/src/app/features/admin/pages/parties/parties.component.ts
+++ workspace/apps/pidp/src/app/features/admin/pages/parties/parties.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
 
-import { DashboardHeaderTheme } from '@bcgov/shared/ui';
+import {
+  AdminResource,
+  PartyList,
+} from '../../shared/resources/admin-resource.service';
 
 @Component({
   selector: 'app-parties',
@@ -8,11 +13,30 @@ import { DashboardHeaderTheme } from '@bcgov/shared/ui';
   styleUrls: ['./parties.component.scss'],
 })
 export class PartiesComponent implements OnInit {
-  public theme: DashboardHeaderTheme;
+  public title: string;
+  public dataSource: MatTableDataSource<PartyList>;
+  public displayedColumns: string[] = [
+    'id',
+    'providerName',
+    'providerCollegeCode',
+    'saEforms',
+  ];
 
-  public constructor() {
-    this.theme = 'dark';
+  public constructor(
+    private adminResource: AdminResource,
+    route: ActivatedRoute
+  ) {
+    this.title = route.snapshot.data.title;
+    this.dataSource = new MatTableDataSource();
   }
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.getParties();
+  }
+
+  private getParties(): void {
+    this.adminResource
+      .getParties()
+      .subscribe((parties: PartyList[]) => (this.dataSource.data = parties));
+  }
 }
