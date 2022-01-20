93c6556dea62fc08b0d1e6e28490322e289d05e3
Martin Pultz <mtpultz@gmail.com>
2022-01-19 18:19:31 -0800

    Added temp admin resource

---
+++ workspace/apps/pidp/src/app/features/admin/shared/resources/admin-resource.service.ts
@@ -0,0 +1,34 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable, catchError, of } from 'rxjs';
+
+import { ApiResource } from '@app/core/resources/api-resource.service';
+import { ResourceUtilsService } from '@app/core/resources/resource-utils.service';
+
+export interface PartyList {
+  id: number;
+  providerName?: string;
+  providerCollegeCode?: string;
+  saEforms?: boolean;
+}
+
+// TODO temporary resource until more of the admin is known
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminResource {
+  public constructor(
+    private apiResource: ApiResource,
+    private resourceUtilsService: ResourceUtilsService
+  ) {}
+
+  public getParties(): Observable<PartyList[]> {
+    return this.apiResource.get<PartyList[]>('/admin/parties').pipe(
+      catchError((_: HttpErrorResponse) => {
+        // TODO add logging and toast messaging
+        return of([]);
+      })
+    );
+  }
+}
