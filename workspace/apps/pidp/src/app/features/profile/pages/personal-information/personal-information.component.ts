d358075374ad9dc278a6de471f7a9234a8892d29
Martin Pultz <mtpultz@gmail.com>
2022-01-20 15:43:47 -0800

    Updated to display birthdate in personal information

--- workspace/apps/pidp/src/app/features/profile/pages/personal-information/personal-information.component.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/personal-information/personal-information.component.ts
@@ -62,7 +62,7 @@ export class PersonalInformationComponent
 
   public ngOnInit(): void {
     // TODO pull from state management or URI param
-    const partyId = this.partyService.profileStatus?.id; // +this.route.snapshot.params.pid;
+    const partyId = 1; // this.partyService.profileStatus?.id; // +this.route.snapshot.params.pid;
     if (!partyId) {
       throw new Error('No party ID was provided');
     }
