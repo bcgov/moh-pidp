f079bc951f408d9f58d25693db65b6dd2a590e0e
Martin Pultz <mtpultz@gmail.com>
2022-01-19 18:19:21 -0800

    Added dashboard to admin routes, Updated admin module with lookups

--- workspace/apps/pidp/src/app/features/admin/admin-routing.module.ts
+++ workspace/apps/pidp/src/app/features/admin/admin-routing.module.ts
@@ -1,19 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthenticationGuard } from '../auth/guards/authentication.guard';
 import { AdminRoutes } from './admin.routes';
 import { PartiesComponent } from './pages/parties/parties.component';
+import { AdminDashboardComponent } from './shared/components/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   {
-    path: AdminRoutes.PARTIES,
-    component: PartiesComponent,
-    data: { title: 'Provider Identity Portal' },
-  },
-  {
     path: '',
-    redirectTo: AdminRoutes.PARTIES,
-    pathMatch: 'full',
+    component: AdminDashboardComponent,
+    canActivate: [AuthenticationGuard],
+    canActivateChild: [AuthenticationGuard],
+    data: {
+      // TODO don't hardcode in the redirect URL but also don't want cross module dependencies,
+      // TODO refactor when modules become libs otherwise premature optimization
+      routes: {
+        auth: '/auth/admin',
+      },
+    },
+    children: [
+      {
+        path: AdminRoutes.PARTIES,
+        component: PartiesComponent,
+        data: { title: 'Provider Identity Portal' },
+      },
+      {
+        path: '',
+        redirectTo: AdminRoutes.PARTIES,
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
 
--- workspace/apps/pidp/src/app/features/admin/admin.module.ts
+++ workspace/apps/pidp/src/app/features/admin/admin.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 
 import { DashboardModule } from '@bcgov/shared/ui';
 
+import { LookupModule } from '@app/modules/lookup/lookup.module';
 import { SharedModule } from '@app/shared/shared.module';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { PartiesComponent } from './pages/parties/parties.component';
+import { AdminDashboardComponent } from './shared/components/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
-  declarations: [PartiesComponent],
-  imports: [AdminRoutingModule, DashboardModule, SharedModule],
+  declarations: [PartiesComponent, AdminDashboardComponent],
+  imports: [
+    AdminRoutingModule,
+    DashboardModule,
+    SharedModule,
+    LookupModule.forChild(),
+  ],
 })
 export class AdminModule {}
