f33216a18ba5d66278b31e34fd5d93145536dfbb
Martin Pultz <mtpultz@gmail.com>
2022-01-19 19:28:04 -0800

    Reduce DI into service

--- workspace/apps/pidp/src/app/features/admin/shared/resources/admin-resource.service.ts
+++ workspace/apps/pidp/src/app/features/admin/shared/resources/admin-resource.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 import { Observable, catchError, of } from 'rxjs';
 
 import { ApiResource } from '@app/core/resources/api-resource.service';
-import { ResourceUtilsService } from '@app/core/resources/resource-utils.service';
 
 export interface PartyList {
   id: number;
@@ -18,10 +17,7 @@ export interface PartyList {
   providedIn: 'root',
 })
 export class AdminResource {
-  public constructor(
-    private apiResource: ApiResource,
-    private resourceUtilsService: ResourceUtilsService
-  ) {}
+  public constructor(private apiResource: ApiResource) {}
 
   public getParties(): Observable<PartyList[]> {
     return this.apiResource.get<PartyList[]>('/admin/parties').pipe(
