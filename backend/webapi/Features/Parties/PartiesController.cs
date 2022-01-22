de91a363fa1fecb0c09a4fc129babe0dd3130e1a
James <jameshollinger.ee@gmail.com>
2022-01-22 00:06:02 -0800

    swagger

--- backend/webapi/Features/Parties/PartiesController.cs
+++ backend/webapi/Features/Parties/PartiesController.cs
@@ -26,12 +26,16 @@ public class PartiesController : ControllerBase
 
     [HttpGet("{partyId}/college-certification")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    // [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<CollegeCertification.Command>> GetPartyCollegeCertification([FromServices] IQueryHandler<CollegeCertification.Query, CollegeCertification.Command> handler,
                                                                                                [FromRoute] CollegeCertification.Query query)
         => await handler.HandleAsync(query);
 
     [HttpPut("{partyId}/college-certification")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    // [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> UpdatePartyCollegeCertification([FromServices] ICommandHandler<CollegeCertification.Command> handler,
                                                                     [FromHybrid] CollegeCertification.Command command)
     {
@@ -41,12 +45,16 @@ public class PartiesController : ControllerBase
 
     [HttpGet("{id}/demographics")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<Demographics.Command>> GetPartyDemographics([FromServices] IQueryHandler<Demographics.Query, IDomainResult<Demographics.Command>> handler,
                                                                                [FromRoute] Demographics.Query query)
         => await handler.HandleAsync(query).ToActionResultOfT();
 
     [HttpPut("{id}/demographics")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    // [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> UpdatePartyDemographics([FromServices] ICommandHandler<Demographics.Command> handler,
                                                             [FromHybrid] Demographics.Command command)
     {
@@ -62,18 +70,24 @@ public class PartiesController : ControllerBase
 
     [HttpGet("{id}/profile-status")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProfileStatus.Model>> GetPartyProfileStatus([FromServices] IQueryHandler<ProfileStatus.Query, IDomainResult<ProfileStatus.Model>> handler,
                                                                                [FromHybrid] ProfileStatus.Query query)
         => await handler.HandleAsync(query).ToActionResultOfT();
 
     [HttpGet("{id}/work-setting")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<WorkSetting.Command>> GetPartyWorkSetting([FromServices] IQueryHandler<WorkSetting.Query, IDomainResult<WorkSetting.Command>> handler,
                                                                              [FromRoute] WorkSetting.Query query)
         => await handler.HandleAsync(query).ToActionResultOfT();
 
     [HttpPut("{id}/work-setting")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    // [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> UpdatePartyWorkSetting([FromServices] ICommandHandler<WorkSetting.Command> handler,
                                                            [FromHybrid] WorkSetting.Command command)
     {
