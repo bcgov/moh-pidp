298b720e1b1c97fc9ad6ae275629c63472d30f42
Martin Pultz <mtpultz@gmail.com>
2022-01-25 12:40:09 -0800

    Updated input bindings on card components

--- workspace/libs/shared/ui/src/lib/components/card-summary/card-summary.component.ts
+++ workspace/libs/shared/ui/src/lib/components/card-summary/card-summary.component.ts
@@ -17,12 +17,10 @@ import { AlertType } from '../alert/alert.component';
 export class CardSummaryComponent {
   @Input() public icon!: string;
   @Input() public title!: string;
-  @Input() public hint?: string;
-  @Input() public description!: string;
-  @Input() public actionLabel?: string;
   @Input() public statusType?: AlertType;
   @Input() public status?: string;
-  @Input() public disabled?: boolean;
+  @Input() public actionLabel?: string;
+  @Input() public actionDisabled?: boolean;
   @Output() public action: EventEmitter<void>;
 
   public constructor() {
