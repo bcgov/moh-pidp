33a35f452108b92cc04ab1844d075edfd75f7a34
James <jameshollinger.ee@gmail.com>
2022-01-27 18:36:30 -0800

    manual intake utility

---
+++ backend/utilities.plr-manual-intake/PlrRecordParser.cs
@@ -0,0 +1,170 @@
+namespace PlrManualIntake;
+
+using CsvHelper;
+using Serilog;
+
+using PlrIntake.Models;
+
+public static class PlrRecordParser
+{
+    private static readonly DateTime PlrNullDateTime = new DateTime(9999, 12, 30);
+
+    /// <summary>
+    /// Reads from a row in Excel file into a PlrRecord object.
+    /// </summary>
+    public static PlrRecord ReadRow(CsvReader reader)
+    {
+        PlrRecord provider = new PlrRecord();
+
+        provider.Ipc = GetString(reader, GetIndex("A"))!;
+        provider.Cpn = GetString(reader, GetIndex("B"));
+        provider.IdentifierType = GetString(reader, GetIndex("C"));
+        provider.CollegeId = GetString(reader, GetIndex("D"));
+        provider.ProviderRoleType = GetString(reader, GetIndex("E"));
+        provider.MspId = GetString(reader, GetIndex("F"));
+        provider.NamePrefix = GetString(reader, GetIndex("G"));
+        provider.FirstName = GetString(reader, GetIndex("H"));
+        provider.SecondName = GetString(reader, GetIndex("I"));
+        provider.ThirdName = GetString(reader, GetIndex("J"));
+        provider.LastName = GetString(reader, GetIndex("K"));
+        provider.Suffix = GetString(reader, GetIndex("L"));
+        provider.Gender = GetString(reader, GetIndex("M"));
+        provider.DateOfBirth = TryGetDateTime(reader, "N");
+        provider.StatusCode = GetString(reader, GetIndex("O"));
+        provider.StatusReasonCode = GetString(reader, GetIndex("P"));
+        provider.StatusStartDate = TryGetDateTime(reader, "Q");
+        provider.StatusExpiryDate = TryGetDateTime(reader, "R");
+        provider.Expertise = GetMultipleElements(GetString(reader, GetIndex("S")))
+            .Select(x => new Expertise(x))
+            .ToList();
+        // Not collecting Languages
+        // provider.Languages = GetString(reader, GetIndex("T"));
+        provider.Address1Line1 = GetString(reader, GetIndex("U"));
+        provider.Address1Line2 = GetString(reader, GetIndex("V"));
+        provider.Address1Line3 = GetString(reader, GetIndex("W"));
+        provider.City1 = GetString(reader, GetIndex("X"));
+        provider.Province1 = GetString(reader, GetIndex("Y"));
+        provider.Country1 = GetString(reader, GetIndex("Z"));
+        provider.PostalCode1 = GetString(reader, GetIndex("AA"));
+        provider.Address1StartDate = TryGetDateTime(reader, "AB");
+        provider.Credentials = GetMultipleElements(GetString(reader, GetIndex("AC")))
+            .Select(x => new Credential(x))
+            .ToList();
+        provider.TelephoneAreaCode = GetString(reader, GetIndex("AD"));
+        provider.TelephoneNumber = GetString(reader, GetIndex("AE"));
+        provider.FaxAreaCode = GetString(reader, GetIndex("AF"));
+        provider.FaxNumber = GetString(reader, GetIndex("AG"));
+        provider.Email = GetString(reader, GetIndex("AH"));
+        provider.ConditionCode = GetString(reader, GetIndex("AI"));
+        provider.ConditionStartDate = TryGetDateTime(reader, "AJ");
+        provider.ConditionEndDate = TryGetDateTime(reader, "AK");
+
+        return provider;
+    }
+
+    /// <summary>
+    /// Returns `null` if cell is empty
+    /// </summary>
+    private static string? GetString(CsvReader reader, int colIndex)
+    {
+        string value = reader.GetField<string>(colIndex);
+        return string.IsNullOrEmpty(value) ? null : value;
+    }
+
+    /// <summary>
+    /// Returns DateTime representing cell value, or `null` if cell is empty
+    /// </summary>
+    private static DateTime? TryGetDateTime(CsvReader reader, string columnId)
+    {
+        var dateTime = reader.GetField<DateTime?>(GetIndex(columnId));
+        if (dateTime == null)
+        {
+            return null;
+        }
+        // Treat value meant to represent NULL as `null`
+        else if (dateTime.Equals(PlrNullDateTime))
+        {
+            return null;
+        }
+        else
+        {
+            return dateTime;
+        }
+    }
+
+    /// <param name="aValue">A pipe-delimited string</param>
+    /// <returns>Array containing parsed elements or an empty array if given input is <c>null</c>.</returns>
+    private static string[] GetMultipleElements(string? aValue)
+    {
+        if (aValue != null)
+        {
+            return aValue.Split('|');
+        }
+        else
+        {
+            return new string[0];
+        }
+    }
+
+    /// <summary>
+    /// Check fields that are expected to be populated, logging error if not populated.
+    /// </summary>
+    public static void CheckData(PlrRecord provider, int rowNum)
+    {
+        CheckRequiredField(provider.Ipc, nameof(provider.Ipc), rowNum);
+        CheckRequiredField(provider.IdentifierType, nameof(provider.IdentifierType), rowNum);
+        CheckRequiredField(provider.CollegeId, nameof(provider.CollegeId), rowNum);
+        CheckRequiredField(provider.ProviderRoleType, nameof(provider.ProviderRoleType), rowNum);
+        CheckRequiredField(provider.FirstName, nameof(provider.FirstName), rowNum);
+        CheckRequiredField(provider.LastName, nameof(provider.LastName), rowNum);
+        CheckRequiredField(provider.Gender, nameof(provider.Gender), rowNum);
+        CheckRequiredField(provider.DateOfBirth, nameof(provider.DateOfBirth), rowNum);
+        CheckRequiredField(provider.StatusCode, nameof(provider.StatusCode), rowNum);
+        CheckRequiredField(provider.StatusReasonCode, nameof(provider.StatusReasonCode), rowNum);
+        CheckRequiredField(provider.StatusStartDate, nameof(provider.StatusStartDate), rowNum);
+        CheckRequiredField(provider.Address1Line1, nameof(provider.Address1Line1), rowNum);
+        CheckRequiredField(provider.City1, nameof(provider.City1), rowNum);
+        CheckRequiredField(provider.Province1, nameof(provider.Province1), rowNum);
+        CheckRequiredField(provider.PostalCode1, nameof(provider.PostalCode1), rowNum);
+        CheckRequiredField(provider.Address1StartDate, nameof(provider.Address1StartDate), rowNum);
+        CheckRequiredField(provider.TelephoneAreaCode, nameof(provider.TelephoneAreaCode), rowNum);
+        CheckRequiredField(provider.TelephoneNumber, nameof(provider.TelephoneNumber), rowNum);
+        CheckRequiredField(provider.Email, nameof(provider.Email), rowNum);
+    }
+
+    private static bool CheckRequiredField(object? fieldValue, string fieldName, int rowNum)
+    {
+        if (fieldValue == null || fieldValue.Equals(DateTime.MinValue))
+        {
+            Log.Warning($"'{fieldName}' was empty at row number {rowNum}.");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Algorithm courtesy of https://stackoverflow.com/questions/667802/what-is-the-algorithm-to-convert-an-excel-column-letter-into-its-number
+    /// </summary>
+    /// <param name="excelColumn">For example, "AC"</param>
+    /// <returns>Zero-based index, e.g. 28</returns>
+    public static int GetIndex(string excelColumn)
+    {
+        if (string.IsNullOrEmpty(excelColumn)) throw new ArgumentNullException("excelColumn");
+
+        excelColumn = excelColumn.ToUpperInvariant();
+
+        int sum = 0;
+
+        for (int i = 0; i < excelColumn.Length; i++)
+        {
+            sum *= 26;
+            sum += excelColumn[i] - 'A' + 1;
+        }
+
+        // Zero-based indexing
+        return sum - 1;
+    }
+}
---
+++ backend/utilities.plr-manual-intake/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace PlrManualIntake;
+
+using CsvHelper;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.Globalization;
+
+using PlrIntake.Models;
+using PlrIntake.Data;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    /// <summary>
+    /// Can be used like this:  `dotnet run PLR_Test_Data_IAT20210617_v2.0.csv intake.log`
+    /// </summary>
+    /// <param name="args">Expecting path to .csv file and desired log file</param>
+    static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(args[1])
+            .CreateLogger();
+
+        // See https://github.com/ExcelDataReader/ExcelDataReader#important-note-on-net-core
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+        var connectionString = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build()
+            .GetConnectionString("PlrDatabase");
+
+        var optionsBuilder = new DbContextOptionsBuilder<PlrDbContext>()
+            .UseNpgsql(connectionString);
+
+        Log.Information($"Started loading {args[0]} at {DateTime.Now}");
+        try
+        {
+            var dbContext = new PlrDbContext(optionsBuilder.Options);
+
+            using (var stream = new StreamReader(args[0]))
+            {
+                using (var reader = new CsvReader(stream, CultureInfo.InvariantCulture))
+                {
+                    int numProviders = 0;
+                    int rowNum = 1;
+
+                    // Consume header row
+                    reader.Read();
+
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            rowNum++;
+                            var provider = PlrRecordParser.ReadRow(reader);
+                            PlrRecordParser.CheckData(provider, rowNum);
+                            dbContext.PlrRecords.Add(provider);
+                            try
+                            {
+                                dbContext.SaveChanges();
+                                numProviders++;
+                            }
+                            catch (DbUpdateException e)
+                            {
+                                // e.g. May get `duplicate key value violates unique constraint "IX_PlrProvider_Ipc"` error
+                                Log.Error(e, $"Error saving {nameof(PlrRecord)} at row number {rowNum} to the database.");
+                                dbContext.PlrRecords.Remove(provider);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Error(e, $"Error ingesting row at row number {rowNum}, IPC: {reader.GetField<string>(0)}");
+                        }
+                    }
+
+                    Log.Information($"Number of providers loaded: {numProviders}, Final row number: {rowNum}.");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Unexpected error encountered.");
+        }
+        finally
+        {
+            Log.Information($"Program ended at {DateTime.Now}");
+            Console.WriteLine($"Program completed ... check {args[1]}.");
+        }
+    }
+}
