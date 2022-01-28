7ce90b6701a6880df98d523ff8d747598a80ccba
James <jameshollinger.ee@gmail.com>
2022-01-27 17:23:29 -0800

    add auth and action names to addressautocomplete controller

--- backend/webapi/Features/AddressAutocomplete/AddressAutocompleteController.cs
+++ backend/webapi/Features/AddressAutocomplete/AddressAutocompleteController.cs
@@ -1,24 +1,28 @@
 namespace Pidp.Features.AddressAutocomplete;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
+using Pidp.Infrastructure.Auth;
 using Pidp.Infrastructure.HttpClients.AddressAutocomplete;
 
 [Produces("application/json")]
 [Route("api/[controller]")]
 [ApiController]
+[Authorize(Policy = Policies.BcscAuthentication)]
 public class AddressAutocompleteController : ControllerBase
 {
     [HttpGet("find")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<List<AddressAutocompleteFindResponse>>> GetParties([FromServices] IQueryHandler<Find.Query, List<AddressAutocompleteFindResponse>> handler,
-                                                                                      [FromQuery] Find.Query query)
+    public async Task<ActionResult<List<AddressAutocompleteFindResponse>>> FindAddresses([FromServices] IQueryHandler<Find.Query, List<AddressAutocompleteFindResponse>> handler,
+                                                                                         [FromQuery] Find.Query query)
         => await handler.HandleAsync(query);
 
     [HttpGet("retrieve")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<List<AddressAutocompleteRetrieveResponse>>> GetParties([FromServices] IQueryHandler<Retrieve.Query, List<AddressAutocompleteRetrieveResponse>> handler,
-                                                                     [FromQuery] Retrieve.Query query)
+    public async Task<ActionResult<List<AddressAutocompleteRetrieveResponse>>> RetrieveAddresses([FromServices] IQueryHandler<Retrieve.Query, List<AddressAutocompleteRetrieveResponse>> handler,
+                                                                                                 [FromQuery] Retrieve.Query query)
         => await handler.HandleAsync(query);
 }
