4ec6dd15e7e7d983e5f7ea6d78521496508415be
James <jameshollinger.ee@gmail.com>
2022-01-29 01:14:22 -0800

    update for performance

--- backend/utilities.plr-manual-intake/Program.cs
+++ backend/utilities.plr-manual-intake/Program.cs
@@ -28,49 +28,53 @@ public class Program
             .Build()
             .GetConnectionString("PlrDatabase");
 
-        var optionsBuilder = new DbContextOptionsBuilder<PlrDbContext>()
-            .UseNpgsql(connectionString);
+        var dbOptions = new DbContextOptionsBuilder<PlrDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
 
         Log.Information($"Started loading {args[0]} at {DateTime.Now}");
         try
         {
-            var dbContext = new PlrDbContext(optionsBuilder.Options);
+            var dbContext = CreateContext(dbOptions);
+            var batchSize = 1000;
+            var recordCount = 0;
+            var rowNum = 1;
 
             using var stream = new StreamReader(args[0]);
             using var reader = new CsvReader(stream, CultureInfo.InvariantCulture);
-            var numProviders = 0;
-            var rowNum = 1;
 
             // Consume header row
             reader.Read();
 
             while (reader.Read())
             {
+                rowNum++;
+
+                PlrRecord? record = null;
                 try
                 {
-                    rowNum++;
-                    var provider = PlrRecordParser.ReadRow(reader);
-                    PlrRecordParser.CheckData(provider, rowNum);
-                    dbContext.PlrRecords.Add(provider);
-                    try
-                    {
-                        dbContext.SaveChanges();
-                        numProviders++;
-                    }
-                    catch (DbUpdateException e)
-                    {
-                        // e.g. May get `duplicate key value violates unique constraint "IX_PlrProvider_Ipc"` error
-                        Log.Error(e, $"Error saving {nameof(PlrRecord)} at row number {rowNum} to the database.");
-                        dbContext.PlrRecords.Remove(provider);
-                    }
+                    record = PlrRecordParser.ReadRow(reader);
                 }
                 catch (Exception e)
                 {
-                    Log.Error(e, $"Error ingesting row at row number {rowNum}, IPC: {reader.GetField<string>(0)}");
+                    Log.Error(e, $"Error parsing row at row number {rowNum}");
+                    continue;
                 }
+
+                if (record == null)
+                {
+                    Log.Error($"Null Record at row number {rowNum}");
+                    continue;
+                }
+
+                recordCount++;
+                PlrRecordParser.CheckData(record, rowNum);
+                dbContext = AddToContext(dbContext, record, recordCount, batchSize, dbOptions);
             }
 
-            Log.Information($"Number of providers loaded: {numProviders}, Final row number: {rowNum}.");
+            dbContext.SaveChanges();
+
+            Log.Information($"Number of records loaded: {recordCount}, Final row number: {rowNum}.");
         }
         catch (Exception e)
         {
@@ -81,5 +85,30 @@ public class Program
             Log.Information($"Program ended at {DateTime.Now}");
             Console.WriteLine($"Program completed ... check {args[1]}.");
         }
+    }
+
+    private static PlrDbContext AddToContext(PlrDbContext context, PlrRecord record, int count, int batchSize, DbContextOptions<PlrDbContext>? recreationOptions = null)
+    {
+        context.PlrRecords.Add(record);
+
+        if (count % batchSize == 0)
+        {
+            context.SaveChanges();
+
+            if (recreationOptions != null)
+            {
+                context.Dispose();
+                context = CreateContext(recreationOptions);
+            }
+        }
+
+        return context;
+    }
+
+    private static PlrDbContext CreateContext(DbContextOptions<PlrDbContext> options)
+    {
+        var context = new PlrDbContext(options);
+        context.ChangeTracker.AutoDetectChangesEnabled = false;
+        return context;
     }
 }
