330d4588297709ca98615efc8714bd2f4296402c
Martin Pultz <mtpultz@gmail.com>
2022-02-03 11:44:43 -0800

    Move saEforms URL to constants file

--- workspace/apps/pidp/src/app/features/access/pages/special-authority-eforms/special-authority-eforms.component.ts
+++ workspace/apps/pidp/src/app/features/access/pages/special-authority-eforms/special-authority-eforms.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { LoggerService } from '@app/core/services/logger.service';
 import { PartyService } from '@app/core/services/party.service';
 
+import { saEformsUrl } from './special-authority.constants';
+
 @Component({
   selector: 'app-special-authority-eforms',
   templateUrl: './special-authority-eforms.component.html',
@@ -20,7 +22,7 @@ export class SpecialAuthorityEformsComponent implements OnInit {
     private logger: LoggerService
   ) {
     this.title = this.route.snapshot.data.title;
-    this.saEformsUrl = 'https://www.eforms.phsahealth.ca/appdash';
+    this.saEformsUrl = saEformsUrl;
   }
 
   public onBack(): void {
---
+++ workspace/apps/pidp/src/app/features/access/pages/special-authority-eforms/special-authority.constants.ts
@@ -0,0 +1,1 @@
+export const saEformsUrl = 'https://www.eforms.phsahealth.ca/appdash';
