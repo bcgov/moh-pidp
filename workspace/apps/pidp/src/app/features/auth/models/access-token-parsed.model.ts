85513e72b2cae06d0d8d34a8a71158fbcc4a9b8a
Martin Pultz <mtpultz@gmail.com>
2022-02-03 14:00:12 -0800

    Added identity provider to parsed token model

--- workspace/apps/pidp/src/app/features/auth/models/access-token-parsed.model.ts
+++ workspace/apps/pidp/src/app/features/auth/models/access-token-parsed.model.ts
@@ -1,5 +1,7 @@
 import { KeycloakTokenParsed } from 'keycloak-js';
 
+import { IdentityProviderEnum } from '../enums/identity-provider.enum';
+
 export interface AccessTokenParsed extends KeycloakTokenParsed {
   acr: string;
   'allowed-origins': string[];
@@ -12,6 +14,7 @@ export interface AccessTokenParsed extends KeycloakTokenParsed {
   typ: string;
   sub: string;
   // User specific attributes:
+  identity_provider: IdentityProviderEnum;
   email_verified: boolean;
   family_name: string;
   given_name: string;
