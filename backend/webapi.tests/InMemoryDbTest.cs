8a4df2d121d4d15d3dc2baa89cb31c03310b10f9
James <jameshollinger.ee@gmail.com>
2022-02-08 18:56:06 -0800

    Improve webapi tests, add PidpAuthorizationService tests

--- backend/webapi.tests/InMemoryDbTest.cs
+++ backend/webapi.tests/InMemoryDbTest.cs
@@ -1,28 +1,57 @@
+namespace PidpTests;
+
+using FakeItEasy.Sdk;
 using Microsoft.EntityFrameworkCore;
 using NodaTime;
 
 using Pidp.Data;
 
-namespace PidpTests
+public class InMemoryDbTest : IDisposable
 {
-    public class InMemoryDbTest : IDisposable
+    protected PidpDbContext TestDb { get; }
+
+    protected InMemoryDbTest()
     {
-        protected PidpDbContext TestDb;
+        var options = new DbContextOptionsBuilder<PidpDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
 
-        protected InMemoryDbTest()
+        this.TestDb = new PidpDbContext(options, SystemClock.Instance);
+        this.TestDb.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        this.TestDb.Database.EnsureDeleted();
+        this.TestDb.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Creates an instance of the given Type, injecting the TestDb and empty mocks for all dependencies (as applicable).
+    /// Any supplied implementations or mocks of Interfaces will be used instead of empty mocks.
+    /// Requires exactly one public constructor on the Type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="dependencyOverrides"></param>
+    /// <returns></returns>
+    public T MockDependenciesFor<T>(params object[] dependencyOverrides)
+    {
+        var ctor = typeof(T).GetConstructors().Single();
+
+        var parameters = new List<object>();
+        foreach (var parameterType in ctor.GetParameters().Select(p => p.ParameterType))
         {
-            var options = new DbContextOptionsBuilder<PidpDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
+            var defaultParameter = parameterType == typeof(PidpDbContext)
+                ? this.TestDb
+                : Create.Fake(parameterType);
 
-            TestDb = new PidpDbContext(options, SystemClock.Instance);
-            TestDb.Database.EnsureCreated();
+            parameters.Add(
+                dependencyOverrides.SingleOrDefault(x => x.GetType().GetInterfaces().Contains(parameterType))
+                    ?? defaultParameter
+            );
         }
 
-        public void Dispose()
-        {
-            TestDb.Database.EnsureDeleted();
-            TestDb.Dispose();
-        }
+        return (T)ctor.Invoke(parameters.ToArray());
     }
 }
