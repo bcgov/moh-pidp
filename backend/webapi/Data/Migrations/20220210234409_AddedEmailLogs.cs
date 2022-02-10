da0ddf12ce6a92936fb10c18170a905ec5e27aaf
Martin Pultz <mtpultz@gmail.com>
2022-02-10 15:48:14 -0800

    Added migrations

---
+++ backend/webapi/Data/Migrations/20220210234409_AddedEmailLogs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Pidp.Data.Migrations
+{
+    public partial class AddedEmailLogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmailLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SendType = table.Column<string>(type: "text", nullable: false),
+                    MsgId = table.Column<Guid>(type: "uuid", nullable: true),
+                    SentTo = table.Column<string>(type: "text", nullable: false),
+                    Cc = table.Column<string>(type: "text", nullable: false),
+                    Subject = table.Column<string>(type: "text", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    DateSent = table.Column<Instant>(type: "timestamp with time zone", nullable: true),
+                    LatestStatus = table.Column<string>(type: "text", nullable: true),
+                    StatusMessage = table.Column<string>(type: "text", nullable: true),
+                    UpdateCount = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<Instant>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<Instant>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailLog", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailLog");
+        }
+    }
+}
