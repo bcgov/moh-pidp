fcca67c1f587c50e8f5edc2680d594bce4bdb4dd
Martin Pultz <mtpultz@users.noreply.github.com>
2022-02-14 18:20:51 -0800

    Remove opening and closing spaces in template literal

--- workspace/libs/shared/ui/src/lib/components/dialogs/content/html/html.component.ts
+++ workspace/libs/shared/ui/src/lib/components/dialogs/content/html/html.component.ts
@@ -4,7 +4,7 @@ import { IDialogContent } from '@bcgov/shared/ui';
 
 @Component({
   selector: 'ui-html',
-  template: ` <p [innerHtml]="data.content | safe: 'html'"></p> `,
+  template: `<p [innerHtml]="data.content | safe: 'html'"></p>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HtmlComponent implements IDialogContent {
