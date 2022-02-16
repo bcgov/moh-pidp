d8c9b24aa41819ad1df4d0035eb4e9b65adcb565
James <jameshollinger.ee@gmail.com>
2022-02-16 13:52:36 -0800

    Core send method on BaseClient

--- backend/webapi/Infrastructure/HttpClients/BaseClient.cs
+++ backend/webapi/Infrastructure/HttpClients/BaseClient.cs
@@ -6,6 +6,9 @@ using System.Text.Json;
 using System.Net.Http;
 
 using Pidp.Extensions;
+using Pidp.Features;
+using DomainResults.Common;
+using System.Net;
 
 public enum PropertySerialization
 {
@@ -45,4 +48,106 @@ public class BaseClient
     /// </summary>
     /// <param name="data"></param>
     public StringContent CreateStringContent(object data) => new(JsonSerializer.Serialize(data, this.serializationOptions), Encoding.UTF8, "application/json");
+
+    /// <summary>
+    /// Sends an HTTP message to the API; returning:
+    ///  a) a Success result, or
+    ///  b) a Failure result in the case of errors or a non-success status code
+    /// </summary>
+    /// <param name="method"></param>
+    /// <param name="url"></param>
+    /// <param name="cancellationToken"></param>
+    protected async Task<IDomainResult> SendCoreAsync(HttpMethod method, string url, CancellationToken cancellationToken)
+    {
+        var result = await this.SendCoreInternalAsync(method, url, cancellationToken);
+        result.Deconstruct(out _, out var details);
+        return details;
+    }
+
+    /// <summary>
+    /// Send an HTTTP message to the API; returning:
+    ///  a) a Success result with a (non-null) value of the indicated type, or
+    ///  b) a Failure result in the case of errors, non-success status codes, or a missing/null response value.
+    /// </summary>
+    /// <typeparam name="T">Type of the API's Response Content.</typeparam>
+    /// <param name="method"></param>
+    /// <param name="url"></param>
+    /// <param name="cancellationToken"></param>
+    protected async Task<IDomainResult<T>> SendCoreAsync<T>(HttpMethod method, string url, CancellationToken cancellationToken)
+    {
+        var result = await this.SendCoreInternalAsync(method, url, cancellationToken);
+        if (!result.IsSuccess)
+        {
+            return result.To<T>();
+        }
+
+        var responseContent = result.Value;
+
+        if (responseContent == null)
+        {
+            this.Logger.LogError("Response content was null");
+            return DomainResult.Failed<T>("Response content was null");
+        }
+
+        try
+        {
+            var deserializationResult = await responseContent.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+            if (deserializationResult == null)
+            {
+                this.Logger.LogError("Response content was null");
+                return DomainResult.Failed<T>("Response content was null");
+            }
+
+            return DomainResult.Success(deserializationResult);
+        }
+        catch (JsonException exception)
+        {
+            this.Logger.LogError(exception, "Error when deserializaing response body after calling the API");
+            return DomainResult.Failed<T>("Could not deserialize API response");
+        }
+    }
+
+    private async Task<IDomainResult<HttpContent>> SendCoreInternalAsync(HttpMethod method, string url, CancellationToken cancellationToken)
+    {
+        try
+        {
+            using var response = await this.Client.SendAsync(new HttpRequestMessage(method, url), cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // TODO: retryable status codes?
+                // if (RetryableStatusCodes.Contains(response.StatusCode))
+                // {
+                //     return DomainResult.Failed<T>($"Received retryable status code '{response.StatusCode}'");
+                // }
+
+                this.Logger.LogError($"Recieved non-success status code {response.StatusCode}.");
+                return DomainResult.Failed<HttpContent>(response.StatusCode == HttpStatusCode.NotFound
+                    ? $"The URL {url} was not found"
+                    : "Did not receive a successful status code");
+            }
+
+            return DomainResult.Success(response.Content);
+        }
+        catch (HttpRequestException exception)
+        {
+            this.Logger.LogError(exception, "HttpRequestException when calling the API");
+            return DomainResult.Failed<HttpContent>("HttpRequestException during call to API");
+        }
+        catch (TimeoutException exception)
+        {
+            this.Logger.LogError(exception, "TimeoutException during call to API");
+            return DomainResult.Failed<HttpContent>("TimeoutException during call to API");
+        }
+        catch (OperationCanceledException exception)
+        {
+            this.Logger.LogError(exception, "Task was canceled during call to API");
+            return DomainResult.Failed<HttpContent>("Task was canceled during call to API");
+        }
+        catch (Exception exception)
+        {
+            this.Logger.LogError(exception, "Unhandled exception when calling the API");
+            return DomainResult.Failed<HttpContent>("Unhandled exception when calling the API");
+        }
+    }
 }
