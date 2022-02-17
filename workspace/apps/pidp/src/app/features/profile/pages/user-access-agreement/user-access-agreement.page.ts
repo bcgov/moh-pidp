65ef7189bf2cf04836d00cbba9571e8ddaa1d345
Martin Pultz <mtpultz@gmail.com>
2022-02-16 17:28:19 -0800

    Remove demoservice

--- workspace/apps/pidp/src/app/features/profile/pages/user-access-agreement/user-access-agreement.page.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/user-access-agreement/user-access-agreement.page.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { DateTime } from 'luxon';
-
-import { APP_DATE_FORMAT } from '@bcgov/shared/ui';
-
-import { DemoService } from '@core/services/demo.service';
-
 @Component({
   selector: 'app-user-access-agreement',
   templateUrl: './user-access-agreement.page.html',
@@ -15,46 +9,9 @@ import { DemoService } from '@core/services/demo.service';
 export class UserAccessAgreementPage {
   public title: string;
 
-  public constructor(
-    private route: ActivatedRoute,
-    private demoService: DemoService
-  ) {
+  public constructor(private route: ActivatedRoute) {
     this.title = this.route.snapshot.data.title;
   }
 
-  public onSubmit(): void {
-    this.demoService.profileComplete = true;
-
-    this.demoService.state.profileIdentitySections =
-      this.demoService.state.profileIdentitySections.map((section) => {
-        // if (section.type === 'user-access-agreement') {
-        //   return {
-        //     ...section,
-        //     statusType: 'success',
-        //     status: 'completed',
-        //     hint: DateTime.now().toFormat(APP_DATE_FORMAT),
-        //   };
-        // }
-
-        return section;
-      });
-
-    // this.demoService.state.accessToSystemsSections =
-    //   this.demoService.state.accessToSystemsSections.map((section) => {
-    //     section.actionDisabled = false;
-    //     return section;
-    //   });
-
-    // this.demoService.state.trainingSections =
-    //   this.demoService.state.trainingSections.map((section) => {
-    //     section.actionDisabled = false;
-    //     return section;
-    //   });
-
-    // this.demoService.state.yourProfileSections =
-    //   this.demoService.state.yourProfileSections.map((section) => {
-    //     section.actionDisabled = false;
-    //     return section;
-    //   });
-  }
+  public onSubmit(): void {}
 }
