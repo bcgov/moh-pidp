d6c7bfcd20f2f6e5a7b2b420fc24ffaf476722b0
Martin Pultz <mtpultz@gmail.com>
2022-03-18 17:19:44 -0700

    Added stub into HCIM resource

--- workspace/apps/pidp/src/app/features/access/pages/hcim-reenrolment/hcim-reenrolment-resource.service.ts
+++ workspace/apps/pidp/src/app/features/access/pages/hcim-reenrolment/hcim-reenrolment-resource.service.ts
@@ -1,6 +1,13 @@
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { CrudResource } from '@bcgov/shared/data-access';
+import { catchError, of, throwError } from 'rxjs';
+
+import {
+  CrudResource,
+  NoContent,
+  NoContentResponse,
+} from '@bcgov/shared/data-access';
 
 import { ApiHttpClient } from '@app/core/resources/api-http-client.service';
 
@@ -16,8 +23,52 @@ export class HcimReenrolmentResource extends CrudResource<HcimReenrolmentModel> 
   // TODO what responses are expected?
   // TODO are we tracking attempts?
 
+  // public hcimReEnrolment(
+  //   partyId: number,
+  //   ldapUsername: string,
+  //   ldapPassword: string
+  // ): NoContent {
+  //   return this.apiResource
+  //     .post<NoContent>(`${this.resourceBaseUri}/hcim-re-enrolment`, {
+  //       partyId,
+  //       ldapUsername,
+  //       ldapPassword,
+  //     })
+  //     .pipe(
+  //       NoContentResponse,
+  //       catchError((error: HttpErrorResponse) => {
+  //         if (error.status === HttpStatusCode.BadRequest) {
+  //           return of(void 0);
+  //         }
+
+  //         return throwError(() => error);
+  //       })
+  //     );
+  // }
+
+  public requestAccess(
+    partyId: number,
+    ldapCredentials: { ldapUsername: string; ldapPassword: string }
+  ): NoContent {
+    return this.apiResource
+      .post<NoContent>('access-requests/hcim-reenrolment', {
+        partyId,
+        ...ldapCredentials,
+      })
+      .pipe(
+        NoContentResponse,
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === HttpStatusCode.BadRequest) {
+            return of(void 0);
+          }
+
+          return throwError(() => error);
+        })
+      );
+  }
+
   protected getResourcePath(partyId: number): string {
     // TODO need resource endpoint
-    return `parties/${partyId}/lorem-ipsum`;
+    return `parties/${partyId}/hcim-reenrolment`;
   }
 }
