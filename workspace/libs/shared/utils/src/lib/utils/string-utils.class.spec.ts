1cb08714b9c5af471784f5a4c14c8cc0a4546151
Panos Hatzinikolaou <45132887+Paahn@users.noreply.github.com>
2022-03-24 17:29:10 -0700

    PIDP Work and Role Information Page - Isolated Unit Tests (#36)
    * create passes
    
    * wip
    
    * string utils test pass
    
    * test INIT
    
    * INIT
    
    * testing onBack
    
    * pr feedback
    
    * updated import

--- workspace/libs/shared/utils/src/lib/utils/string-utils.class.spec.ts
+++ workspace/libs/shared/utils/src/lib/utils/string-utils.class.spec.ts
@@ -19,16 +19,6 @@ describe('StringUtils', () => {
       const result = util.capitalize(value);
       expect(result).toBe(value);
     });
-
-    it('should not fail when passed a null', () => {
-      const result = util.capitalize(null);
-      expect(result).toBeNull();
-    });
-
-    it('should not fail when passed undefined', () => {
-      const result = util.capitalize(undefined);
-      expect(result).toBeUndefined();
-    });
   });
 
   describe('splice', () => {
@@ -36,7 +26,9 @@ describe('StringUtils', () => {
 
     it('should insert text at a specific position in the string', () => {
       const result = util.splice(strValue, 40, 'spotted ');
-      expect(result).toBe('The quick brown fox jumps over the lazy spotted dog.');
+      expect(result).toBe(
+        'The quick brown fox jumps over the lazy spotted dog.'
+      );
     });
 
     it('should insert and remove text at a specific position in the string from the start', () => {
@@ -49,13 +41,12 @@ describe('StringUtils', () => {
       expect(result).toBe('The quick brown fox jumps over the fire engine.');
     });
 
-    it('should not fail when passed null', () => {
-      const result = util.splice(null, 35, 'fire engine', 8);
-      expect(result).toBeNull();
-    });
-
     it('should append to the end of the string when insert position is greater than the text length', () => {
-      const result = util.splice(strValue, strValue.length + 10, '.. and then what?');
+      const result = util.splice(
+        strValue,
+        strValue.length + 10,
+        '.. and then what?'
+      );
       expect(result).toBe(`${strValue}.. and then what?`);
     });
   });
