b2b43eea6ade8b6f240a2d10ced217f18207514c
Panos <paahndev@gmail.com>
2022-03-30 11:30:15 -0700

    add test onBack

--- workspace/apps/pidp/src/app/features/shell/pages/support-error/support-error.page.spec.ts
+++ workspace/apps/pidp/src/app/features/shell/pages/support-error/support-error.page.spec.ts
@@ -7,6 +7,7 @@ import { SupportErrorPage } from './support-error.page';
 
 describe('SupportErrorPage', () => {
   let component: SupportErrorPage;
+  let router: Router;
 
   const mockActivatedRoute = {
     shellRoutes: {
@@ -25,10 +26,20 @@ describe('SupportErrorPage', () => {
         provideAutoSpy(Router),
       ],
     });
+    router = TestBed.inject(Router);
     component = TestBed.inject(SupportErrorPage);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('METHOD: onBack', () => {
+    given('user wants to navigate back to the PIDP', () => {
+      when('onBack is invoked', () => {
+        component.onBack();
+
+        then('router should navigate to the module root route', () => {
+          const moduleRootRoute = mockActivatedRoute.shellRoutes.MODULE_PATH;
+          expect(router.navigate).toHaveBeenCalledWith([moduleRootRoute]);
+        });
+      });
+    });
   });
 });
