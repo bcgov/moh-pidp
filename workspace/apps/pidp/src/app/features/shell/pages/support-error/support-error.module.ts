b8081a4ae715e02ff1483ea0601c1631c013087a
Panos <paahndev@gmail.com>
2022-03-30 15:35:53 -0700

    added routing module

--- workspace/apps/pidp/src/app/features/shell/pages/support-error/support-error.module.ts
+++ workspace/apps/pidp/src/app/features/shell/pages/support-error/support-error.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { DashboardModule, MaterialModule } from '@bcgov/shared/ui';
 
+import { SupportErrorRoutingModule } from './support-error-routing.module';
 import { SupportErrorPage } from './support-error.page';
 
 @NgModule({
   declarations: [SupportErrorPage],
-  imports: [DashboardModule, MaterialModule],
+  imports: [SupportErrorRoutingModule, DashboardModule, MaterialModule],
 })
 export class SupportErrorModule {}
