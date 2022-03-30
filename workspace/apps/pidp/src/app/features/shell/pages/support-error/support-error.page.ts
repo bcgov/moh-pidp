39b833ce56ae267f8c3d4a159c1c7d098dec0f83
Panos <paahndev@gmail.com>
2022-03-29 17:03:45 -0700

    add button

--- workspace/apps/pidp/src/app/features/shell/pages/support-error/support-error.page.ts
+++ workspace/apps/pidp/src/app/features/shell/pages/support-error/support-error.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ShellRoutes } from '../../shell.routes';
 
 @Component({
   selector: 'app-support-error',
   templateUrl: './support-error.page.html',
-  styleUrls: ['./support-error.page.scss']
+  styleUrls: ['./support-error.page.scss'],
 })
 export class SupportErrorPage implements OnInit {
+  public constructor(private route: ActivatedRoute, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public onBack(): void {
+    this.navigateToRoot();
   }
 
+  public ngOnInit(): void {}
+
+  private navigateToRoot(): void {
+    this.router.navigate([ShellRoutes.MODULE_PATH]);
+  }
 }
--- workspace/apps/pidp/src/app/features/shell/shell.module.ts
+++ workspace/apps/pidp/src/app/features/shell/shell.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { DashboardModule } from '@bcgov/shared/ui';
 
+import { SharedModule } from '@app/shared/shared.module';
+
 import { PortalDashboardComponent } from './components/portal-dashboard/portal-dashboard.component';
-import { ShellRoutingModule } from './shell-routing.module';
 import { SupportErrorPage } from './pages/support-error/support-error.page';
+import { ShellRoutingModule } from './shell-routing.module';
 
 @NgModule({
   declarations: [PortalDashboardComponent, SupportErrorPage],
-  imports: [ShellRoutingModule, DashboardModule, CommonModule],
+  imports: [ShellRoutingModule, DashboardModule, CommonModule, SharedModule],
 })
 export class ShellModule {}
