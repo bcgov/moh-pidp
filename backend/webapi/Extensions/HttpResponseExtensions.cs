e1451d90876f086e0d3257a60d57945305bf50a1
James <jameshollinger.ee@gmail.com>
2022-04-18 15:57:28 -0700

    small extension update

--- backend/webapi/Extensions/HttpResponseExtensions.cs
+++ backend/webapi/Extensions/HttpResponseExtensions.cs
@@ -3,15 +3,19 @@ namespace Pidp.Extensions;
 public static class HttpResponseExtensions
 {
     /// <summary>
-    /// Adds the given key + value to the Headers if both the key and value are not null or whitespace.
+    /// Adds the given key + value to the Headers if both the key and value are not null or whitespace and the Header does not already exist.
     /// </summary>
-    public static void SafeAddHeader(this HttpResponse response, string? key, string? value)
+    /// <param name="response"></param>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    /// <returns>true if the header was successfully added, false otherwise.</returns>
+    public static bool SafeAddHeader(this HttpResponse response, string? key, string? value)
     {
         if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(value))
         {
-            return;
+            return false;
         }
 
-        response.Headers.Add(key, value);
+        return response.Headers.TryAdd(key, value);
     }
 }
