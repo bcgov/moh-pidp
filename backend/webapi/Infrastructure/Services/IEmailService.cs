58fbe06c5b2f5b0e83bad1182cb8de4c4f4aa24a
James <jameshollinger.ee@gmail.com>
2022-04-20 10:24:56 -0700

    update SA eForms validation and emailing

--- backend/webapi/Infrastructure/Services/IEmailService.cs
+++ backend/webapi/Infrastructure/Services/IEmailService.cs
@@ -5,6 +5,5 @@ using Pidp.Infrastructure.HttpClients.Mail;
 public interface IEmailService
 {
     Task SendAsync(Email email);
-    Task SendSaEformsAccessRequestConfirmationAsync(int partyId);
     Task<int> UpdateEmailLogStatuses(int limit);
 }
