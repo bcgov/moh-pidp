07d362820ad36e23b4f00016e99da13c3d383315
Martin Pultz <mtpultz@gmail.com>
2022-05-02 15:00:12 -0700

    Added administrator information card, section, module, page, resource, form state, and model

--- workspace/apps/pidp/src/app/features/organization-info/organization-info-routing.module.ts
+++ workspace/apps/pidp/src/app/features/organization-info/organization-info-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { OrganizationInfoRoutes } from './organization-info.routes';
+import { AdministratorInformationModule } from './pages/administrator-information/administrator-information.module';
 import { FacilityDetailsModule } from './pages/facility-details/facility-details.module';
 import { OrganizationDetailsModule } from './pages/organization-details/organization-details.module';
 
@@ -19,6 +20,13 @@ const routes: Routes = [
       import('./pages/facility-details/facility-details.module').then(
         (m) => m.FacilityDetailsModule
       ),
+  },
+  {
+    path: OrganizationInfoRoutes.ADMINISTRATOR_INFO,
+    loadChildren: (): Promise<AdministratorInformationModule> =>
+      import(
+        './pages/administrator-information/administrator-information.module'
+      ).then((m) => m.AdministratorInformationModule),
   },
 ];
 
