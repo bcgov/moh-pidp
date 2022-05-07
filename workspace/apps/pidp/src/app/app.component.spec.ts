3c1d48dbd3cafe567e2ec110ad2de150caaef084
Panos <paahndev@gmail.com>
2022-05-06 18:47:32 -0700

    update

--- workspace/apps/pidp/src/app/app.component.spec.ts
+++ workspace/apps/pidp/src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { TestBed } from '@angular/core/testing';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, RouterEvent } from '@angular/router';
+import { ActivatedRoute, Data, NavigationEnd } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { Subject } from 'rxjs';
+import { of } from 'rxjs';
 
-import { randTextRange } from '@ngneat/falso';
-import { Spy, createSpyFromClass, provideAutoSpy } from 'jest-auto-spies';
+import { randText } from '@ngneat/falso';
+import { Spy, provideAutoSpy } from 'jest-auto-spies';
 
 import { AppComponent } from './app.component';
 import { RouteStateService } from './core/services/route-state.service';
@@ -16,28 +16,18 @@ import { UtilsService } from './core/services/utils.service';
 describe('AppComponent', () => {
   let component: AppComponent;
   let titleServiceSpy: Spy<Title>;
-  // let routeStateServiceSpy: Spy<RouteStateService>;
-  const routerEventsSubject = new Subject<RouterEvent>();
-  const navigationEnd = new NavigationEnd(1, '', '');
-  // let router: Router;
+  let routeStateServiceSpy: Spy<RouteStateService>;
 
-  let mockActivatedRoute: { snapshot: any };
+  let mockActivatedRoute: { data: any; snapshot: any };
 
   beforeEach(() => {
-    mockActivatedRoute = {
-      snapshot: {
-        data: {
-          title: randTextRange({ min: 1, max: 4 }),
-          routes: {
-            root: '../../',
-          },
-        },
-      },
+    const data: Data = {
+      title: randText(),
     };
-
-    titleServiceSpy = createSpyFromClass(Title, {
-      methodsToSpyOn: ['getTitle', 'setTitle'],
-    });
+    mockActivatedRoute = {
+      data: of(data),
+      snapshot: { data },
+    };
 
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -47,33 +37,30 @@ describe('AppComponent', () => {
           provide: ActivatedRoute,
           useValue: mockActivatedRoute,
         },
-        {
-          provide: Title,
-          useValue: titleServiceSpy,
-        },
         provideAutoSpy(RouteStateService),
         provideAutoSpy(UtilsService),
-        // provideAutoSpy(Router),
+        provideAutoSpy(Title),
       ],
     });
 
     component = TestBed.inject(AppComponent);
     titleServiceSpy = TestBed.inject<any>(Title);
-    // routeStateServiceSpy = TestBed.inject<any>(RouteStateService);
-    // router = TestBed.inject(Router);
+    routeStateServiceSpy = TestBed.inject<any>(RouteStateService);
   });
 
   describe('INIT', () => {
-    given('component initializes', () => {
-      component.ngOnInit();
-      when('navigation ends and set title has been called', () => {
-        // routeStateServiceSpy.accessorSpies.getters.onNavigationStart;
-        // routeStateServiceSpy.accessorSpies.getters.onNavigationEnd;
-        routerEventsSubject.next(navigationEnd);
+    given('there is an initial route', () => {
+      const navigationEnd = new NavigationEnd(0, '/', '/');
+      routeStateServiceSpy.onNavigationEnd.nextOneTimeWith(navigationEnd);
 
-        then('should have the correct title', () => {
+      when('the component has been initialized', () => {
+        component.ngOnInit();
+
+        then('the title should be set using the route config', () => {
           expect(titleServiceSpy.setTitle).toHaveBeenCalledTimes(1);
-          expect(titleServiceSpy.getTitle).toReturnWith(component.title);
+          expect(titleServiceSpy.setTitle).toBeCalledWith(
+            mockActivatedRoute.snapshot.data.title
+          );
         });
       });
     });
