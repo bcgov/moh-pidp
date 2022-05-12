7e601a00d83ab93189adba34c08d06b77d9d1072
Panos <paahndev@gmail.com>
2022-05-12 13:05:26 -0700

    make public endpoint

--- backend/webapi/Features/ClientLogs/ClientLogsController.cs
+++ backend/webapi/Features/ClientLogs/ClientLogsController.cs
@@ -1,11 +1,13 @@
 namespace Pidp.Features.ClientLogs;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("api/[controller]")]
 public class ClientLogsController
 {
     [HttpPost]
+    [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task CreateLog([FromServices] ICommandHandler<Create.Command> handler,
