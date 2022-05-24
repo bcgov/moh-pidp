c99913f1e1e32cb8c538f04ea8c7a5a59a6297db
Panos <paahndev@gmail.com>
2022-05-24 16:53:28 -0700

    frontend tests

--- workspace/apps/pidp/src/app/features/access/pages/endorsement/endorsement.page.spec.ts
+++ workspace/apps/pidp/src/app/features/access/pages/endorsement/endorsement.page.spec.ts
@@ -1,25 +1,120 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { randNumber, randTextRange } from '@ngneat/falso';
+import { Spy, createSpyFromClass, provideAutoSpy } from 'jest-auto-spies';
+
+import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+import { PartyService } from '@app/core/party/party.service';
+import { FormUtilsService } from '@app/core/services/form-utils.service';
+import { LoggerService } from '@app/core/services/logger.service';
+
+import { EndorsementResource } from './endorsement-resource.service';
 import { EndorsementPage } from './endorsement.page';
 
 describe('EndorsementPage', () => {
   let component: EndorsementPage;
-  let fixture: ComponentFixture<EndorsementPage>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EndorsementPage ]
-    })
-    .compileComponents();
-  });
+  let partyServiceSpy: Spy<PartyService>;
+  let mockActivatedRoute: { snapshot: any };
+  let router: Router;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EndorsementPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockActivatedRoute = {
+      snapshot: {
+        data: {
+          title: randTextRange({ min: 1, max: 4 }),
+          routes: {
+            root: '../../',
+          },
+        },
+      },
+    };
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        EndorsementPage,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+        {
+          provide: PartyService,
+          useValue: createSpyFromClass(PartyService, {
+            gettersToSpyOn: ['partyId'],
+            settersToSpyOn: ['partyId'],
+          }),
+        },
+        provideAutoSpy(EndorsementResource),
+        provideAutoSpy(FormUtilsService),
+        provideAutoSpy(LoggerService),
+        provideAutoSpy(Router),
+      ],
+    });
+
+    router = TestBed.inject(Router);
+    component = TestBed.inject(EndorsementPage);
+    partyServiceSpy = TestBed.inject<any>(PartyService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('INIT', () => {
+    given('partyId does not exist', () => {
+      const partyId = null;
+      partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
+
+      when('component has initialized', () => {
+        component.ngOnInit();
+
+        then('router should navigate to root route', () => {
+          const rootRoute = mockActivatedRoute.snapshot.data.routes.root;
+          expect(router.navigate).toHaveBeenCalledWith([rootRoute]);
+        });
+      });
+    });
+
+    given(
+      'partyId exists and endorsement enrolment has not yet been completed',
+      () => {
+        const partyId = randNumber({ min: 1 });
+        partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
+        component.completed = null;
+
+        when('component has initialized', () => {
+          component.ngOnInit();
+
+          then('router should navigate to root route', () => {
+            const rootRoute = mockActivatedRoute.snapshot.data.routes.root;
+            expect(router.navigate).toHaveBeenCalledWith([rootRoute]);
+          });
+        });
+      }
+    );
+  });
+
+  describe('METHOD: onBack', () => {
+    given('user wants to go back to the previous page', () => {
+      when('onBack is invoked', () => {
+        component.onBack();
+
+        then('router should navigate to root route', () => {
+          const rootRoute = mockActivatedRoute.snapshot.data.routes.root;
+          expect(router.navigate).toHaveBeenCalledWith([rootRoute]);
+        });
+      });
+    });
   });
 });
