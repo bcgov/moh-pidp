f8a3ad6bb8bc9c91a7bafcaf99c8fd7e39ffe2f1
Panos <paahndev@gmail.com>
2022-06-06 15:56:21 -0700

    tests pass

--- workspace/apps/pidp/src/app/core/services/logger.service.spec.ts
+++ workspace/apps/pidp/src/app/core/services/logger.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
+import { provideAutoSpy } from 'jest-auto-spies';
+
 import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
 
 import { LoggerService } from './logger.service';
@@ -15,6 +17,7 @@ describe('LoggerService', () => {
           provide: APP_CONFIG,
           useValue: APP_DI_CONFIG,
         },
+        provideAutoSpy(LoggerService),
       ],
     });
 
--- workspace/apps/pidp/src/app/features/history/pages/view-document/view-document.directive.spec.ts
+++ workspace/apps/pidp/src/app/features/history/pages/view-document/view-document.directive.spec.ts
@@ -1,8 +1,29 @@
+import { TemplateRef, ViewContainerRef } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
 import { ViewDocumentDirective } from './view-document.directive';
 
 describe('ViewDocumentDirective', () => {
+  let directive: ViewDocumentDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ViewDocumentDirective,
+        {
+          provide: ViewContainerRef,
+          useValue: {},
+        },
+        {
+          provide: TemplateRef,
+          useValue: {},
+        },
+      ],
+    });
+
+    directive = TestBed.inject(ViewDocumentDirective);
+  });
   it('should create an instance', () => {
-    const directive = new ViewDocumentDirective();
     expect(directive).toBeTruthy();
   });
 });
