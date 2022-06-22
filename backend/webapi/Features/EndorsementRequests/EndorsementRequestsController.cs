6410952a1de00b1738d80d38ff8e5d7c1fea87d9
James <jameshollinger.ee@gmail.com>
2022-06-22 15:37:14 -0700

    adjudication endpoint

--- backend/webapi/Features/EndorsementRequests/EndorsementRequestsController.cs
+++ backend/webapi/Features/EndorsementRequests/EndorsementRequestsController.cs
@@ -18,6 +18,7 @@ public class EndorsementRequestsController : PidpControllerBase
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<List<Index.Model>>> GetEndorsementRequests([FromServices] IQueryHandler<Index.Query, List<Index.Model>> handler,
                                                                               [FromRoute] Index.Query query)
         => await this.AuthorizePartyBeforeHandleAsync(query.PartyId, handler, query)
@@ -26,6 +27,7 @@ public class EndorsementRequestsController : PidpControllerBase
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> CreateEndorsementRequest([FromServices] ICommandHandler<Create.Command> handler,
                                                               [FromHybrid] Create.Command command)
         => await this.AuthorizePartyBeforeHandleAsync(command.PartyId, handler, command)
@@ -34,6 +36,7 @@ public class EndorsementRequestsController : PidpControllerBase
     [HttpGet("received")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<List<ReceivedIndex.Model>>> GetReceivedEndorsementRequests([FromServices] IQueryHandler<ReceivedIndex.Query, List<ReceivedIndex.Model>> handler,
                                                                                               [FromRoute] ReceivedIndex.Query query)
         => await this.AuthorizePartyBeforeHandleAsync(query.PartyId, handler, query)
@@ -42,8 +45,18 @@ public class EndorsementRequestsController : PidpControllerBase
     [HttpPost("received")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> ReceiveEndorsementRequest([FromServices] ICommandHandler<Receive.Command, IDomainResult> handler,
                                                                [FromHybrid] Receive.Command command)
+        => await this.AuthorizePartyBeforeHandleAsync(command.PartyId, handler, command)
+            .ToActionResult();
+
+    [HttpPut("received/{endorsementRequestId}/adjudicate")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> AdjudicateEndorsementRequest([FromServices] ICommandHandler<Adjudicate.Command, IDomainResult> handler,
+                                                                  [FromHybrid] Adjudicate.Command command)
         => await this.AuthorizePartyBeforeHandleAsync(command.PartyId, handler, command)
             .ToActionResult();
 }
