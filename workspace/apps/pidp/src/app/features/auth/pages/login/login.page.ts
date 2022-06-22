e82bd90b4daaaf4efa095eaf937d0bbdbba0938d
James <jameshollinger.ee@gmail.com>
2022-06-21 19:55:09 -0700

    fix query strings

--- workspace/apps/pidp/src/app/features/auth/pages/login/login.page.ts
+++ workspace/apps/pidp/src/app/features/auth/pages/login/login.page.ts
@@ -87,11 +87,13 @@ export class LoginPage {
   }
 
   private login(idpHint: IdentityProvider): Observable<void> {
+    const endorsementToken =
+      this.route.snapshot.queryParamMap.get('endorsement-token');
     return this.authService.login({
       idpHint: idpHint,
       redirectUri:
         this.config.applicationUrl +
-        '?guid=asdjfijasdfjasdnfiuasdhfiuasdifjweoifjaowjfoaj',
+        (endorsementToken ? `?endorsement-token=${endorsementToken}` : ''),
     });
   }
 }
