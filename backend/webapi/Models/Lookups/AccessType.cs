95a066b1d58224f0a6526662f2c73c60e3713ddf
James <jameshollinger.ee@gmail.com>
2022-07-04 17:22:54 -0700

    access request API

--- backend/webapi/Models/Lookups/AccessType.cs
+++ backend/webapi/Models/Lookups/AccessType.cs
@@ -8,7 +8,8 @@ public enum AccessTypeCode
     SAEforms = 1,
     HcimAccountTransfer,
     HcimEnrolment,
-    DriverFitness
+    DriverFitness,
+    Uci
 }
 
 // This table is only for reporting/database readability; it is not used by the FE or BE of the app.
@@ -25,9 +26,10 @@ public class AccessTypeDataGenerator : ILookupDataGenerator<AccessType>
 {
     public IEnumerable<AccessType> Generate() => new[]
     {
-        new AccessType { Code = AccessTypeCode.SAEforms,            Name = "Special Authority eForms"},
-        new AccessType { Code = AccessTypeCode.HcimAccountTransfer, Name = "HCIMWeb Account Transfer"},
-        new AccessType { Code = AccessTypeCode.HcimEnrolment,       Name = "HCIMWeb Enrolment"       },
-        new AccessType { Code = AccessTypeCode.DriverFitness,       Name = "Driver Medical Fitness"  },
+        new AccessType { Code = AccessTypeCode.SAEforms,            Name = "Special Authority eForms"      },
+        new AccessType { Code = AccessTypeCode.HcimAccountTransfer, Name = "HCIMWeb Account Transfer"      },
+        new AccessType { Code = AccessTypeCode.HcimEnrolment,       Name = "HCIMWeb Enrolment"             },
+        new AccessType { Code = AccessTypeCode.DriverFitness,       Name = "Driver Medical Fitness"        },
+        new AccessType { Code = AccessTypeCode.Uci,                 Name = "Unifying Clinical Information" },
     };
 }
