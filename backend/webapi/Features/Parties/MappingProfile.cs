a5b8b697e1974cba92c0d4e6b2665efe8121234f
James <jameshollinger.ee@gmail.com>
2022-07-21 16:18:01 -0700

    update

--- backend/webapi/Features/Parties/MappingProfile.cs
+++ backend/webapi/Features/Parties/MappingProfile.cs
@@ -1,6 +1,7 @@
 namespace Pidp.Features.Parties;
 
 using AutoMapper;
+using NodaTime;
 using static NodaTime.Extensions.DateTimeExtensions;
 
 using Pidp.Infrastructure.HttpClients.Plr;
@@ -12,7 +13,6 @@ public class MappingProfile : Profile
     {
         this.CreateProjection<Party, Demographics.Command>();
         this.CreateProjection<Party, ProfileStatus.ProfileStatusDto>()
-            // .IncludeMembers(party => party.LicenceDeclaration)
             .ForMember(dest => dest.CompletedEnrolments, opt => opt.MapFrom(src => src.AccessRequests.Select(x => x.AccessTypeCode)))
             .ForMember(dest => dest.OrganizationDetailEntered, opt => opt.MapFrom(src => src.OrgainizationDetail != null))
             .ForMember(dest => dest.PlrGoodStanding, opt => opt.Ignore())
@@ -28,12 +28,7 @@ public class MappingProfile : Profile
         this.CreateProjection<PartyOrgainizationDetail, OrganizationDetails.Command>();
 
         this.CreateMap<PlrRecord, CollegeCertifications.Model>()
-            .AfterMap((record, model) =>
-            {
-                model.IsGoodStanding = record.IsGoodStanding();
-                model.StatusStartDate = record.StatusStartDate.HasValue
-                    ? record.StatusStartDate.Value.ToLocalDateTime().Date
-                    : null;
-            });
+            .ForMember(dest => dest.IsGoodStanding, opt => opt.MapFrom(src => src.IsGoodStanding()))
+            .ForMember(dest => dest.StatusStartDate, opt => opt.MapFrom(src => src.StatusStartDate != null ? src.StatusStartDate.Value.ToLocalDateTime().Date : (LocalDate?)null));
     }
 }
