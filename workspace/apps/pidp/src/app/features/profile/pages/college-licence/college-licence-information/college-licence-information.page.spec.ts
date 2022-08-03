1b161320636a9b85dd97c115f4e626ae0b84b72c
Panos <paahndev@gmail.com>
2022-08-03 15:52:12 -0700

    tests

--- workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information.page.spec.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information.page.spec.ts
@@ -1,25 +1,82 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { randTextRange } from '@ngneat/falso';
+import { createSpyFromClass, provideAutoSpy } from 'jest-auto-spies';
+
+import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+import { PartyService } from '@app/core/party/party.service';
+import { LoggerService } from '@app/core/services/logger.service';
+
+import { CollegeLicenceInformationResource } from './college-licence-information-resource.service';
 import { CollegeLicenceInformationPage } from './college-licence-information.page';
 
 describe('CollegeLicenceInformationPage', () => {
   let component: CollegeLicenceInformationPage;
-  let fixture: ComponentFixture<CollegeLicenceInformationPage>;
+  let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CollegeLicenceInformationPage ]
-    })
-    .compileComponents();
-  });
+  let mockActivatedRoute: { snapshot: any };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CollegeLicenceInformationPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockActivatedRoute = {
+      snapshot: {
+        data: {
+          title: randTextRange({ min: 1, max: 4 }),
+          routes: {
+            root: '../../',
+          },
+        },
+      },
+    };
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        CollegeLicenceInformationPage,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+        {
+          provide: PartyService,
+          useValue: createSpyFromClass(PartyService, {
+            gettersToSpyOn: ['partyId'],
+            settersToSpyOn: ['partyId'],
+          }),
+        },
+        provideAutoSpy(Router),
+        provideAutoSpy(LoggerService),
+        provideAutoSpy(CollegeLicenceInformationResource),
+      ],
+    });
+    router = TestBed.inject(Router);
+    component = TestBed.inject(CollegeLicenceInformationPage);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('METHOD: onBack', () => {
+    given('user wants to go back to the previous page', () => {
+      when('onBack is invoked', () => {
+        component.onBack();
+
+        then('router should navigate to root route', () => {
+          const rootRoute = mockActivatedRoute.snapshot.data.routes.root;
+          expect(router.navigate).toHaveBeenCalledWith([rootRoute]);
+        });
+      });
+    });
   });
 });
