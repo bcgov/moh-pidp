554d4d256c3ad990c0afec0939eeef7d8cf038dc
Panos <paahndev@gmail.com>
2022-08-18 18:16:43 -0700

    button to delete parties on admin

--- workspace/apps/pidp/src/app/features/admin/pages/parties/parties.page.ts
+++ workspace/apps/pidp/src/app/features/admin/pages/parties/parties.page.ts
@@ -1,6 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
+
+import { EMPTY, exhaustMap } from 'rxjs';
+
+import {
+  ConfirmDialogComponent,
+  DialogOptions,
+  HtmlComponent,
+} from '@bcgov/shared/ui';
+
+import { APP_CONFIG, AppConfig } from '@app/app.config';
 
 import {
   AdminResource,
@@ -21,13 +32,36 @@ export class PartiesPage implements OnInit {
     'providerCollegeCode',
     'saEforms',
   ];
+  public environment: string;
 
   public constructor(
+    @Inject(APP_CONFIG) private config: AppConfig,
     private adminResource: AdminResource,
+    private dialog: MatDialog,
     route: ActivatedRoute
   ) {
     this.title = route.snapshot.data.title;
     this.dataSource = new MatTableDataSource();
+    this.environment = this.config.environmentName;
+  }
+
+  public onDelete(): void {
+    const data: DialogOptions = {
+      title: 'Delete all parties',
+      component: HtmlComponent,
+      data: {
+        content: 'You are about to delete all parties. Continue?',
+      },
+    };
+    this.dialog
+      .open(ConfirmDialogComponent, { data })
+      .afterClosed()
+      .pipe(
+        exhaustMap((result) =>
+          result ? this.adminResource.deleteParties() : EMPTY
+        )
+      )
+      .subscribe();
   }
 
   public ngOnInit(): void {
