976add5d1c6a37afc953e22326053889a37de773
Panos <paahndev@gmail.com>
2022-09-16 13:48:52 -0700

    ui updates

--- workspace/apps/pidp/src/app/features/access/pages/ms-teams/clinic-member-form.component.ts
+++ workspace/apps/pidp/src/app/features/access/pages/ms-teams/clinic-member-form.component.ts
@@ -14,31 +14,48 @@ import { MsTeamsFormState } from './ms-teams-form-state';
 @Component({
   selector: 'app-clinic-member-form',
   template: `
-    <ng-container [formGroup]="form">
-      <mat-form-field class="w-100">
-        <input matInput placeholder="Name" formControlName="name" />
-        <mat-error>Required</mat-error>
-      </mat-form-field>
-      <mat-form-field class="w-100">
-        <input matInput placeholder="Job Title" formControlName="jobTitle" />
-        <mat-error>Required</mat-error>
-      </mat-form-field>
-      <mat-form-field class="w-100">
-        <input matInput placeholder="Email" formControlName="email" />
-        <mat-error>Required</mat-error>
-      </mat-form-field>
-      <mat-form-field class="w-100">
-        <input
-          matInput
-          placeholder="Phone Number"
-          formControlName="phone"
-          mask="(000) 000-0000"
-          [showMaskTyped]="false" />
-        <mat-error>Required</mat-error>
-      </mat-form-field>
-    </ng-container>
+    <div class="clinic-member-form">
+      <ng-container [formGroup]="form">
+        <mat-icon class="material-icons-sharp">account_circle</mat-icon>
+        <mat-form-field class="w-100 form-field" appearance="fill">
+          <input matInput placeholder="Name" formControlName="name" />
+          <mat-error>Required</mat-error>
+        </mat-form-field>
+        <mat-form-field class="w-100 form-field" appearance="fill">
+          <input matInput placeholder="Job Title" formControlName="jobTitle" />
+          <mat-error>Required</mat-error>
+        </mat-form-field>
+        <mat-form-field class="w-100 form-field" appearance="fill">
+          <input matInput placeholder="Email" formControlName="email" />
+          <mat-error>Required</mat-error>
+        </mat-form-field>
+        <mat-form-field class="w-100 form-field" appearance="fill">
+          <input
+            matInput
+            placeholder="Phone Number"
+            formControlName="phone"
+            mask="(000) 000-0000"
+            [showMaskTyped]="false" />
+          <mat-error>Required</mat-error>
+        </mat-form-field>
+      </ng-container>
+    </div>
   `,
-  styles: [],
+  styles: [
+    `
+      .clinic-member-form {
+        display: flex !important;
+      }
+
+      .form-field {
+        margin: 5px;
+      }
+
+      mat-icon {
+        align-self: center;
+      }
+    `,
+  ],
 })
 export class ClinicMemberFormComponent
   extends AbstractFormPage
