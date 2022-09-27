b7d97e46e9dd9471a5f1ff9510d1a974b398c39f
James <jameshollinger.ee@gmail.com>
2022-09-27 13:44:44 -0700

    WIP component

--- workspace/libs/shared/ui/src/lib/components/email-domain-warning/email-domain-warning.component.ts
+++ workspace/libs/shared/ui/src/lib/components/email-domain-warning/email-domain-warning.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'ui-email-domain-warning',
   templateUrl: './email-domain-warning.component.html',
-  styleUrls: ['./email-domain-warning.component.scss']
+  styleUrls: ['./email-domain-warning.component.scss'],
 })
-export class EmailDomainWarningComponent implements OnInit {
+export class EmailDomainWarningComponent implements OnChanges {
+  @Input() public inputEmail: string;
+  @Input() public commonDomains: string[];
 
-  constructor() { }
+  public showWarning: boolean;
 
-  ngOnInit(): void {
+  public constructor() {
+    this.inputEmail = '';
+    this.commonDomains = [];
+    this.showWarning = false;
   }
 
+  public ngOnChanges(): void {
+    const inputDomain = this.inputEmail?.split('@').pop() ?? '';
+    this.showWarning = !this.commonDomains.includes(inputDomain);
+  }
 }
