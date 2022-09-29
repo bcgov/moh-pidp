f671e6f2d7a59f92da2b28640a20d9ddb3c207c3
James <jameshollinger.ee@gmail.com>
2022-09-28 18:07:20 -0700

    fix tests

--- backend/webapi.tests/Features/Lookups/CommonEmailDomains.cs
+++ backend/webapi.tests/Features/Lookups/CommonEmailDomains.cs
@@ -5,11 +5,16 @@ using Xunit;
 using Pidp.Features.Lookups;
 using Pidp.Models;
 using PidpTests.TestingExtensions;
+using DomainResults.Common;
 
 public class CommonEmailDomainsTests : InMemoryDbTest
 {
-    [Fact]
-    public async void GetCommonEmailDomains_SingleOccurrances_Empty()
+    [Theory]
+    [InlineData("1@one.com")]
+    [InlineData("2@one.ca")]
+    [InlineData("3@three.gov.bc.ca")]
+    [InlineData("4@four.net")]
+    public async void FindCommonEmailDomains_SingleOccurrances_False(string email)
     {
         this.TestDb.HasSome(new[]
         {
@@ -18,19 +23,20 @@ public class CommonEmailDomainsTests : InMemoryDbTest
             new Party { Email = "3@three.gov.bc.ca" },
             new Party { Email = "4@four.net" },
         });
-
+        var query = new CommonEmailDomains.Query { Email = email };
         var handler = this.MockDependenciesFor<CommonEmailDomains.QueryHandler>();
 
-        var result = await handler.HandleAsync(new CommonEmailDomains.Query());
+        var result = await handler.HandleAsync(query);
 
-        Assert.NotNull(result);
-        Assert.Empty(result);
+        Assert.Equal(DomainOperationStatus.NotFound, result.Status);
     }
 
-    [Fact]
-    public async void GetCommonEmailDomains_TwoOrMoreOccurrances_InResult()
+    [Theory]
+    [InlineData("test1@one.com", DomainOperationStatus.NotFound)]
+    [InlineData("test2@two.com", DomainOperationStatus.Success)]
+    [InlineData("test3@three.com", DomainOperationStatus.Success)]
+    public async void FindCommonEmailDomains_TwoOrMoreOccurrances_True(string email, DomainOperationStatus expected)
     {
-        var expected = new[] { "two.com", "three.com" };
         this.TestDb.HasSome(new[]
         {
             new Party { Email = "1@one.com" },
@@ -40,19 +46,22 @@ public class CommonEmailDomainsTests : InMemoryDbTest
             new Party { Email = "5@three.com" },
             new Party { Email = "6@three.com" },
         });
-
+        var query = new CommonEmailDomains.Query { Email = email };
         var handler = this.MockDependenciesFor<CommonEmailDomains.QueryHandler>();
 
-        var result = await handler.HandleAsync(new CommonEmailDomains.Query());
+        var result = await handler.HandleAsync(query);
 
-        Assert.NotNull(result);
-        Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        Assert.Equal(expected, result.Status);
     }
 
-    [Fact]
-    public async void GetCommonEmailDomains_DifferentCases_InResult()
+    [Theory]
+    [InlineData("test1@one.com", DomainOperationStatus.NotFound)]
+    [InlineData("test2@two.com", DomainOperationStatus.Success)]
+    [InlineData("test3@TwO.COm", DomainOperationStatus.Success)]
+    [InlineData("test4@three.com", DomainOperationStatus.Success)]
+    [InlineData("test5@THREE.coM", DomainOperationStatus.Success)]
+    public async void FindCommonEmailDomains_DifferentCases_True(string email, DomainOperationStatus expected)
     {
-        var expected = new[] { "two.com", "three.com" };
         this.TestDb.HasSome(new[]
         {
             new Party { Email = "1@one.com" },
@@ -62,12 +71,27 @@ public class CommonEmailDomainsTests : InMemoryDbTest
             new Party { Email = "5@Three.Com" },
             new Party { Email = "6@THREE.COM" },
         });
-
+        var query = new CommonEmailDomains.Query { Email = email };
         var handler = this.MockDependenciesFor<CommonEmailDomains.QueryHandler>();
 
-        var result = await handler.HandleAsync(new CommonEmailDomains.Query());
+        var result = await handler.HandleAsync(query);
 
-        Assert.NotNull(result);
-        Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        Assert.Equal(expected, result.Status);
+    }
+
+    [Fact]
+    public async void FindCommonEmailDomains_DomainOnly_True()
+    {
+        this.TestDb.HasSome(new[]
+        {
+            new Party { Email = "1@email.com" },
+            new Party { Email = "2@email.com" },
+        });
+        var query = new CommonEmailDomains.Query { Email = "email.com" };
+        var handler = this.MockDependenciesFor<CommonEmailDomains.QueryHandler>();
+
+        var result = await handler.HandleAsync(query);
+
+        Assert.Equal(DomainOperationStatus.Success, result.Status);
     }
 }
