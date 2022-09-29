64d51036dda16d2a27d5f765eb4fc197aba2e3a0
James <jameshollinger.ee@gmail.com>
2022-09-28 18:11:32 -0700

    format

--- backend/webapi/Features/Lookups/CommonEmailDomains.cs
+++ backend/webapi/Features/Lookups/CommonEmailDomains.cs
@@ -24,15 +24,16 @@ public class CommonEmailDomains
 
         public QueryHandler(PidpDbContext context) => this.context = context;
 
-#pragma warning disable CA1304 // ToLower() is Locale Dependant
         public async Task<IDomainResult> HandleAsync(Query query)
         {
-            // If the input Email is just the domain, this should return the entire input.
-            var domain = query.Email[(query.Email.IndexOf("@") + 1)..].ToLower();
+            // If the input Email is just the domain, this should use the entire input.
+            var domain = query.Email[(query.Email.IndexOf("@", StringComparison.OrdinalIgnoreCase) + 1)..].ToLowerInvariant();
 
+#pragma warning disable CA1304 // ToLower() is Locale Dependant
             var count = await this.context.Parties
                 .Where(party => party.Email!.Substring(party.Email.IndexOf("@") + 1).ToLower() == domain)
                 .CountAsync();
+#pragma warning restore CA1304
 
             if (count > 1)
             {
@@ -43,7 +44,5 @@ public class CommonEmailDomains
                 return DomainResult.NotFound();
             }
         }
-#pragma warning restore CA1304
     }
 }
-
