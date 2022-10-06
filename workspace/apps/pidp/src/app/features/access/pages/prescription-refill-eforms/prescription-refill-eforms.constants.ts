31553adf7132d7d8be36db69d564e476464d1b4b
Panos Hatzinikolaou <45132887+Paahn@users.noreply.github.com>
2022-10-05 17:53:00 -0700

    Update text on Prescription Refill eForms (#110)
    * update
    
    * duplicate
    
    * capitalize

---
+++ workspace/apps/pidp/src/app/features/access/pages/prescription-refill-eforms/prescription-refill-eforms.constants.ts
@@ -0,0 +1,4 @@
+export const prescriptionRefillUrl = `https://www.gov.bc.ca/pharmacarespecialauthority`;
+export const prescriptionRefillEformsUrl = `https://www.eforms.healthbc.org/login`;
+export const prescriptionRefillEformsSupportEmail =
+  'satransformation@gov.bc.ca';
