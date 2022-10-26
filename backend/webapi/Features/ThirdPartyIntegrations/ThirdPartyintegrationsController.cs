d2fc6a4aa9cad9073c4dda4fe84a9ba8b422095a
James <jameshollinger.ee@gmail.com>
2022-10-25 20:38:30 -0700

    move stuff

---
+++ backend/webapi/Features/ThirdPartyIntegrations/ThirdPartyintegrationsController.cs
@@ -0,0 +1,21 @@
+namespace Pidp.Features.ThirdPartyIntegrations;
+
+using DomainResults.Common;
+using DomainResults.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using Pidp.Infrastructure.Auth;
+
+[Route("api/etx")]
+public class ThirdPartyintegrationsController : ControllerBase
+{
+    [HttpGet("parties/{hpdid}/endorsements")]
+    [Authorize(Roles = Roles.ViewEndorsements)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<List<EndorsementData.Model>>> GetEndorsemetData([FromServices] IQueryHandler<EndorsementData.Query, IDomainResult<List<EndorsementData.Model>>> handler,
+                                                                                   [FromRoute] EndorsementData.Query query)
+        => await handler.HandleAsync(query)
+            .ToActionResultOfT();
+}
