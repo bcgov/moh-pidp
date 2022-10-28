9dc7ac6ca6817b03ee948e556d47e69ba9a33e28
Andrew Ang <Andrew.Ang@newvaluegroup.com>
2022-10-28 16:17:28 -0400

    378. Landing v2 admin

--- workspace/apps/pidp/src/app/features/auth/auth-routing.module.ts
+++ workspace/apps/pidp/src/app/features/auth/auth-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AuthRoutes } from './auth.routes';
 import { AuthorizationRedirectGuard } from './guards/authorization-redirect.guard';
+import { LoginPageV2Component } from './pages/login-v2/login.page.component';
 import { LoginPage } from './pages/login/login.page';
 
 const routes: Routes = [
@@ -18,9 +19,34 @@ const routes: Routes = [
     },
   },
   {
+    path: 'loginv2',
+    canActivate: [AuthorizationRedirectGuard],
+    component: LoginPageV2Component,
+    data: {
+      loginPageData: {
+        title: 'Provider Identity Portal',
+        isAdminLogin: false,
+      },
+    },
+  },
+  {
     path: AuthRoutes.ADMIN_LOGIN,
     canActivate: [AuthorizationRedirectGuard],
     component: LoginPage,
+    data: {
+      loginPageData: {
+        title: 'Provider Identity Portal',
+        isAdminLogin: true,
+      },
+      routes: {
+        auth: AuthRoutes.routePath(AuthRoutes.ADMIN_LOGIN),
+      },
+    },
+  },
+  {
+    path: 'adminv2',
+    canActivate: [AuthorizationRedirectGuard],
+    component: LoginPageV2Component,
     data: {
       loginPageData: {
         title: 'Provider Identity Portal',
--- workspace/apps/pidp/src/app/features/auth/auth.module.ts
+++ workspace/apps/pidp/src/app/features/auth/auth.module.ts
@@ -6,11 +6,12 @@ import { DashboardModule } from '@bcgov/shared/ui';
 import { SharedModule } from '@shared/shared.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageV2Component } from './pages/login-v2/login.page.component';
 import { LoginPage } from './pages/login/login.page';
 import { SystemCardComponent } from './pages/system-card/system-card.page';
 
 @NgModule({
-  declarations: [LoginPage, SystemCardComponent],
+  declarations: [LoginPage, LoginPageV2Component, SystemCardComponent],
   imports: [AuthRoutingModule, DashboardModule, SharedModule, LayoutModule],
 })
 export class AuthModule {}
