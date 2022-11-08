bcc09f3139d31cbf53e3df63bfc15a0e2b4a93c7
Andrew Ang <Andrew.Ang@newvaluegroup.com>
2022-11-08 09:28:16 -0500

    397. Replace college licence info with v2

--- workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information.module.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
 
 import { CollegeLicenceInformationRoutingModule } from './college-licence-information-routing.module';
-import { CollegeLicenceInformationV2Page } from './college-licence-information-v2.page';
 import { CollegeLicenceInformationPage } from './college-licence-information.page';
 import { CollegeLicenceInformationDetailComponent } from './components/college-licence-information-detail.component';
 
 @NgModule({
   declarations: [
     CollegeLicenceInformationPage,
-    CollegeLicenceInformationV2Page,
     CollegeLicenceInformationDetailComponent,
   ],
   imports: [SharedModule, CollegeLicenceInformationRoutingModule],
