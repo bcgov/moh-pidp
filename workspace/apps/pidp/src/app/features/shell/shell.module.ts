c5a93e1865d2afd914667f60f0f73cf2f4fe9ea6
Andrew Ang <Andrew.Ang@newvaluegroup.com>
2022-11-29 10:03:46 -0500

    449 Added DataModelModule and PresentationModule

--- workspace/apps/pidp/src/app/features/shell/shell.module.ts
+++ workspace/apps/pidp/src/app/features/shell/shell.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { PidpDataModelModule } from '@pidp/data-model';
+
 import { DashboardModule } from '@bcgov/shared/ui';
 
 import { PortalDashboardComponent } from './components/portal-dashboard/portal-dashboard.component';
@@ -8,6 +10,11 @@ import { ShellRoutingModule } from './shell-routing.module';
 
 @NgModule({
   declarations: [PortalDashboardComponent],
-  imports: [ShellRoutingModule, DashboardModule, CommonModule],
+  imports: [
+    ShellRoutingModule,
+    DashboardModule,
+    CommonModule,
+    PidpDataModelModule,
+  ],
 })
 export class ShellModule {}
