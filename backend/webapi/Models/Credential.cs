8863e66dc71ad7d1601e5c794baee758993de380
Panos <paahndev@gmail.com>
2022-11-30 16:58:06 -0800

    model and table

---
+++ backend/webapi/Models/Credential.cs
@@ -0,0 +1,28 @@
+namespace Pidp.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public enum CredentialType
+{
+    Bcsc = 1,
+    BcProvider,
+    Idir,
+    HealthAuthority
+
+}
+
+[Table(nameof(Credential))]
+public abstract class Credential : BaseAuditable
+{
+    [Key]
+    public int Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string IdpId { get; set; } = string.Empty;
+
+    public int PartyId { get; set; }
+
+    public CredentialType CredentialType { get; set; }
+}
