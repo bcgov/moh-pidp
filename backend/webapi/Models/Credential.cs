4648d75752b5895d8f240050d30e9808c1b07af6
Panos <paahndev@gmail.com>
2022-11-30 17:15:35 -0800

    nullable Party navigation property

--- backend/webapi/Models/Credential.cs
+++ backend/webapi/Models/Credential.cs
@@ -13,6 +13,7 @@ public class Credential : BaseAuditable
     public int Id { get; set; }
 
     public int PartyId { get; set; }
+    public Party? Party { get; set; }
 
     public Party? Party { get; set; }
 
