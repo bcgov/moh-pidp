4650a97ad621fe5fbeb7bb6eed8df7db77012f18
Panos <paahndev@gmail.com>
2022-12-02 18:10:59 -0800

    migrations

---
+++ backend/webapi/Data/Migrations/20221203013655_MultipleCredentials.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Pidp.Data.Migrations
+{
+    public partial class MultipleCredentials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Credential",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IdpId = table.Column<string>(type: "text", nullable: false),
+                    PartyId = table.Column<int>(type: "integer", nullable: false),
+                    CredentialType = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<Instant>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<Instant>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credential", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Credential_Party_PartyId",
+                        column: x => x.PartyId,
+                        principalTable: "Party",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Credential_PartyId",
+                table: "Credential",
+                column: "PartyId");
+
+            migrationBuilder.Sql(@"
+                INSERT INTO ""Credential"" (""UserId"", ""PartyId"", ""IdpId"")
+                SELECT ""UserId"", ""Id"" AS ""PartyId"", ""Hpdid"" AS ""IdpId""
+                FROM ""Party"";
+            ");
+
+            migrationBuilder.Sql(@"
+                UPDATE ""Credential""
+                SET ""CredentialType"" = 1
+                WHERE ""IdpId"" IS NOT NULL;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Credential");
+        }
+    }
+}
