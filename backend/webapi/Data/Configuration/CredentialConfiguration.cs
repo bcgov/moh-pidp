02ae372686d05f24dceaeeb4aff653ccf6692262
Panos <paahndev@gmail.com>
2022-12-06 18:17:04 -0800

    config

--- backend/webapi/Data/Configuration/CredentialConfiguration.cs
+++ backend/webapi/Data/Configuration/CredentialConfiguration.cs
@@ -8,6 +8,9 @@ public class CredentialConfiguration : IEntityTypeConfiguration<Credential>
 {
     public virtual void Configure(EntityTypeBuilder<Credential> builder)
     {
+        builder.HasIndex(x => x.UserId)
+            .IsUnique();
+
         builder.Property(x => x.CredentialType)
             .HasDefaultValue(0);
     }
