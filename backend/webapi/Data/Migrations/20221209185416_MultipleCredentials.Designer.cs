aa63be7a035d8b602d15613a1e2a7b8476d1796a
Panos <paahndev@gmail.com>
2022-12-09 11:02:50 -0800

    migrations

--- backend/webapi/Data/Migrations/20221209185416_MultipleCredentials.Designer.cs
+++ backend/webapi/Data/Migrations/20221209185416_MultipleCredentials.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,10 @@ using Pidp.Data;
 namespace Pidp.Data.Migrations
 {
     [DbContext(typeof(PidpDbContext))]
-    partial class PidpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221209185416_MultipleCredentials")]
+    partial class MultipleCredentials
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
