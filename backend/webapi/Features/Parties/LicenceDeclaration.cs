2a15bc491d439816d82b135bd274b4ba66267d7a
James <jameshollinger.ee@gmail.com>
2023-01-05 01:06:50 -0800

    update

--- backend/webapi/Features/Parties/LicenceDeclaration.cs
+++ backend/webapi/Features/Parties/LicenceDeclaration.cs
@@ -95,13 +95,9 @@ public class LicenceDeclaration
         public async Task<IDomainResult<string?>> HandleAsync(Command command)
         {
             var party = await this.context.Parties
+                .Include(party => party.Credentials)
                 .Include(party => party.LicenceDeclaration)
                 .SingleAsync(party => party.Id == command.PartyId);
-
-            var userId = await this.context.Credentials
-                .Where(credential => credential.PartyId == command.PartyId)
-                .Select(credential => credential.UserId)
-                .SingleAsync();
 
             if (!string.IsNullOrWhiteSpace(party.Cpn))
             {
@@ -123,7 +119,7 @@ public class LicenceDeclaration
                 }
                 else
                 {
-                    await this.keycloakClient.UpdateUserCpn(userId, party.Cpn);
+                    await this.keycloakClient.UpdateUserCpn(party.PrimaryUserId, party.Cpn);
                 }
             }
 
