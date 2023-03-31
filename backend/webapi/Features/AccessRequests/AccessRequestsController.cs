837878c740b8833710c5e418e1ec5d41870788fd
James <jameshollinger.ee@gmail.com>
2023-03-30 18:55:41 -0700

    API stub

--- backend/webapi/Features/AccessRequests/AccessRequestsController.cs
+++ backend/webapi/Features/AccessRequests/AccessRequestsController.cs
@@ -83,6 +83,15 @@ public class AccessRequestsController : PidpControllerBase
         => await this.AuthorizePartyBeforeHandleAsync(command.PartyId, handler, command)
             .ToActionResult();
 
+    [HttpPost("ms-teams-clinic-member")]
+    [Authorize(Policy = Policies.HighAssuranceIdentityProvider)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateMSTeamsClinicMemberEnrolment([FromServices] ICommandHandler<MSTeamsClinicMember.Command, IDomainResult> handler,
+                                                                        [FromHybrid] MSTeamsClinicMember.Command command)
+        => await this.AuthorizePartyBeforeHandleAsync(command.PartyId, handler, command)
+            .ToActionResult();
+
     [HttpPost("ms-teams-privacy-officer")]
     [Authorize(Policy = Policies.HighAssuranceIdentityProvider)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
---
+++ backend/webapi/Features/AccessRequests/MSTeamsClinicMember.cs
@@ -0,0 +1,95 @@
+namespace Pidp.Features.AccessRequests;
+
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using DomainResults.Common;
+using FluentValidation;
+using HybridModelBinding;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Pidp.Data;
+using Pidp.Infrastructure.HttpClients.Mail;
+using Pidp.Infrastructure.HttpClients.Plr;
+using Pidp.Infrastructure.Services;
+using Pidp.Models;
+using Pidp.Models.Lookups;
+
+public class MSTeamsClinicMember
+{
+    public class Command : ICommand<IDomainResult>
+    {
+        [JsonIgnore]
+        [HybridBindProperty(Source.Route)]
+        public int PartyId { get; set; }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            this.RuleFor(x => x.PartyId).GreaterThan(0);
+        }
+    }
+
+    public class CommandHandler : ICommandHandler<Command, IDomainResult>
+    {
+        private readonly IClock clock;
+        private readonly IEmailService emailService;
+        private readonly ILogger logger;
+        private readonly IMapper mapper;
+        private readonly IPlrClient plrClient;
+        private readonly PidpDbContext context;
+
+        public CommandHandler(
+            IClock clock,
+            IEmailService emailService,
+            ILogger<CommandHandler> logger,
+            IMapper mapper,
+            IPlrClient plrClient,
+            PidpDbContext context)
+        {
+            this.clock = clock;
+            this.emailService = emailService;
+            this.logger = logger;
+            this.mapper = mapper;
+            this.plrClient = plrClient;
+            this.context = context;
+        }
+
+        public async Task<IDomainResult> HandleAsync(Command command)
+        {
+            return DomainResult.Success();
+        }
+
+        private async Task SendEnrolmentEmailAsync()
+        {
+            var email = new Email(
+                from: EmailService.PidpEmail,
+                to: "enrolment_securemessagingsupport@fraserhealth.ca",
+                subject: "New MS Teams for Clinical Use Clinic Member Enrolment",
+                body: $"<pre>{JsonSerializer.Serialize(new { }, new JsonSerializerOptions { WriteIndented = true })}</pre>"
+            );
+            await this.emailService.SendAsync(email);
+        }
+
+        private async Task SendConfirmationEmailAsync(string partyEmail, string partyName)
+        {
+            var email = new Email(
+                from: EmailService.PidpEmail,
+                to: partyEmail,
+                subject: "MS Teams for Clinical Use Enrolment Confirmation",
+                body: $"Dear {partyName},<br><br>You have been successfully enrolled for MS Teams for Clinical Use.<br>The Fraser Health mHealth team will contact you via email with account information and setup instructions. In the meantime please email <a href=\"mailto:securemessagingsupport@fraserhealth.ca\">securemessagingsupport@fraserhealth.ca</a> if you have any questions."
+            );
+            await this.emailService.SendAsync(email);
+        }
+    }
+}
+
+public static partial class MSTeamsLoggingExtensions
+{
+    [LoggerMessage(1, LogLevel.Warning, "MS Teams Privacy Officer Access Request denied due to the Party Record not meeting all prerequisites.")]
+    public static partial void LogMSTeamsPrivacyOfficerAccessRequestDenied(this ILogger logger);
+}
