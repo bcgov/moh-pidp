71c9898b005fe97589f3dcc4cbfebb342239cd4c
James <jameshollinger.ee@gmail.com>
2023-04-13 12:36:48 -0700

    fix

--- backend/webapi/Features/Endorsements/MappingProfile.cs
+++ backend/webapi/Features/Endorsements/MappingProfile.cs
@@ -9,6 +9,7 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         this.CreateProjection<MSTeamsClinic, MSTeamsPrivacyOfficers.Model>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.PrivacyOfficer!.FullName))
             .ForMember(dest => dest.ClinicId, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.ClinicName, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.ClinicAddress, opt => opt.MapFrom(src => src.Address));
