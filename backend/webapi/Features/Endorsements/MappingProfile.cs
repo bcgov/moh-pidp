6869b8f6a2fac6c0611176fda1e9eb99e8d67e49
James <jameshollinger.ee@gmail.com>
2023-04-12 17:24:48 -0700

    simplify

--- backend/webapi/Features/Endorsements/MappingProfile.cs
+++ backend/webapi/Features/Endorsements/MappingProfile.cs
@@ -9,7 +9,6 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         this.CreateProjection<MSTeamsClinic, MSTeamsPrivacyOfficers.Model>()
-            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.PrivacyOfficer!.FirstName} {src.PrivacyOfficer.LastName}"))
             .ForMember(dest => dest.ClinicId, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.ClinicName, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.ClinicAddress, opt => opt.MapFrom(src => src.Address));
