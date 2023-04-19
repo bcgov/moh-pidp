2c2a417b6ab51f321c32fc772453823429d89e95
Marc UHART <marcuhart@gmail.com>
2023-04-19 11:13:09 -0700

    The sortByKey function no longer exists

--- workspace/apps/pidp/src/app/core/services/utils.service.spec.ts
+++ workspace/apps/pidp/src/app/core/services/utils.service.spec.ts
@@ -33,19 +33,4 @@ describe('UtilsService', () => {
     const service: UtilsService = TestBed.inject(UtilsService);
     expect(service).toBeTruthy();
   });
-
-  it('should sort by a key contained within an object literal', () => {
-    const service: UtilsService = TestBed.inject(UtilsService);
-    const sortableModels: Sortable[] = [
-      { name: 'b', code: 2, weight: 1 },
-      { name: 'a', code: 1, weight: 3 },
-      { name: 'c', code: 3, weight: 2 },
-    ];
-
-    let results = sortableModels.sort(service.sortByKey<Sortable>('name'));
-    expect(results.map((r) => r.code)).toEqual([1, 2, 3]);
-
-    results = results.sort(service.sortByKey<Sortable>('weight'));
-    expect(results.map((r) => r.name)).toEqual(['b', 'c', 'a']);
-  });
 });
