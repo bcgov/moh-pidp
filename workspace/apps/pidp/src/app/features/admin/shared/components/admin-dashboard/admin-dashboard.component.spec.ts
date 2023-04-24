76de2f8475f360530824c5d3b6f11f4ae20f710f
Marc UHART <marcuhart@gmail.com>
2023-04-24 15:51:58 -0700

    AdminDashboardComponent Unit test

--- workspace/apps/pidp/src/app/features/admin/shared/components/admin-dashboard/admin-dashboard.component.spec.ts
+++ workspace/apps/pidp/src/app/features/admin/shared/components/admin-dashboard/admin-dashboard.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
-import { provideAutoSpy } from 'jest-auto-spies';
+import { Spy, provideAutoSpy } from 'jest-auto-spies';
 
 import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+import { AccessTokenParsed } from '@app/features/auth/models/access-token-parsed.model';
 import { AccessTokenService } from '@app/features/auth/services/access-token.service';
 import { AuthService } from '@app/features/auth/services/auth.service';
+import { IdentityProvider } from '@app/features/auth/enums/identity-provider.enum';
 
 import { AdminDashboardComponent } from './admin-dashboard.component';
+import { randFirstName, randFullName, randLastName, randPastDate, randUserName } from '@ngneat/falso';
 
 describe('AdminDashboardComponent', () => {
   let component: AdminDashboardComponent;
+  let accessTokenServiceSpy: Spy<AccessTokenService>;
+
+  let mockAccessTokenParsed: AccessTokenParsed;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,6 +29,30 @@ describe('AdminDashboardComponent', () => {
         provideAutoSpy(AccessTokenService),
       ],
     });
+
+    accessTokenServiceSpy = TestBed.inject<any>(AccessTokenService);
+    mockAccessTokenParsed = {
+      "allowed-origins": [],
+      acr: '',
+      aud: [],
+      auth_time: 0,
+      azp: '',
+      iss: '',
+      jti: '',
+      scope: '',
+      sub: '',
+      typ: '',
+      // User specific attributes:
+      identity_provider: IdentityProvider.IDIR,
+      email_verified: true,
+      family_name: randLastName(),
+      given_name: randFirstName(),
+      name: randFullName(),
+      preferred_username: randUserName(),
+      birthdate: randPastDate().toString(),
+    };
+
+    accessTokenServiceSpy.decodeToken.nextWith(mockAccessTokenParsed);
 
     component = TestBed.inject(AdminDashboardComponent);
   });
