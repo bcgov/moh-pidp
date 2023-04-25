15e3cf9aad9f7be7afd478b5291533330ac11b23
Marc UHART <marcuhart@gmail.com>
2023-04-25 10:51:54 -0700

    UserAccessAgreementPage unit test

--- workspace/apps/pidp/src/app/features/profile/pages/user-access-agreement/user-access-agreement.page.spec.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/user-access-agreement/user-access-agreement.page.spec.ts
@@ -2,15 +2,19 @@ import { TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { randTextRange } from '@ngneat/falso';
-import { provideAutoSpy } from 'jest-auto-spies';
+import { randFirstName, randFullName, randLastName, randPastDate, randTextRange, randUserName } from '@ngneat/falso';
+import { Spy, provideAutoSpy } from 'jest-auto-spies';
 
 import { UserAccessAgreementPage } from './user-access-agreement.page';
 import { AccessTokenService } from '@app/features/auth/services/access-token.service';
+import { AccessTokenParsed } from '@app/features/auth/models/access-token-parsed.model';
+import { IdentityProvider } from '@app/features/auth/enums/identity-provider.enum';
 
 describe('UserAccessAgreementPage', () => {
   let component: UserAccessAgreementPage;
+  let accessTokenServiceSpy: Spy<AccessTokenService>;
 
+  let mockAccessTokenParsed: AccessTokenParsed;
   let mockActivatedRoute;
 
   beforeEach(() => {
@@ -36,6 +40,30 @@ describe('UserAccessAgreementPage', () => {
         provideAutoSpy(AccessTokenService)
       ],
     });
+
+    accessTokenServiceSpy = TestBed.inject<any>(AccessTokenService);
+    mockAccessTokenParsed = {
+      "allowed-origins": [],
+      acr: '',
+      aud: [],
+      auth_time: 0,
+      azp: '',
+      iss: '',
+      jti: '',
+      scope: '',
+      sub: '',
+      typ: '',
+      // User specific attributes:
+      identity_provider: IdentityProvider.IDIR,
+      email_verified: true,
+      family_name: randLastName(),
+      given_name: randFirstName(),
+      name: randFullName(),
+      preferred_username: randUserName(),
+      birthdate: randPastDate().toString(),
+    };
+
+    accessTokenServiceSpy.decodeToken.nextWith(mockAccessTokenParsed);
 
     component = TestBed.inject(UserAccessAgreementPage);
   });
