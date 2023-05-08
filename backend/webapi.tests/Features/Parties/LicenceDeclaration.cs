7dacfb6c94da1f1f8eabcc4d0aac37c7f809ac52
James <jameshollinger.ee@gmail.com>
2023-05-08 14:48:04 -0700

    finish events for profile status

--- backend/webapi.tests/Features/Parties/LicenceDeclaration.cs
+++ backend/webapi.tests/Features/Parties/LicenceDeclaration.cs
@@ -4,6 +4,7 @@ using FakeItEasy;
 using NodaTime;
 using Xunit;
 
+using Pidp.Models.DomainEvents;
 using Pidp.Models.Lookups;
 using Pidp.Infrastructure.HttpClients.Keycloak;
 using Pidp.Infrastructure.HttpClients.Plr;
@@ -14,9 +15,9 @@ public class LicenceDeclarationTests : InMemoryDbTest
 {
     [Theory]
     [MemberData(nameof(CollegeCodeTestCases))]
-    public async void HandleAsync_LicenceFoundInGoodStanding_KeycloakUpdates(CollegeCode collegeCode)
+    public async void HandleAsync_LicenceFoundInGoodStanding_FoundEventPublished(CollegeCode collegeCode)
     {
-        var expectedCpn = "Cpn";
+        var expectedCpn = "Cpnn";
         var licenceNumber = collegeCode.ToString();
         var party = this.TestDb.HasAParty(party =>
         {
@@ -27,12 +28,9 @@ public class LicenceDeclarationTests : InMemoryDbTest
             party.Phone = "5555555555";
         });
 
-        var keycloakClient = A.Fake<IKeycloakAdministrationClient>()
-            .ReturningTrueWhenAssigingClientRoles();
-        var plrClient = A.Fake<IPlrClient>()
-            .ReturningAStatandingsDigest(true);
+        var plrClient = A.Fake<IPlrClient>();
         A.CallTo(() => plrClient.FindCpnAsync(collegeCode, licenceNumber, party.Birthdate!.Value)).Returns(expectedCpn);
-        var handler = this.MockDependenciesFor<CommandHandler>(keycloakClient, plrClient);
+        var handler = this.MockDependenciesFor<CommandHandler>(plrClient);
 
         var result = await handler.HandleAsync(new Command { PartyId = party.Id, LicenceNumber = licenceNumber, CollegeCode = collegeCode });
 
@@ -42,13 +40,16 @@ public class LicenceDeclarationTests : InMemoryDbTest
         Assert.Equal(licenceNumber, party.LicenceDeclaration.LicenceNumber);
         Assert.Equal(collegeCode, party.LicenceDeclaration.CollegeCode);
 
-        A.CallTo(() => keycloakClient.UpdateUserCpn(party.PrimaryUserId, party.Cpn)).MustHaveHappened();
-        A.CallTo(() => keycloakClient.AssignAccessRoles(party.PrimaryUserId, MohKeycloakEnrolment.PractitionerLicenceStatus)).MustHaveHappened();
+        Assert.Single(this.PublishedEvents.OfType<PlrCpnLookupFound>());
+        var foundEvent = this.PublishedEvents.OfType<PlrCpnLookupFound>().Single();
+        Assert.Equal(party.Id, foundEvent.PartyId);
+        Assert.Equal(party.PrimaryUserId, foundEvent.UserIds.Single());
+        Assert.Equal(party.Cpn, foundEvent.Cpn);
     }
 
     [Theory]
     [MemberData(nameof(CollegeCodeTestCases))]
-    public async void HandleAsync_LicenceNotFound_NoKeycloakUpdates(CollegeCode collegeCode)
+    public async void HandleAsync_LicenceNotFound_NotFoundEventPublished(CollegeCode collegeCode)
     {
         var licenceNumber = collegeCode.ToString();
         var party = this.TestDb.HasAParty(party =>
@@ -60,10 +61,9 @@ public class LicenceDeclarationTests : InMemoryDbTest
             party.Phone = "5555555555";
         });
 
-        var keycloakClient = A.Fake<IKeycloakAdministrationClient>();
         var plrClient = A.Fake<IPlrClient>();
-        A.CallTo(() => plrClient.FindCpnAsync(A<CollegeCode>._, A<string>._, A<LocalDate>._)).Returns((string?)null);
-        var handler = this.MockDependenciesFor<CommandHandler>(keycloakClient, plrClient);
+        A.CallTo(() => plrClient.FindCpnAsync(A<CollegeCode>._, A<string>._, A<LocalDate>._)).Returns<string?>(null);
+        var handler = this.MockDependenciesFor<CommandHandler>(plrClient);
 
         var result = await handler.HandleAsync(new Command { PartyId = party.Id, LicenceNumber = licenceNumber, CollegeCode = collegeCode });
 
@@ -73,42 +73,9 @@ public class LicenceDeclarationTests : InMemoryDbTest
         Assert.Equal(licenceNumber, party.LicenceDeclaration.LicenceNumber);
         Assert.Equal(collegeCode, party.LicenceDeclaration.CollegeCode);
 
-        A.CallTo(() => keycloakClient.UpdateUserCpn(A<Guid>._, A<string>._)).MustNotHaveHappened();
-        keycloakClient.AssertNoRolesAssigned();
-    }
-
-    [Theory]
-    [MemberData(nameof(CollegeCodeTestCases))]
-    public async void HandleAsync_LicenceFoundInBadStanding_CpnUpdatedButNoRoleAssigned(CollegeCode collegeCode)
-    {
-        var expectedCpn = "Cpn";
-        var licenceNumber = collegeCode.ToString();
-        var party = this.TestDb.HasAParty(party =>
-        {
-            party.FirstName = "first";
-            party.LastName = "last";
-            party.Birthdate = LocalDate.FromDateTime(DateTime.Today);
-            party.Email = "an@email.com";
-            party.Phone = "5555555555";
-        });
-
-        var keycloakClient = A.Fake<IKeycloakAdministrationClient>()
-            .ReturningTrueWhenAssigingClientRoles();
-        var plrClient = A.Fake<IPlrClient>()
-            .ReturningAStatandingsDigest(false);
-        A.CallTo(() => plrClient.FindCpnAsync(collegeCode, licenceNumber, party.Birthdate!.Value)).Returns(expectedCpn);
-        var handler = this.MockDependenciesFor<CommandHandler>(keycloakClient, plrClient);
-
-        var result = await handler.HandleAsync(new Command { PartyId = party.Id, LicenceNumber = licenceNumber, CollegeCode = collegeCode });
-
-        Assert.True(result.IsSuccess);
-        Assert.Equal(expectedCpn, party.Cpn);
-        Assert.NotNull(party.LicenceDeclaration);
-        Assert.Equal(licenceNumber, party.LicenceDeclaration.LicenceNumber);
-        Assert.Equal(collegeCode, party.LicenceDeclaration.CollegeCode);
-
-        A.CallTo(() => keycloakClient.UpdateUserCpn(party.PrimaryUserId, party.Cpn)).MustHaveHappened();
-        keycloakClient.AssertNoRolesAssigned();
+        Assert.Single(this.PublishedEvents.OfType<PlrCpnLookupNotFound>());
+        var notFoundEvent = this.PublishedEvents.OfType<PlrCpnLookupNotFound>().Single();
+        Assert.Equal(party.Id, notFoundEvent.PartyId);
     }
 
     public static IEnumerable<object[]> CollegeCodeTestCases()
