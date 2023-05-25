e22365f5c12bd71f8c0c1c2ff234db3eff5a57c1
Panos <paahndev@gmail.com>
2023-05-25 14:27:27 -0700

    check if party exists and they have BCProvider already

--- backend/webapi/Infrastructure/Services/ScheduledPlrStatusChangeTaskService.cs
+++ backend/webapi/Infrastructure/Services/ScheduledPlrStatusChangeTaskService.cs
@@ -1,5 +1,9 @@
 namespace Pidp.Infrastructure.Services;
 
+using Microsoft.EntityFrameworkCore;
+using Pidp.Data;
+using Pidp.Infrastructure.Auth;
+using Pidp.Infrastructure.HttpClients.BCProvider;
 using Pidp.Infrastructure.HttpClients.Plr;
 
 public class ScheduledPlrStatusChangeTaskService : IScheduledPlrStatusChangeTaskService
@@ -9,12 +13,19 @@ public class ScheduledPlrStatusChangeTaskService : IScheduledPlrStatusChangeTask
     private readonly CancellationTokenSource cts = new();
     private readonly TimeSpan interval;
     private readonly IPlrClient plrClient;
+    private readonly PidpDbContext context;
+    private readonly IBCProviderClient bCProviderClient;
 
-    public ScheduledPlrStatusChangeTaskService(IPlrClient plrClient)
+    public ScheduledPlrStatusChangeTaskService(
+        IPlrClient plrClient,
+        PidpDbContext context,
+        IBCProviderClient bCProviderClient)
     {
         this.interval = TimeSpan.FromSeconds(10);
         this.timer = new PeriodicTimer(this.interval);
         this.plrClient = plrClient;
+        this.context = context;
+        this.bCProviderClient = bCProviderClient;
     }
 
     public void Start()
@@ -36,6 +47,20 @@ public class ScheduledPlrStatusChangeTaskService : IScheduledPlrStatusChangeTask
                 {
                     foreach (var status in statusChange)
                     {
+                        var party = await this.context.Parties
+                            .Where(party => party.Cpn == status.Cpn)
+                            .Select(party => new
+                            {
+                                HasBCProviderCredential = party.Credentials.Any(credential => credential.IdentityProvider == IdentityProviders.BCProvider),
+                            })
+                            .SingleAsync();
+
+                        if (!party.HasBCProviderCredential)
+                        {
+                            return;
+                        }
+
+                        var plrStanding = await this.plrClient.GetStandingsDigestAsync(status.Cpn);
                         // perform business rules
 #if DEBUG
                         Console.WriteLine($"cpn {status.Cpn}, status change from {status.OldStatusCode} to {status.NewStatusCode} - StatusId {status.Id}");
