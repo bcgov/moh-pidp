fda19927f8f8caa0332ef9b1a1b55a35efac73a4
James <jameshollinger.ee@gmail.com>
2023-06-29 12:10:28 -0700

    reorder

--- backend/webapi/Infrastructure/HttpClients/BCProvider/BCProviderApiDefinitions.cs
+++ backend/webapi/Infrastructure/HttpClients/BCProvider/BCProviderApiDefinitions.cs
@@ -11,10 +11,9 @@ public class NewUserRepresentation
     public bool IsMoa { get; set; }
     public bool IsRnp { get; set; }
     public string PidpEmail { get; set; } = string.Empty;
+    public DateTimeOffset UaaDate { get; set; }
 
     public string Password { get; set; } = string.Empty;
-
-    public DateTimeOffset UaaDate { get; set; }
 }
 
 /// <summary>
