e9f006f5b418a6f237be6b7c478bb6cbdb7a71ff
Panos <paahndev@gmail.com>
2023-07-05 13:38:08 -0700

    type change

--- backend/webapi/Features/Credentials/CredentialsController.cs
+++ backend/webapi/Features/Credentials/CredentialsController.cs
@@ -35,7 +35,7 @@ public class CredentialsController : PidpControllerBase
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<string?>> CreateBCProviderCredential([FromServices] ICommandHandler<BCProviderCreate.Command, IDomainResult<string?>> handler,
+    public async Task<ActionResult<string>> CreateBCProviderCredential([FromServices] ICommandHandler<BCProviderCreate.Command, IDomainResult<string>> handler,
                                                                 [FromHybrid] BCProviderCreate.Command command)
         => await this.AuthorizePartyBeforeHandleAsync(command.PartyId, handler, command)
             .ToActionResultOfT();
