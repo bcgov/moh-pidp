69bb33de3fcb33757ca8e64a21247e223659a2da
Panos <paahndev@gmail.com>
2023-07-07 13:52:39 -0700

    scroll to top

--- workspace/apps/pidp/src/app/features/faq/pages/mfa-setup/mfa-setup.page.ts
+++ workspace/apps/pidp/src/app/features/faq/pages/mfa-setup/mfa-setup.page.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import { UtilsService } from '@app/core/services/utils.service';
 
 @Component({
   selector: 'app-mfa-setup',
   templateUrl: './mfa-setup.page.html',
   styleUrls: ['./mfa-setup.page.scss'],
 })
-export class MfaSetupPage {
-  public constructor(private route: ActivatedRoute, private router: Router) {}
+export class MfaSetupPage implements OnInit {
+  public constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private utilsService: UtilsService
+  ) {}
 
   public onBack(): void {
     this.navigateToRoot();
+  }
+
+  public ngOnInit(): void {
+    this.utilsService.scrollTop();
   }
 
   private navigateToRoot(): void {
