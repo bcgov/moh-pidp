45dd9491353734e24c843336ffec0352e9517f5d
Marc UHART <marcuhart@gmail.com>
2023-07-25 15:43:23 -0700

    Mobile view fixed and animation

--- workspace/apps/pidp/src/app/features/portal/components/expansion-panel/expansion-panel.component.ts
+++ workspace/apps/pidp/src/app/features/portal/components/expansion-panel/expansion-panel.component.ts
@@ -1,9 +1,29 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-expansion-panel',
   templateUrl: './expansion-panel.component.html',
   styleUrls: ['./expansion-panel.component.scss'],
+  animations: [
+    trigger('expandAnimation', [
+      state(
+        'true',
+        style({ height: 'fit-content', opacity: 1, transform: 'translateY(0)' })
+      ),
+      state(
+        'false',
+        style({ height: '0', opacity: 0, transform: 'translateY(-10px)' })
+      ),
+      transition('true <=> false', animate('300ms ease-in')),
+    ]),
+  ],
 })
 export class ExpansionPanelComponent {
   @Input() public expanded: boolean | null;
