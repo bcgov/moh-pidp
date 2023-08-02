7ec1ceac67f9d1a791d532ac6ed4384918ac6d2a
Panos <paahndev@gmail.com>
2023-08-02 15:07:17 -0700

    linting fixes

--- workspace/apps/pidp/src/app/features/portal/components/portal-card/portal-card.component.spec.ts
+++ workspace/apps/pidp/src/app/features/portal/components/portal-card/portal-card.component.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Router } from '@angular/router';
