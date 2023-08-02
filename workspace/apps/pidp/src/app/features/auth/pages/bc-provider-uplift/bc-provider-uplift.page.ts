fff60a9b106f54b970ee83ff3f68d1dc732e2182
Marc UHART <marcuhart@gmail.com>
2023-08-02 16:02:26 -0700

    Add a button to leave the BC Provider uplift page

--- workspace/apps/pidp/src/app/features/auth/pages/bc-provider-uplift/bc-provider-uplift.page.ts
+++ workspace/apps/pidp/src/app/features/auth/pages/bc-provider-uplift/bc-provider-uplift.page.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { CookieService } from 'ngx-cookie-service';
+
+import { APP_CONFIG, AppConfig } from '@app/app.config';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-bc-provider-uplift',
   templateUrl: './bc-provider-uplift.page.html',
   styleUrls: ['./bc-provider-uplift.page.scss'],
 })
-export class BcProviderUpliftPage implements OnInit {
-  constructor() {}
+export class BcProviderUpliftPage {
+  public bcscMobileSetupUrl: string;
+  public logoutRedirectUrl: string;
 
-  ngOnInit(): void {}
+  public constructor(
+    @Inject(APP_CONFIG) private config: AppConfig,
+    private cookieService: CookieService,
+    private authService: AuthService
+  ) {
+    this.bcscMobileSetupUrl = this.config.urls.bcscMobileSetup;
+    this.logoutRedirectUrl = `${this.config.applicationUrl}/`;
+  }
+
+  public onLogin(): void {
+    throw new Error('onLogin function not implemented');
+  }
+
+  public onBack(): void {
+    this.cookieService.delete('bcprovider_aad_userid');
+    this.authService.logout(this.logoutRedirectUrl);
+  }
 }
