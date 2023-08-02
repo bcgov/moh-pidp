102f7da71486e0c2680337d6750aa47bf48d54f0
Marc UHART <marcuhart@gmail.com>
2023-08-02 16:11:01 -0700

    Unit test

--- workspace/apps/pidp/src/app/features/auth/pages/bc-provider-uplift/bc-provider-uplift.page.spec.ts
+++ workspace/apps/pidp/src/app/features/auth/pages/bc-provider-uplift/bc-provider-uplift.page.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { provideAutoSpy } from 'jest-auto-spies';
+import { CookieService } from 'ngx-cookie-service';
+
+import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+
+import { AuthService } from '../../services/auth.service';
 import { BcProviderUpliftPage } from './bc-provider-uplift.page';
 
 describe('BcProviderUpliftPage', () => {
@@ -8,7 +15,16 @@ describe('BcProviderUpliftPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [BcProviderUpliftPage],
+      providers: [
+        provideAutoSpy(CookieService),
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG,
+        },
+        provideAutoSpy(AuthService),
+      ],
     }).compileComponents();
   });
 
