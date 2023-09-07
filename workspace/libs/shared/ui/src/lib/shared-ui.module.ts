488758f75665e8b01a086bb16b9e9dff9d1f448e
Marc UHART <marcuhart@gmail.com>
2023-09-07 09:28:48 -0700

    Nx Upgrade

--- workspace/libs/shared/ui/src/lib/shared-ui.module.ts
+++ workspace/libs/shared/ui/src/lib/shared-ui.module.ts
@@ -1,4 +1,4 @@
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
