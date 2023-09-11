f585723a58dd87d0530f9eb8d65ae4adcd2d933b
Marc Uhart <marcuhart@gmail.com>
2023-09-11 15:05:41 -0700

    Fix angular unit tests (#354)
    * Front end unit test error
    
    * fix console log error

--- workspace/apps/pidp/src/app/features/auth/pages/bc-provider-uplift/bc-provider-uplift.page.spec.ts
+++ workspace/apps/pidp/src/app/features/auth/pages/bc-provider-uplift/bc-provider-uplift.page.spec.ts
@@ -4,32 +4,28 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { provideAutoSpy } from 'jest-auto-spies';
 
 import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+import { SharedModule } from '@app/shared/shared.module';
 
 import { AuthService } from '../../services/auth.service';
 import { BcProviderUpliftPage } from './bc-provider-uplift.page';
 
 describe('BcProviderUpliftPage', () => {
   let component: BcProviderUpliftPage;
-  let fixture: ComponentFixture<BcProviderUpliftPage>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [BcProviderUpliftPage],
       providers: [
+        BcProviderUpliftPage,
         {
           provide: APP_CONFIG,
           useValue: APP_DI_CONFIG,
         },
         provideAutoSpy(AuthService),
       ],
-    }).compileComponents();
-  });
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BcProviderUpliftPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = TestBed.inject(BcProviderUpliftPage);
   });
 
   it('should create', () => {
