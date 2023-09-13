27e8f9879d72382f3de2cb35b15883ba415c564a
Marc UHART <marcuhart@gmail.com>
2023-09-13 12:26:06 -0700

    fixed access-denied.component.spec.ts

--- workspace/libs/shared/ui/src/lib/modules/root-routing/pages/access-denied/access-denied.component.spec.ts
+++ workspace/libs/shared/ui/src/lib/modules/root-routing/pages/access-denied/access-denied.component.spec.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { randTextRange } from '@ngneat/falso';
+import { provideAutoSpy } from 'jest-auto-spies';
 
 import { AccessDeniedComponent } from './access-denied.component';
 
@@ -6,9 +11,29 @@ describe('AccessDeniedComponent', () => {
   let component: AccessDeniedComponent;
   let fixture: ComponentFixture<AccessDeniedComponent>;
 
+  let mockActivatedRoute: { snapshot: any };
+
   beforeEach(async () => {
+    mockActivatedRoute = {
+      snapshot: {
+        data: {
+          title: randTextRange({ min: 1, max: 4 }),
+          routes: {
+            root: '../../',
+          },
+        },
+      },
+    };
+
     await TestBed.configureTestingModule({
       declarations: [AccessDeniedComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+        provideAutoSpy(Router),
+      ],
     }).compileComponents();
   });
 
