76f5de62d3de28a85cb36cc752442f7692b93548
Marc UHART <marcuhart@gmail.com>
2023-09-14 11:10:03 -0700

    Fixed html.component.spec.ts

--- workspace/libs/shared/ui/src/lib/components/dialogs/content/html/html.component.spec.ts
+++ workspace/libs/shared/ui/src/lib/components/dialogs/content/html/html.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { SafePipe } from '../../../../pipes';
 import { HtmlComponent } from './html.component';
 
 describe('HtmlComponent', () => {
@@ -8,13 +9,14 @@ describe('HtmlComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HtmlComponent],
+      declarations: [HtmlComponent, SafePipe],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HtmlComponent);
     component = fixture.componentInstance;
+    component.data = { content: '' };
     fixture.detectChanges();
   });
 
