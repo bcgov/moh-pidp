3b22b82a63838f070acc47111c0837098d40f348
Marc UHART <marcuhart@gmail.com>
2023-09-29 10:17:15 -0700

    ExpansionPanel unit tests

--- workspace/libs/shared/ui/src/lib/components/expansion-panel/expansion-panel.component.spec.ts
+++ workspace/libs/shared/ui/src/lib/components/expansion-panel/expansion-panel.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ExpansionPanelComponent } from './expansion-panel.component';
@@ -18,7 +19,68 @@ describe('ExpansionPanelComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Default state', () => {
+    given('the component as default values', () => {
+      when('the component has been initialized', () => {
+        then('the panel is collapsed', () => {
+          const expansionPanelContent = fixture.debugElement.query(
+            By.css('.expansion-panel-content')
+          );
+          expect(expansionPanelContent.styles.visibility).toBe('hidden');
+          expect(component.expanded).toBeFalsy;
+        });
+      });
+    });
+  });
+
+  describe('Panel should be expanded', () => {
+    given('expanded property is true', () => {
+      component.expanded = true;
+
+      when('the component has been initialized', () => {
+        fixture.detectChanges();
+
+        then('the panel is expanded', () => {
+          const expansionPanelContent = fixture.debugElement.query(
+            By.css('.expansion-panel-content')
+          );
+          expect(expansionPanelContent.styles.visibility).toBe('');
+          expect(component.expanded).toBeTruthy;
+        });
+      });
+    });
+  });
+
+  describe('METHOD: toggle()', () => {
+    given('user wants to expand the panel', () => {
+      when('toggle is invoked', () => {
+        component.toggle();
+        fixture.detectChanges();
+
+        then('the panel is collapsed', () => {
+          const expansionPanelContent = fixture.debugElement.query(
+            By.css('.expansion-panel-content')
+          );
+          expect(expansionPanelContent.styles.visibility).toBe('');
+          expect(component.expanded).toBeTruthy;
+        });
+      });
+    });
+
+    given('user wants to collapse the panel', () => {
+      component.expanded = true;
+      when('toggle is invoked', () => {
+        component.toggle();
+        fixture.detectChanges();
+
+        then('the panel is collapsed', () => {
+          const expansionPanelContent = fixture.debugElement.query(
+            By.css('.expansion-panel-content')
+          );
+          expect(expansionPanelContent.styles.visibility).toBe('hidden');
+          expect(component.expanded).toBeFalsy;
+        });
+      });
+    });
   });
 });
