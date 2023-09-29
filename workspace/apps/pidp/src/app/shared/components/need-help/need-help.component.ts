cace0edb8d758b8144f1da00e4907fd7af9bddda
Marc UHART <marcuhart@gmail.com>
2023-09-29 15:58:18 -0700

    Customizing and using NeedHelpComponent

--- workspace/apps/pidp/src/app/shared/components/need-help/need-help.component.ts
+++ workspace/apps/pidp/src/app/shared/components/need-help/need-help.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 
 import { APP_CONFIG, AppConfig } from '@app/app.config';
 
@@ -8,14 +8,27 @@ import { APP_CONFIG, AppConfig } from '@app/app.config';
   styleUrls: ['./need-help.component.scss'],
 })
 export class NeedHelpComponent {
+  @Input() public customClass: 'need-help-large' | undefined;
+  @Input() public showIcon: boolean;
   public showNeedHelp: boolean;
   public providerIdentitySupport: string;
   public additionalSupportPhone: string;
 
   public constructor(@Inject(APP_CONFIG) private config: AppConfig) {
+    this.showIcon = false;
     this.showNeedHelp = false;
+
     this.providerIdentitySupport = this.config.emails.providerIdentitySupport;
     this.additionalSupportPhone = this.config.phones.additionalSupport;
+  }
+
+  public getClasses(): string[] {
+    const classes = ['need-help'];
+    if (this.customClass) {
+      classes.push(this.customClass);
+      return classes;
+    }
+    return classes;
   }
 
   public onShowNeedHelp(): void {
