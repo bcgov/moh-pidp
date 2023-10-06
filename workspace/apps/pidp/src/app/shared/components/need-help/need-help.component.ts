590fcab6f23b64921145c77b19338e83de5ca08c
Marc UHART <marcuhart@gmail.com>
2023-10-06 11:20:21 -0700

    Using property instead of function to the class

--- workspace/apps/pidp/src/app/shared/components/need-help/need-help.component.ts
+++ workspace/apps/pidp/src/app/shared/components/need-help/need-help.component.ts
@@ -8,11 +8,22 @@ import { APP_CONFIG, AppConfig } from '@app/app.config';
   styleUrls: ['./need-help.component.scss'],
 })
 export class NeedHelpComponent {
-  @Input() public customClass: 'need-help-large' | undefined;
   @Input() public showIcon: boolean;
   public showNeedHelp: boolean;
   public providerIdentitySupport: string;
   public additionalSupportPhone: string;
+
+  public get customClasses(): string[] {
+    return this._customClasses;
+  }
+
+  private _customClasses: string[] = ['need-help'];
+
+  @Input() public set customClass(value: 'need-help-large' | undefined) {
+    if (value && this._customClasses.length === 1) {
+      this._customClasses.push(value);
+    }
+  }
 
   public constructor(@Inject(APP_CONFIG) private config: AppConfig) {
     this.showIcon = false;
@@ -20,15 +31,6 @@ export class NeedHelpComponent {
 
     this.providerIdentitySupport = this.config.emails.providerIdentitySupport;
     this.additionalSupportPhone = this.config.phones.additionalSupport;
-  }
-
-  public getClasses(): string[] {
-    const classes = ['need-help'];
-    if (this.customClass) {
-      classes.push(this.customClass);
-      return classes;
-    }
-    return classes;
   }
 
   public onShowNeedHelp(): void {
