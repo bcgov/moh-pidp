ff47135807cd36fd9f5e9d525f3a0b94086273c7
Panos Hatzinikolaou <45132887+Paahn@users.noreply.github.com>
2023-11-17 11:46:35 -0500

    PIDP-678 College Licence and BCProvider Route guards (#420)
    * create high assurance guard
    
    * guard both bcprovider pages from low assurance credentials
    
    * guard college licence from low assurance credentials
    
    * prevent from visiting declaration page after we find CPN
    
    * tests
    
    * prevent going to bcprovider application if completed
    
    * highAssuranceCredentialGuard unit tests
    
    * bcProviderCompletedResolver unit tests
    
    * collegeLicenceCompletedResolver unit tests
    
    * only allow if route available
    
    * navigate to portal to keep wizard in tact
    
    * test
    
    * prevent user going to bcprovider edit if application not completed
    
    * portal
    
    * bcProvider edit resolver tests
    
    ---------
    
    Co-authored-by: Marc Uhart <marcuhart@gmail.com>

--- workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information-routing.module.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { setDashboardTitleGuard } from '@pidp/presentation';
 
 import { CollegeLicenceInformationPage } from './college-licence-information.page';
+import { highAssuranceCredentialGuard } from '@app/features/auth/guards/high-assurance-credential.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: CollegeLicenceInformationPage,
-    canActivate: [setDashboardTitleGuard],
+    canActivate: [setDashboardTitleGuard, highAssuranceCredentialGuard],
     data: {
       title: 'OneHealthID Service',
       routes: {
