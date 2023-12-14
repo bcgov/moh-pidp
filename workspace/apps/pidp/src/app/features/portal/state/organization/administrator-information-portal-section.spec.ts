99f6799aaae80b8046e61fe89ab3b8b2b9f5ef2f
Marc UHART <marcuhart@gmail.com>
2023-12-14 15:33:13 -0800

    administrator-information-portal-section.spec.ts get hint()

--- workspace/apps/pidp/src/app/features/portal/state/organization/administrator-information-portal-section.spec.ts
+++ workspace/apps/pidp/src/app/features/portal/state/organization/administrator-information-portal-section.spec.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { MockProfileStatus } from '@test/mock-profile-status';
 import { provideAutoSpy } from 'jest-auto-spies';
 
+import { StatusCode } from '../../enums/status-code.enum';
 import { ProfileStatus } from '../../models/profile-status.model';
 import { AdministratorInfoPortalSection } from './administrator-information-portal-section';
 
@@ -32,6 +33,28 @@ describe('AdministratorInfoPortalSection', () => {
           expect(section.description).toBe(
             `Provide your office administrator's contact information.`,
           );
+        });
+      });
+    });
+  });
+
+  describe('PROPERTY: get hint', () => {
+    given('StatusCode is AVAILABLE', () => {
+      mockProfileStatus.status.administratorInfo.statusCode =
+        StatusCode.AVAILABLE;
+      when('the class is instanciated', () => {
+        then('hint should return a text', () => {
+          expect(section.hint).toBe('1 minute to complete');
+        });
+      });
+    });
+
+    given('StatusCode is COMPLETED', () => {
+      mockProfileStatus.status.administratorInfo.statusCode =
+        StatusCode.COMPLETED;
+      when('the class is instanciated', () => {
+        then('hint should return an empty text', () => {
+          expect(section.hint).toBe('');
         });
       });
     });
