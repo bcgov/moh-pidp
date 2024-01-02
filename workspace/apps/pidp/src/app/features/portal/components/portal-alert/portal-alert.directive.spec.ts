f6a7fd8081de02228aacd1e9ab8ccb9c299e8ddc
Panos Hatzinikolaou <paahndev@gmail.com>
2024-01-02 13:14:04 -0800

    unused

--- workspace/apps/pidp/src/app/features/portal/components/portal-alert/portal-alert.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PortalAlertDirective } from './portal-alert.directive';
-
-describe('PortalAlertDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PortalAlertDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- workspace/apps/pidp/src/app/features/portal/components/portal-alert/portal-alert.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[appPortalAlert]',
-  standalone: true,
-})
-export class PortalAlertDirective {
-  public constructor() {}
-}
