0146807d5f2b55d6b7d6ea0d541bc554b947c395
Panos Hatzinikolaou <paahndev@gmail.com>
2024-01-10 16:01:37 -0800

    wizard resolver implmentation

---
+++ workspace/apps/pidp/src/app/features/auth/resolvers/wizard.resolver.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+
+import { wizardResolver } from './wizard.resolver';
+
+describe('wizardResolver', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  // test if demographics is not completed
+
+  // test if collegeCertification is not completed
+
+  // test if uaa is not completed
+});
