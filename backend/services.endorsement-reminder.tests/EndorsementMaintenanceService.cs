8855f8ae28eb400129b52869d513f6f8eee0dad7
James Hollinger <jameshollinger.ee@gmail.com>
2024-01-15 14:28:03 -0800

    add test

--- backend/services.endorsement-reminder.tests/EndorsementMaintenanceService.cs
+++ backend/services.endorsement-reminder.tests/EndorsementMaintenanceService.cs
@@ -14,6 +14,50 @@ using PidpTests.TestingExtensions;
 public class EndorsementMaintenanceServiceTests : InMemoryDbTest
 {
     [Theory]
+    [MemberData(nameof(ExpiryScheduleTestCases))]
+    public async void EndorsementMainenanceService_ExpireOldEndorsementRequestsAsync_ShouldNotSendDuplicateEmails(EndorsementRequestStatus status, Instant statusDate, Instant scheduleDate, bool expectedExpired)
+    {
+        var requestingParty = this.TestDb.HasAParty();
+        requestingParty.Email = "requesting@email.com";
+        var endorsementRequest = this.TestDb.Has(new EndorsementRequest
+        {
+            Token = Guid.NewGuid(),
+            RequestingPartyId = requestingParty.Id,
+            Status = status,
+            StatusDate = statusDate,
+            RecipientEmail = "recipient@email.com"
+        });
+
+        var clockMock = AMock.Clock(scheduleDate);
+        var service = this.MockDependenciesFor<EndorsementMaintenanceService>(clockMock);
+
+        await service.ExpireOldEndorsementRequestsAsync();
+
+        Assert.Equal(expectedExpired ? EndorsementRequestStatus.Expired : status, endorsementRequest.Status);
+        if (expectedExpired)
+        {
+            Assert.Equal(scheduleDate, endorsementRequest.StatusDate);
+        }
+    }
+
+    public static IEnumerable<object[]> ExpiryScheduleTestCases()
+    {
+        // Scheduler runs at 8 AM on a monday
+        var scheduleDate = new ZonedDateTime(new LocalDateTime(2023, 11, 13, 8, 0), DateTimeZoneProviders.Tzdb.GetZoneOrNull("America/Vancouver")!, Offset.FromHours(-8)).ToInstant();
+
+        foreach (var status in TestData.AllValuesOf<EndorsementRequestStatus>())
+        {
+            foreach (var daysOld in Enumerable.Range(0, 32))
+            {
+                // We expect that the request will be marked as expired when the Request is 30 days or more old.
+                var expected = daysOld >= 30 && status is EndorsementRequestStatus.Created or EndorsementRequestStatus.Received or EndorsementRequestStatus.Approved;
+
+                yield return new object[] { status, scheduleDate - Duration.FromHours(1) - Duration.FromDays(daysOld), scheduleDate, expected };
+            }
+        }
+    }
+
+    [Theory]
     [MemberData(nameof(ScheduleTestCases))]
     public async void EndorsementMaintenanceService_SendReminderEmailsAsync_SendsEmailsOnSchedule(EndorsementRequestStatus status, Instant statusDate, Instant scheduleDate, bool emailExpected)
     {
