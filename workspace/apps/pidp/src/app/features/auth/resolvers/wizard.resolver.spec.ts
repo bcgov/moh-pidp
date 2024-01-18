35d62bbfd10e19ae8009b8632dcbebee4c480d2f
Panos Hatzinikolaou <45132887+Paahn@users.noreply.github.com>
2024-01-18 18:01:36 -0500

    Update wizard resolver to use Discovery endpoint (#472)
    * update to use discovery
    
    * tests
    
    ---------
    
    Co-authored-by: Marc Uhart <marcuhart@gmail.com>

--- workspace/apps/pidp/src/app/features/auth/resolvers/wizard.resolver.spec.ts
+++ workspace/apps/pidp/src/app/features/auth/resolvers/wizard.resolver.spec.ts
@@ -9,13 +9,14 @@ import {
 import { Observable, of } from 'rxjs';
 
 import { randNumber } from '@ngneat/falso';
-import { MockProfileStatus } from '@test/mock-profile-status';
 import { Spy, createSpyFromClass, provideAutoSpy } from 'jest-auto-spies';
 
+import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+import {
+  Destination,
+  DiscoveryResource,
+} from '@app/core/party/discovery-resource.service';
 import { PartyService } from '@app/core/party/party.service';
-import { StatusCode } from '@app/features/portal/enums/status-code.enum';
-import { ProfileStatus } from '@app/features/portal/models/profile-status.model';
-import { PortalResource } from '@app/features/portal/portal-resource.service';
 
 import { wizardResolver } from './wizard.resolver';
 
@@ -23,10 +24,10 @@ describe('wizardResolver', () => {
   let activatedRouteSnapshotSpy: Spy<ActivatedRouteSnapshot>;
   let routerStateSnapshotSpy: Spy<RouterStateSnapshot>;
   let partyServiceSpy: Spy<PartyService>;
-  let portalResource: Spy<PortalResource>;
+  let discoveryResource: Spy<DiscoveryResource>;
   let router: Router;
 
-  let mockProfileStatus: ProfileStatus;
+  let mockDestination: Destination;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -37,32 +38,116 @@ describe('wizardResolver', () => {
             gettersToSpyOn: ['partyId'],
           }),
         },
-        provideAutoSpy(PortalResource),
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG,
+        },
+        provideAutoSpy(DiscoveryResource),
         provideAutoSpy(Router),
       ],
     });
 
     partyServiceSpy = TestBed.inject(PartyService) as Spy<PartyService>;
-    portalResource = TestBed.inject(PortalResource) as Spy<PortalResource>;
+    discoveryResource = TestBed.inject(
+      DiscoveryResource,
+    ) as Spy<DiscoveryResource>;
     router = TestBed.inject(Router);
-    mockProfileStatus = MockProfileStatus.get();
   });
 
-  given('partyId does not exists, profileStatus is null', (done) => {
+  given('partyId exists, destination is DEMOGRAPHICS', (done) => {
     const partyId = randNumber({ min: 1 });
     partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
-    portalResource.getProfileStatus.mockReturnValueOnce(of(null));
+    mockDestination = 1;
+    discoveryResource.getDestination.mockReturnValueOnce(of(mockDestination));
 
     when('Resolver is called', () => {
       const result = TestBed.runInInjectionContext(() =>
         wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
       );
 
-      then('should return null', () => {
+      then('should navigate to portal page', () => {
+        if (result instanceof Observable) {
+          result.subscribe(() => {
+            try {
+              expect(router.navigateByUrl).toHaveBeenCalledWith('portal');
+              done();
+            } catch (error: any) {
+              done(error);
+            }
+          });
+        }
+      });
+    });
+  });
+
+  given('partyId exists, destination is USER_ACCESS_AGREEMENT', (done) => {
+    const partyId = randNumber({ min: 1 });
+    partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
+    mockDestination = 2;
+    discoveryResource.getDestination.mockReturnValueOnce(of(mockDestination));
+
+    when('Resolver is called', () => {
+      const result = TestBed.runInInjectionContext(() =>
+        wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
+      );
+
+      then('should navigate to portal page', () => {
+        if (result instanceof Observable) {
+          result.subscribe(() => {
+            try {
+              expect(router.navigateByUrl).toHaveBeenCalledWith('portal');
+              done();
+            } catch (error: any) {
+              done(error);
+            }
+          });
+        }
+      });
+    });
+  });
+
+  given('partyId exists, destination is LICENCE_DECLARATION', (done) => {
+    const partyId = randNumber({ min: 1 });
+    partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
+    mockDestination = 3;
+    discoveryResource.getDestination.mockReturnValueOnce(of(mockDestination));
+
+    when('Resolver is called', () => {
+      const result = TestBed.runInInjectionContext(() =>
+        wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
+      );
+
+      then('should navigate to portal page', () => {
+        if (result instanceof Observable) {
+          result.subscribe(() => {
+            try {
+              expect(router.navigateByUrl).toHaveBeenCalledWith('portal');
+              done();
+            } catch (error: any) {
+              done(error);
+            }
+          });
+        }
+      });
+    });
+  });
+
+  given('partyId exists, destination is PORTAL', (done) => {
+    const partyId = randNumber({ min: 1 });
+    partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
+    mockDestination = 4;
+    discoveryResource.getDestination.mockReturnValueOnce(of(mockDestination));
+
+    when('Resolver is called', () => {
+      const result = TestBed.runInInjectionContext(() =>
+        wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
+      );
+
+      then('should return true and access to the route', () => {
         if (result instanceof Observable) {
           result.subscribe((value) => {
             try {
-              expect(value).toBeNull();
+              expect(value).toBeTruthy();
               done();
             } catch (error: any) {
               done(error);
@@ -72,190 +157,4 @@ describe('wizardResolver', () => {
       });
     });
   });
-
-  given('partyId exists, Demographics StatusCode is AVAILABLE', (done) => {
-    const partyId = randNumber({ min: 1 });
-    partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
-    mockProfileStatus.status.demographics.statusCode = StatusCode.AVAILABLE;
-    portalResource.getProfileStatus.mockReturnValueOnce(of(mockProfileStatus));
-
-    when('Resolver is called', () => {
-      const result = TestBed.runInInjectionContext(() =>
-        wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
-      );
-
-      then('should navigate to portal page', () => {
-        if (result instanceof Observable) {
-          result.subscribe(() => {
-            try {
-              expect(router.navigateByUrl).toHaveBeenCalledWith('portal');
-              done();
-            } catch (error: any) {
-              done(error);
-            }
-          });
-        }
-      });
-    });
-  });
-
-  given(
-    'partyId exists, College Certification StatusCode is AVAILABLE',
-    (done) => {
-      const partyId = randNumber({ min: 1 });
-      partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
-      mockProfileStatus.status.collegeCertification.statusCode =
-        StatusCode.AVAILABLE;
-      portalResource.getProfileStatus.mockReturnValueOnce(
-        of(mockProfileStatus),
-      );
-
-      when('Resolver is called', () => {
-        const result = TestBed.runInInjectionContext(() =>
-          wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
-        );
-
-        then('should navigate to portal page', () => {
-          if (result instanceof Observable) {
-            result.subscribe(() => {
-              try {
-                expect(router.navigateByUrl).toHaveBeenCalledWith('portal');
-                done();
-              } catch (error: any) {
-                done(error);
-              }
-            });
-          }
-        });
-      });
-    },
-  );
-
-  given('partyId exists, College Certification isComplete is FALSE', (done) => {
-    const partyId = randNumber({ min: 1 });
-    partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
-    mockProfileStatus.status.collegeCertification.isComplete = false;
-    portalResource.getProfileStatus.mockReturnValueOnce(of(mockProfileStatus));
-
-    when('Resolver is called', () => {
-      const result = TestBed.runInInjectionContext(() =>
-        wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
-      );
-
-      then('should navigate to portal page', () => {
-        if (result instanceof Observable) {
-          result.subscribe(() => {
-            try {
-              expect(router.navigateByUrl).toHaveBeenCalledWith('portal');
-              done();
-            } catch (error: any) {
-              done(error);
-            }
-          });
-        }
-      });
-    });
-  });
-
-  given(
-    'partyId exists, User Access Agreement StatusCode is AVAILABLE',
-    (done) => {
-      const partyId = randNumber({ min: 1 });
-      partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
-      mockProfileStatus.status.userAccessAgreement.statusCode =
-        StatusCode.AVAILABLE;
-      portalResource.getProfileStatus.mockReturnValueOnce(
-        of(mockProfileStatus),
-      );
-
-      when('Resolver is called', () => {
-        const result = TestBed.runInInjectionContext(() =>
-          wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
-        );
-
-        then('should navigate to portal page', () => {
-          if (result instanceof Observable) {
-            result.subscribe(() => {
-              try {
-                expect(router.navigateByUrl).toHaveBeenCalledWith('portal');
-                done();
-              } catch (error: any) {
-                done(error);
-              }
-            });
-          }
-        });
-      });
-    },
-  );
-
-  given(
-    'partyId exists, user has completed the wizard with a college licence',
-    (done) => {
-      const partyId = randNumber({ min: 1 });
-      partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
-      mockProfileStatus.status.demographics.statusCode = StatusCode.COMPLETED;
-      mockProfileStatus.status.collegeCertification.isComplete = true;
-      mockProfileStatus.status.collegeCertification.hasCpn = true;
-      mockProfileStatus.status.userAccessAgreement.statusCode =
-        StatusCode.COMPLETED;
-      portalResource.getProfileStatus.mockReturnValueOnce(
-        of(mockProfileStatus),
-      );
-
-      when('Resolver is called', () => {
-        const result = TestBed.runInInjectionContext(() =>
-          wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
-        );
-
-        then('should return true and access to the route', () => {
-          if (result instanceof Observable) {
-            result.subscribe((value) => {
-              try {
-                expect(value).toBeTruthy();
-                done();
-              } catch (error: any) {
-                done(error);
-              }
-            });
-          }
-        });
-      });
-    },
-  );
-
-  given(
-    'partyId exists, user has completed the wizard but selected no college licence',
-    (done) => {
-      const partyId = randNumber({ min: 1 });
-      partyServiceSpy.accessorSpies.getters.partyId.mockReturnValue(partyId);
-      mockProfileStatus.status.demographics.statusCode = StatusCode.COMPLETED;
-      mockProfileStatus.status.collegeCertification.isComplete = true;
-      mockProfileStatus.status.collegeCertification.hasCpn = false;
-      mockProfileStatus.status.userAccessAgreement.statusCode =
-        StatusCode.COMPLETED;
-      portalResource.getProfileStatus.mockReturnValueOnce(
-        of(mockProfileStatus),
-      );
-
-      when('Resolver is called', () => {
-        const result = TestBed.runInInjectionContext(() =>
-          wizardResolver(activatedRouteSnapshotSpy, routerStateSnapshotSpy),
-        );
-
-        then('should return true and access to the route', () => {
-          if (result instanceof Observable) {
-            result.subscribe((value) => {
-              try {
-                expect(value).toBeTruthy();
-                done();
-              } catch (error: any) {
-                done(error);
-              }
-            });
-          }
-        });
-      });
-    },
-  );
 });
