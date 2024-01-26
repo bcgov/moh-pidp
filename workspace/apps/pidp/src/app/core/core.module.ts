bc5a15a0d7ee3f79b36df0f66e024f8976ab7cd1
Marc UHART <marcuhart@gmail.com>
2024-01-26 09:17:07 -0800

    core module migration remove useless import

--- workspace/apps/pidp/src/app/core/core.module.ts
+++ workspace/apps/pidp/src/app/core/core.module.ts
@@ -1,10 +1,7 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { httpInterceptorProviders } from '@bcgov/shared/data-access';
-import { NgxProgressBarModule, RootRoutingModule } from '@bcgov/shared/ui';
+import { RootRoutingModule } from '@bcgov/shared/ui';
 import { EnsureModuleLoadedOnceGuard } from '@bcgov/shared/utils';
 
 import { KeycloakModule } from '@app/modules/keycloak/keycloak.module';
@@ -12,14 +9,6 @@ import { LookupModule } from '@app/modules/lookup/lookup.module';
 import { PermissionsModule } from '@app/modules/permissions/permissions.module';
 
 const modules = [
-  // TODO required at root to connect with HttpClientModule
-  NgxProgressBarModule,
-  // TODO only applied to allow for a few core services until moved
-  ReactiveFormsModule,
-  // TODO only applied to allow for a few core services until moved
-  MatDialogModule,
-  // TODO only applied to allow for a few core services until moved
-  MatSnackBarModule,
   LookupModule.forRoot(),
   KeycloakModule,
   PermissionsModule.forRoot(),
