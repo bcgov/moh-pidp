6ccfb08404395b0090264a3fed0804b930d06675
Panos Hatzinikolaou <paahndev@gmail.com>
2024-02-05 13:43:38 -0800

    raise domain event

--- backend/webapi/Models/DomainEvents/PlrCpnLookup.cs
+++ backend/webapi/Models/DomainEvents/PlrCpnLookup.cs
@@ -1,11 +1,10 @@
 namespace Pidp.Models.DomainEvents;
 
-using Pidp.Infrastructure.HttpClients.Keycloak;
 using Pidp.Infrastructure.HttpClients.Plr;
 
-public record PlrCpnLookupFound(int PartyId, IEnumerable<Guid> UserIds, string Cpn, PlrStandingsDigest StandingsDigest, IEnumerable<CollegeLicenceInformation> CollegeLicenceInformation) : IDomainEvent
+public record PlrCpnLookupFound(int PartyId, IEnumerable<Guid> UserIds, string Cpn, PlrStandingsDigest StandingsDigest) : IDomainEvent
 {
-    public PlrCpnLookupFound(int partyId, Guid userId, string cpn, PlrStandingsDigest standingsDigest, IEnumerable<CollegeLicenceInformation> collegeLicenceInformation) : this(partyId, new[] { userId }, cpn, standingsDigest, collegeLicenceInformation) { }
+    public PlrCpnLookupFound(int partyId, Guid userId, string cpn, PlrStandingsDigest standingsDigest) : this(partyId, new[] { userId }, cpn, standingsDigest) { }
 }
 
 public record PlrCpnLookupNotFound(int PartyId) : IDomainEvent { }
--- backend/webapi/Models/Party.cs
+++ backend/webapi/Models/Party.cs
@@ -11,8 +11,6 @@ using Pidp.Extensions;
 using Pidp.Infrastructure.Auth;
 using Pidp.Infrastructure.HttpClients.Plr;
 using Pidp.Models.DomainEvents;
-using Pidp.Infrastructure.HttpClients.Keycloak;
-
 
 [Table(nameof(Party))]
 public class Party : BaseAuditable
@@ -109,25 +107,9 @@ public class Party : BaseAuditable
         }
 
         var standingsDigest = await client.GetStandingsDigestAsync(this.Cpn);
-        // TODO possibly reduce the number calls to PLR?
-        var records = await client.GetRecordsAsync(this.Cpn);
 
-        var collegeLicenceInformationList = new List<CollegeLicenceInformation>();
-
-        foreach (var record in records ?? Enumerable.Empty<PlrRecord>())
-        {
-            var collegeLicenceInformation = new CollegeLicenceInformation
-            {
-                ProviderRoleType = record.ProviderRoleType,
-                StatusCode = record.StatusCode,
-                StatusReasonCode = record.StatusReasonCode,
-                MspId = record.MspId,
-                CollegeId = record.CollegeId
-            };
-            collegeLicenceInformationList.Add(collegeLicenceInformation);
-        }
-
-        this.DomainEvents.Add(new PlrCpnLookupFound(this.Id, this.PrimaryUserId, this.Cpn, standingsDigest, collegeLicenceInformationList));
+        this.DomainEvents.Add(new PlrCpnLookupFound(this.Id, this.PrimaryUserId, this.Cpn, standingsDigest));
+        this.DomainEvents.Add(new CollegeLicenceUpdated(this.Id, this.PrimaryUserId));
 
         if (standingsDigest.HasGoodStanding)
         {
