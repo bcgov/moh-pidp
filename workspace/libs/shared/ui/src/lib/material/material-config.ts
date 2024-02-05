ea5e33ce90a1a2148cecfc0105d354255f82531e
Marc UHART <marcuhart@gmail.com>
2024-02-05 15:55:37 -0800

    maxWidth for dialog

--- workspace/libs/shared/ui/src/lib/material/material-config.ts
+++ workspace/libs/shared/ui/src/lib/material/material-config.ts
@@ -52,7 +52,7 @@ export function provideMaterialConfig(): Provider[] {
     {
       provide: MAT_DIALOG_DEFAULT_OPTIONS,
       useValue: {
-        width: '500px',
+        maxWidth: '500px',
         hasBackdrop: true,
       },
     },
