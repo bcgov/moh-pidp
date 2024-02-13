dfba3758a21fb5f47327a76a4400acbbecc0467e
Panos Hatzinikolaou <paahndev@gmail.com>
2024-02-12 16:17:22 -0800

    migration

---
+++ backend/webapi/Data/Migrations/20240213000408_AddOPIDToPartyTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pidp.Data.Migrations
+{
+    public partial class AddOPIDToPartyTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OpId",
+                table: "Party",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Party_OpId",
+                table: "Party",
+                column: "OpId",
+                unique: true,
+                filter: "\"OpId\" != ''");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Party_OpId",
+                table: "Party");
+
+            migrationBuilder.DropColumn(
+                name: "OpId",
+                table: "Party");
+        }
+    }
+}
