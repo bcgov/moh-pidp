2438f88f8e04570900cdb9e031cbc17b0714adcd
James Hollinger <jameshollinger.ee@gmail.com>
2024-02-19 03:45:28 -0800

    generate username

--- backend/webapi/Models/Credential.cs
+++ backend/webapi/Models/Credential.cs
@@ -27,4 +27,22 @@ public class Credential : BaseAuditable
 
     [Projectable]
     public bool IsBcServicesCard => this.IdentityProvider == IdentityProviders.BCServicesCard;
+
+    /// <summary>
+    /// The expected Ministry of Health Keycloak username for this user. The schema is {IdentityProviderIdentifier}@{IdentityProvider}.
+    /// Most of our Credentials come to us from Keycloak and so the username is saved as-is in the column IdpId.
+    /// However, we create BC Provider Credentials directly; so the User Principal Name is saved to the database without the suffix.
+    /// There are also two inconsistencies with how the MOH Keycloak handles BCP usernames:
+    /// 1. The username suffix is @bcp rather than @bcprovider_aad.
+    /// 2. This suffix is only added in Test and Production; there is no suffix at all for BCP Users in the Dev environment.
+    /// </summary>
+    public string? GenerateMohKeycloakUsername()
+    {
+        if (this.IdentityProvider == IdentityProviders.BCProvider && !PidpConfiguration.IsDevelopment())
+        {
+            return this.IdpId + "@bcp";
+        }
+
+        return this.IdpId;
+    }
 }
