862140f48bf8e94821fd3b10c023689824c0d954
Panos Hatzinikolaou <paahndev@gmail.com>
2024-03-07 18:34:27 -0800

    update title

--- workspace/apps/pidp/src/app/features/profile/pages/work-and-role-information/work-and-role-information-routing.routes.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/work-and-role-information/work-and-role-information-routing.routes.ts
@@ -14,6 +14,11 @@ export const routes: Routes = [
       routes: {
         root: '../../',
       },
+      setDashboardTitleGuard: {
+        titleText: 'Welcome to OneHealthID Service',
+        titleDescriptionText:
+          'Complete your profile to gain access to the systems you are eligible for',
+      },
     },
   },
 ];
