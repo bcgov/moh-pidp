25e6596cc1bce0a002c00cc5a327a0958c0b4e50
Panos Hatzinikolaou <paahndev@gmail.com>
2024-04-05 12:12:34 -0700

    cleanup

--- backend/tools.do-work/DoWorkService.cs
+++ backend/tools.do-work/DoWorkService.cs
@@ -1,44 +1,12 @@
 namespace DoWork;
 
-using Microsoft.EntityFrameworkCore;
-
-using Pidp.Data;
-using Pidp.Infrastructure.Auth;
-using Pidp.Infrastructure.HttpClients.Keycloak;
-
 public class DoWorkService : IDoWorkService
 {
-    private readonly IKeycloakAdministrationClient keycloakClient;
-    private readonly PidpDbContext context;
-
-    public DoWorkService(IKeycloakAdministrationClient keycloakClient, PidpDbContext context)
-    {
-        this.keycloakClient = keycloakClient;
-        this.context = context;
-    }
+    public DoWorkService() { }
 
     public async Task DoWorkAsync()
     {
         Console.WriteLine(">>>>Start!");
-        var userIds = await this.context.Parties
-            .Where(party => party.Credentials.Any(credential => credential.IdentityProvider == IdentityProviders.BCServicesCard))
-            .SelectMany(party => party.Credentials.Select(credential => credential.UserId))
-            .ToListAsync();
-
-        foreach (var userId in userIds)
-        {
-            try
-            {
-                if (null == await this.keycloakClient.GetUser(userId))
-                {
-                    Console.WriteLine($"User with ID {userId} not found in Keycloak.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error finding user with ID {userId}: {ex.Message}");
-            }
-        }
 
         Console.WriteLine("<<<<End!");
     }
