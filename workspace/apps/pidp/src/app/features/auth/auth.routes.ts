2d3158b57810fb0e3962acc5a286985ca2abd1a0
Panos Hatzinikolaou <paahndev@gmail.com>
2024-04-30 15:56:37 -0700

    register route

--- workspace/apps/pidp/src/app/features/auth/auth.routes.ts
+++ workspace/apps/pidp/src/app/features/auth/auth.routes.ts
@@ -5,6 +5,7 @@ export class AuthRoutes {
   public static AUTO_LOGIN = 'auto-login';
   public static BC_PROVIDER_UPLIFT = 'bc-provider-uplift';
   public static PORTAL_LOGIN = 'login';
+  public static LINK_EXISTING_ACCOUNT = 'link-existing-account';
 
   /**
    * @description
