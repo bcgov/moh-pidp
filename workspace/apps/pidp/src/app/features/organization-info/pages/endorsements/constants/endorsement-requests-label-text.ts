399e80ddaf70830cafc301dab736d8153d09d4a7
Panos Hatzinikolaou <paahndev@gmail.com>
2024-05-02 12:13:51 -0700

    add constant descriptions

--- workspace/apps/pidp/src/app/features/organization-info/pages/endorsements/constants/endorsement-requests-label-text.ts
+++ workspace/apps/pidp/src/app/features/organization-info/pages/endorsements/constants/endorsement-requests-label-text.ts
@@ -1,5 +1,9 @@
 import { EndorsementRequestStatus } from '../enums/endorsement-request-status.enum';
 
+/**
+ * @description
+ * Mapping of endorsement request statuses to their respective labels.
+ */
 export const endorsementRequestsLabelText: Partial<
   Record<EndorsementRequestStatus, string>
 > = {
@@ -7,6 +11,12 @@ export const endorsementRequestsLabelText: Partial<
   [EndorsementRequestStatus.DECLINED]: 'Declined',
   [EndorsementRequestStatus.APPROVED]: 'In progress',
 };
+
+/**
+ * @description
+ * List of endorsement request statuses that
+ * have a red colored label to denote they are no longer active.
+ */
 export const endorsementRequestsRedStatus: EndorsementRequestStatus[] = [
   EndorsementRequestStatus.CANCELLED,
   EndorsementRequestStatus.DECLINED,
