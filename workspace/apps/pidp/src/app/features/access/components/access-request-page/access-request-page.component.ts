d21507c9bfb8efef6d5a4174995c270aaba33747
Aditya M <150046114+AdityaMantripragada@users.noreply.github.com>
2024-05-06 09:46:55 -0700

    back-to-top

--- workspace/apps/pidp/src/app/features/access/components/access-request-page/access-request-page.component.ts
+++ workspace/apps/pidp/src/app/features/access/components/access-request-page/access-request-page.component.ts
@@ -1,17 +1,12 @@
 import { NgClass, NgIf } from '@angular/common';
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject, HostListener } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
 
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faPlus, faThumbsUp, faFile,faFileLines,faArrowUp } from '@fortawesome/free-solid-svg-icons';
-
+import { faPlus, faThumbsUp, faFileLines, faArrowUp, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { InjectViewportCssClassDirective } from '@bcgov/shared/ui';
 
 import { APP_CONFIG, AppConfig } from '@app/app.config';
-import { AuthService } from '@app/features/auth/services/auth.service';
-
-//import { IPortalSection } from '../../state/portal-section.model';
 
 @Component({
   selector: 'app-access-request-page',
@@ -31,21 +26,24 @@ export class AccessRequestPageComponent {
   public faPlus = faPlus;
   public faFileLines = faFileLines;
   public logoutRedirectUrl: string;
-
   public faArrowUp = faArrowUp;
-
+  public faAngleRight = faAngleRight;
+  showBackToTopButton: Boolean = false;
 
   public constructor(
     @Inject(APP_CONFIG) private config: AppConfig,
-    private router: Router,
-    private authService: AuthService,
   ) {
     this.logoutRedirectUrl = `${this.config.applicationUrl}/`;
   }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const scrollThreshold = 200;
+    this.showBackToTopButton = scrollPosition > scrollThreshold;
+  }
+
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
 }
-
- 
