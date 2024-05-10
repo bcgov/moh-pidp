c22a8b041fae1dbe5e3bc8eeb6ba8771a70fc15d
James Hollinger <jameshollinger.ee@gmail.com>
2024-05-09 18:02:43 -0700

    update Primary UserId

--- backend/webapi/Models/Party.cs
+++ backend/webapi/Models/Party.cs
@@ -76,13 +76,14 @@ public class Party : BaseAuditable
     public string DisplayFullName => $"{this.DisplayFirstName} {this.DisplayLastName}";
 
     /// <summary>
-    /// The "primary" Credential of a Party is the a) only, or b) BC Services Card Credential.
-    /// As of now, the only Parties that have two Credentials are first BC Services Card and then later recieve a BC Provider Credential.
+    /// The "primary" Credential of a Party is the a) BC Services Card Credential or b) the first non-BC Services Card Credential.
     /// </summary>
     [Projectable]
     public Guid PrimaryUserId => this.Credentials
-        .Single(credential => credential.IdentityProvider != IdentityProviders.BCProvider)
-        .UserId;
+        .OrderByDescending(credential => credential.IdentityProvider == IdentityProviders.BCServicesCard)
+        .ThenBy(credential => credential.Created)
+        .Select(credential => credential.UserId)
+        .First();
 
     /// <summary>
     /// Uses the Party's Licence Declaration to search PLR for records.
