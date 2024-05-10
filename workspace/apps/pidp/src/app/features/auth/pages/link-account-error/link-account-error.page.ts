a2b396ef0be5023234d60563419c8293933f665a
Panos Hatzinikolaou <paahndev@gmail.com>
2024-05-10 14:18:53 -0700

    show dynamic content on error page

--- workspace/apps/pidp/src/app/features/auth/pages/link-account-error/link-account-error.page.ts
+++ workspace/apps/pidp/src/app/features/auth/pages/link-account-error/link-account-error.page.ts
@@ -1,9 +1,15 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import {
+  NgOptimizedImage,
+  NgSwitch,
+  NgSwitchCase,
+  NgSwitchDefault,
+  NgTemplateOutlet,
+} from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import {
   AnchorDirective,
@@ -12,6 +18,7 @@ import {
 } from '@bcgov/shared/ui';
 
 import { APP_CONFIG, AppConfig } from '@app/app.config';
+import { DiscoveryStatus } from '@app/core/party/discovery-resource.service';
 import { ShellRoutes } from '@app/features/shell/shell.routes';
 
 import { AuthService } from '../../services/auth.service';
@@ -29,19 +36,30 @@ import { AuthService } from '../../services/auth.service';
     MatIconModule,
     MatTooltipModule,
     NgOptimizedImage,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgTemplateOutlet,
   ],
 })
-export class LinkAccountErrorPage {
+export class LinkAccountErrorPage implements OnInit {
   public logoutRedirectUrl: string;
   public providerIdentitySupport: string;
+  public activeLayout = '';
+  public status = '';
+  public DiscoveryStatus = DiscoveryStatus;
 
   public constructor(
     @Inject(APP_CONFIG) private config: AppConfig,
+    private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService,
   ) {
     this.logoutRedirectUrl = `${this.config.applicationUrl}/`;
     this.providerIdentitySupport = this.config.emails.providerIdentitySupport;
+    this.route.queryParams.subscribe((params) => {
+      this.status = params['status'];
+    });
   }
 
   public onBack(): void {
@@ -50,5 +68,15 @@ export class LinkAccountErrorPage {
 
   public onLogout(): void {
     this.authService.logout(this.logoutRedirectUrl);
+  }
+
+  public ngOnInit(): void {
+    this.setLayout(this.status);
+  }
+
+  private setLayout(activeLayout: string): void {
+    if (this.activeLayout !== activeLayout) {
+      this.activeLayout = activeLayout;
+    }
   }
 }
