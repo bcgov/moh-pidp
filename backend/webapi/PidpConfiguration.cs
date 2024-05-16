e1f6f92e591c6f3b267b3476d23d4704da52c5ef
Shourouni <317198@NTTDATA.COM>
2024-05-16 09:53:13 -0700

    make environmentname public

--- backend/webapi/PidpConfiguration.cs
+++ backend/webapi/PidpConfiguration.cs
@@ -6,7 +6,7 @@ public class PidpConfiguration
 {
     public static bool IsProduction() => EnvironmentName == Environments.Production;
     public static bool IsDevelopment() => EnvironmentName == Environments.Development;
-    private static readonly string? EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    public static readonly string? EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
     public string ApplicationUrl { get; set; } = string.Empty;
 
