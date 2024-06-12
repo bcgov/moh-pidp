6f975a7e05ac1434d23e9a1001396ff6ad30f312
Panos Hatzinikolaou <paahndev@gmail.com>
2024-06-12 15:24:29 -0700

    implement icon

--- workspace/apps/pidp/src/app/features/access/components/access-request-card/access-request-card.component.ts
+++ workspace/apps/pidp/src/app/features/access/components/access-request-card/access-request-card.component.ts
@@ -1,14 +1,24 @@
 import { Component, Input } from '@angular/core';
 
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faFileLines } from '@fortawesome/free-solid-svg-icons';
+
+import { InjectViewportCssClassDirective } from '@bcgov/shared/ui';
+
 @Component({
   selector: 'app-access-request-card',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent, InjectViewportCssClassDirective],
   templateUrl: './access-request-card.component.html',
   styleUrl: './access-request-card.component.scss',
 })
 export class AccessRequestCardComponent {
-  @Input() public icon: string = '';
+  @Input() public icon: IconProp;
   @Input() public heading: string = '';
   @Input() public description: string = '';
+  public faFileLines = faFileLines;
+  public constructor() {
+    this.icon = faFileLines;
+  }
 }
