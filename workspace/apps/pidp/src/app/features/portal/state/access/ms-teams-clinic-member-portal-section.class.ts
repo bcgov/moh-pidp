bed33eadd6de38ebca7e78aef7f529662ef6c6e4
Aditya M <150046114+AdityaMantripragada@users.noreply.github.com>
2024-06-18 15:17:57 -0700

    PIDP-896 New Access Requests page (#524)
    * new component added access request
    
    * temp routing
    
    * back-to-top
    
    * removed unused html
    
    * Revert "removed unused html"
    
    This reverts commit 3e41bfec964829f0f80a7e3e0db15cf92f5fc4fe.
    
    * mobile view and serach field modified
    
    * css fixes
    
    * temp header,footer added,css fix
    
    * dynamic card,backtop css fix
    
    * sonar issue and css fix
    
    * routing added
    
    * added spec.ts data
    
    * removed duplicate code
    
    * duplicate code removed
    
    * lint fixes
    
    * return type added
    
    * css fixes
    
    * removed unused import
    
    * added constant,removed comments
    
    * sonar fix
    
    * card css added
    
    * icons added
    
    * card css fix
    
    * remove route
    
    * rename and move into pages
    
    * navigate using angular router
    
    * linting
    
    * reusable BreadcrumbButton directive
    
    * new access request card component
    
    * move to access/ route
    
    * rewrite routing
    
    * use directive
    
    * rename directive
    
    * tests
    
    * Access state refactor - first pass
    
    * update inputs
    
    * added icon input
    
    * implement icon
    
    * make access requests page work
    
    * add action for access card
    
    * update description
    
    * make count work
    
    * implement search bar
    
    * temporary button for access to systems
    
    * search when enter key is hit
    
    * replace lorum ipsum
    
    * display green icon when completed
    
    * fix test
    
    * border
    
    * wordsmithing
    
    * move seachbar to center
    
    * sub pixel precision
    
    * bug
    
    * remove
    
    * todo
    
    * using fa icon isntead
    
    * sort alphabetically
    
    * whitespace
    
    * bugfix
    
    ---------
    
    Co-authored-by: Panos Hatzinikolaou <paahndev@gmail.com>
    Co-authored-by: Panos Hatzinikolaou <45132887+Paahn@users.noreply.github.com>
    Co-authored-by: James Hollinger <jameshollinger.ee@gmail.com>

--- workspace/apps/pidp/src/app/features/portal/state/access/ms-teams-clinic-member-portal-section.class.ts
+++ workspace/apps/pidp/src/app/features/portal/state/access/ms-teams-clinic-member-portal-section.class.ts
@@ -2,6 +2,9 @@ import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faPeopleGroup, faUserCheck } from '@fortawesome/free-solid-svg-icons';
+
 import { AlertType } from '@bcgov/shared/ui';
 
 import { AccessRoutes } from '@app/features/access/access.routes';
@@ -17,6 +20,8 @@ export class MsTeamsClinicMemberPortalSection implements IPortalSection {
   public readonly key: PortalSectionKey;
   public heading: string;
   public description: string;
+  public faPeopleGroup = faPeopleGroup;
+  public faUserCheck = faUserCheck;
 
   public constructor(
     private profileStatus: ProfileStatus,
@@ -55,6 +60,11 @@ export class MsTeamsClinicMemberPortalSection implements IPortalSection {
       : statusCode === StatusCode.COMPLETED
         ? 'Completed'
         : 'Incomplete';
+  }
+
+  public get icon(): IconProp {
+    const statusCode = this.getStatusCode();
+    return statusCode === StatusCode.COMPLETED ? faUserCheck : faPeopleGroup;
   }
 
   public performAction(): Observable<void> | void {
