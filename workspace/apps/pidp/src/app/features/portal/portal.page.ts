3d131753f3afb58194dc75607c8b146c8d74d0bf
Panos Hatzinikolaou <paahndev@gmail.com>
2024-06-19 15:23:51 -0700

    navigate to Endorsements

--- workspace/apps/pidp/src/app/features/portal/portal.page.ts
+++ workspace/apps/pidp/src/app/features/portal/portal.page.ts
@@ -1,19 +1,18 @@
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { NgIf, NgOptimizedImage } from '@angular/common';
-import { Component, Inject, HostListener } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faArrowUp, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import {
-  InjectViewportCssClassDirective
-} from '@bcgov/shared/ui';
+import { faArrowRight, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+import { InjectViewportCssClassDirective } from '@bcgov/shared/ui';
 
 import { APP_CONFIG, AppConfig } from '@app/app.config';
-import { Router } from '@angular/router';
-import { ProfileRoutes } from '../profile/profile.routes';
+
 import { AccessRoutes } from '../access/access.routes';
-
-
+import { OrganizationInfoRoutes } from '../organization-info/organization-info.routes';
+import { ProfileRoutes } from '../profile/profile.routes';
 
 @Component({
   selector: 'app-portal',
@@ -30,24 +29,26 @@ import { AccessRoutes } from '../access/access.routes';
     FaIconComponent,
     InjectViewportCssClassDirective,
     NgIf,
-    NgOptimizedImage
+    NgOptimizedImage,
   ],
 })
 export class PortalPage {
-
   public faArrowRight = faArrowRight;
   public faArrowUp = faArrowUp;
   public showBackToTopButton: boolean = false;
 
   public constructor(
     @Inject(APP_CONFIG) private config: AppConfig,
-    private router: Router
-  ) {
-  }
+    private router: Router,
+  ) {}
 
   @HostListener('window:scroll', [])
   public onWindowScroll(): void {
-    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const scrollPosition =
+      window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
     const scrollThreshold = 200;
     this.showBackToTopButton = scrollPosition > scrollThreshold;
   }
@@ -55,10 +56,20 @@ export class PortalPage {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
   public navigateToAccountLinkingPage(): void {
-    this.router.navigateByUrl(ProfileRoutes.routePath(ProfileRoutes.ACCOUNT_LINKING));
+    this.router.navigateByUrl(
+      ProfileRoutes.routePath(ProfileRoutes.ACCOUNT_LINKING),
+    );
   }
 
   public navigateToAccessPage(): void {
-    this.router.navigateByUrl(AccessRoutes.routePath(AccessRoutes.ACCESS_REQUESTS));
+    this.router.navigateByUrl(
+      AccessRoutes.routePath(AccessRoutes.ACCESS_REQUESTS),
+    );
+  }
+
+  public navigateToEndorsements(): void {
+    this.router.navigateByUrl(
+      OrganizationInfoRoutes.routePath(OrganizationInfoRoutes.ENDORSEMENTS),
+    );
   }
 }
