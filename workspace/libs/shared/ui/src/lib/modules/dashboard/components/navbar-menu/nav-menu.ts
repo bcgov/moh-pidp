6c6b0e7159306ef898e8b32076e6a57e40411fc1
vinod kumar kakarla <vinodkakarla564@gmail.com>
2024-06-26 14:52:01 -0700

    merge reverted

--- workspace/libs/shared/ui/src/lib/modules/dashboard/components/navbar-menu/nav-menu.ts
+++ workspace/libs/shared/ui/src/lib/modules/dashboard/components/navbar-menu/nav-menu.ts
@@ -147,7 +147,7 @@ export class NavMenuComponent implements OnChanges {
     this.logout.emit();
   }
   public onMenuItemClicked(): void {
-    if (this.sidenavMode === 'over') {
+    if (this.showMiniMenuButton && this.sidenavMode === 'over') {
       this.sidenav.close();
     }
   }
