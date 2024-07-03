9924ff1f71041a89a3cee5d73bb8dd576fef6c1f
Panos Hatzinikolaou <paahndev@gmail.com>
2024-07-03 15:33:28 -0700

    fix tests

--- workspace/apps/pidp/src/app/features/portal/portal.page.spec.ts
+++ workspace/apps/pidp/src/app/features/portal/portal.page.spec.ts
@@ -1,28 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { provideAutoSpy } from 'jest-auto-spies';
+
 import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+
 import { PortalPage } from './portal.page';
 
 describe('PortalPage', () => {
   let component: PortalPage;
-  let fixture: ComponentFixture<PortalPage>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PortalPage, NoopAnimationsModule],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule],
       providers: [
+        PortalPage,
         {
           provide: APP_CONFIG,
           useValue: APP_DI_CONFIG,
         },
+        provideAutoSpy(HttpClient),
       ],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PortalPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    });
+    component = TestBed.inject(PortalPage);
   });
 
   it('should create', () => {
