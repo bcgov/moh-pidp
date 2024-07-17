e4e3546f265bf7304091296bbb7fe0777a409710
Aditya M <150046114+AdityaMantripragada@users.noreply.github.com>
2024-07-17 07:46:40 -0700

    sonar fix and test case added

--- workspace/apps/pidp/src/app/features/profile/pages/user-access-agreement/components/user-access-agreement-document/user-access-agreement-document.component.spec.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/user-access-agreement/components/user-access-agreement-document/user-access-agreement-document.component.spec.ts
@@ -1,13 +1,31 @@
 import { TestBed } from '@angular/core/testing';
+import { randTextRange } from '@ngneat/falso';
 
 import { UserAccessAgreementDocumentComponent } from './user-access-agreement-document.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('UserAccessAgreementDocumentComponent', () => {
   let component: UserAccessAgreementDocumentComponent;
+  let mockActivatedRoute: { snapshot: any };
 
   beforeEach(() => {
+    mockActivatedRoute = {
+      snapshot: {
+        data: {
+          title: randTextRange({ min: 1, max: 4 }),
+          routes: {
+            root: '../../',
+          },
+        },
+      },
+    };
     TestBed.configureTestingModule({
-      providers: [UserAccessAgreementDocumentComponent],
+      providers: [UserAccessAgreementDocumentComponent,
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+      ],
     });
 
     component = TestBed.inject(UserAccessAgreementDocumentComponent);
