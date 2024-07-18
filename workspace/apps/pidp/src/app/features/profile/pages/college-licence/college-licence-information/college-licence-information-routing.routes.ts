43694b7c1d4ea5bf67627c766971c0a8de8add7a
James Hollinger <jameshollinger.ee@gmail.com>
2024-07-17 18:29:33 -0700

    Unlock FE

--- workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information-routing.routes.ts
+++ workspace/apps/pidp/src/app/features/profile/pages/college-licence/college-licence-information/college-licence-information-routing.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { highAssuranceCredentialGuard } from '@app/features/auth/guards/high-assurance-credential.guard';
 import { collegeLicenceInfoResolver } from '@app/features/auth/resolvers/college-licence-info.resolver';
 
 import { CollegeLicenceInformationPage } from './college-licence-information.page';
@@ -12,7 +11,6 @@ export const routes: Routes = [
     resolve: {
       hasCpn: collegeLicenceInfoResolver,
     },
-    canActivate: [highAssuranceCredentialGuard],
     data: {
       title: 'OneHealthID Service',
       routes: {
