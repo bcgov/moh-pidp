3cdcd9aee705f08725c12f85dec0fc67350c9f7f
vinod kumar kakarla <vinodkakarla564@gmail.com>
2024-07-23 18:28:28 -0700

    Temporary fix reverted.

--- backend/webapi/Models/FhirMessage.cs
+++ backend/webapi/Models/FhirMessage.cs
@@ -7,9 +7,6 @@ using System.ComponentModel.DataAnnotations;
 [Table(nameof(FhirMessage))]
 public class FhirMessage : IDisposable
 {
-    public FhirMessage()
-    {
-    }
     [Key]
     public int Id { get; set; }
     public JsonDocument? MessageBody { get; set; }
