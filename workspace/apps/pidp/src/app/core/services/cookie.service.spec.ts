3776cd95b59548b2920e9c96e10c903040655ba1
Aditya M <150046114+AdityaMantripragada@users.noreply.github.com>
2024-08-19 10:04:13 -0700

    test case updated

--- workspace/apps/pidp/src/app/core/services/cookie.service.spec.ts
+++ workspace/apps/pidp/src/app/core/services/cookie.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
-import * as CryptoJS from 'crypto-js';
 
 import { CryptoService } from './crypto.service';
+import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+import { createSpyFromClass } from 'jest-auto-spies';
 
 describe('CryptoService', () => {
   let service: CryptoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CryptoJS],
-      providers: [CryptoService],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG,
+        },
+        {
+          provide: CryptoService,
+          useValue: createSpyFromClass(CryptoService),
+        },
+      ]
     });
 
     service = TestBed.inject(CryptoService);
