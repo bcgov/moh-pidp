a5b9a77b2f9c7561c91defa892646d525a930849
James Hollinger <jameshollinger.ee@gmail.com>
2024-09-16 13:18:38 -0700

    restore

--- backend/tools.do-work/DoWorkService.cs
+++ backend/tools.do-work/DoWorkService.cs
@@ -3,10 +3,7 @@ namespace DoWork;
 using Microsoft.EntityFrameworkCore;
 
 using Pidp.Data;
-using Pidp.Infrastructure.Auth;
 using Pidp.Infrastructure.HttpClients.Keycloak;
-using Pidp.Models;
-using Pidp.Models.Lookups;
 
 public class DoWorkService(IKeycloakAdministrationClient keycloakClient, PidpDbContext context) : IDoWorkService
 {
@@ -15,45 +12,10 @@ public class DoWorkService(IKeycloakAdministrationClient keycloakClient, PidpDbC
 
     public async Task DoWorkAsync()
     {
-        // await this.PopulateJobs();
-
-        var batch = await this.context.Jobs
-            .Where(job => !job.Complete)
-            .Take(10)
-            .ToListAsync();
-
-        foreach (var job in batch)
-        {
-            try
-            {
-                job.Complete = await this.keycloakClient.AssignAccessRoles(job.UserId, MohKeycloakEnrolment.SAEforms);
-
-                if (!job.Complete)
-                {
-                    Console.WriteLine($"Error assigning access roles to user {job.UserId}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception assigning access roles to user {job.UserId}: {ex.Message}");
-            }
-        }
-
-        await this.context.SaveChangesAsync();
-    }
-
-    private async Task PopulateJobs()
-    {
-        var jobs = await this.context.Credentials
-            .Where(credential => credential.IdentityProvider == IdentityProviders.BCProvider
-                && credential.Party!.AccessRequests.Any(request => request.AccessTypeCode == AccessTypeCode.SAEforms))
-            .Select(credential => new Job
-            {
-                UserId = credential.UserId
-            })
-            .ToListAsync();
-
-        this.context.Jobs.AddRange(jobs);
-        await this.context.SaveChangesAsync();
+        var userId = await this.context.Parties
+            .Where(party => party.Id == 1001)
+            .Select(party => party.Credentials.First().UserId)
+            .SingleOrDefaultAsync();
+        await this.keycloakClient.GetUser(userId);
     }
 }
