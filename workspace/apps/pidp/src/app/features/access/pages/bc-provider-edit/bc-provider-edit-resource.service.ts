65708c13459b86731085d65d710d8f5412199d2d
Panos Hatzinikolaou <paahndev@gmail.com>
2024-09-17 11:41:55 -0700

    HTTP DELETE

--- workspace/apps/pidp/src/app/features/access/pages/bc-provider-edit/bc-provider-edit-resource.service.ts
+++ workspace/apps/pidp/src/app/features/access/pages/bc-provider-edit/bc-provider-edit-resource.service.ts
@@ -35,7 +35,7 @@ export class BcProviderEditResource {
 
   public resetMfa(partyId: number): NoContent {
     const url = `parties/${partyId}/credentials/bc-provider/reset-mfa`;
-    return this.apiResource.post<NoContent>(url, {}).pipe(
+    return this.apiResource.delete<NoContent>(url).pipe(
       NoContentResponse,
       catchError((error: HttpErrorResponse) => {
         return throwError(() => error);
