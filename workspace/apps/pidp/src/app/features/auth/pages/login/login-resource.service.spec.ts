63079e70b97b4907320e0315d6cbc72b5617462e
vinod kumar kakarla <vinodkakarla564@gmail.com>
2024-09-26 14:32:35 -0700

    login resource unit test added.

---
+++ workspace/apps/pidp/src/app/features/auth/pages/login/login-resource.service.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { provideAutoSpy } from 'jest-auto-spies';
+
+import { ApiHttpClient } from '@app/core/resources/api-http-client.service';
+import { LoginResource } from './login-resource.service';
+describe('LoginResource', () => {
+  let service: LoginResource;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideAutoSpy(ApiHttpClient),
+      ],
+    });
+
+    service = TestBed.inject(LoginResource);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
