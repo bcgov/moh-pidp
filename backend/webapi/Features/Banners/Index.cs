7b00773e3ab315008e98e80f6bc5f54e30961956
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2024-09-27 18:27:04 -0700

    new lines

--- backend/webapi/Features/Banners/Index.cs
+++ backend/webapi/Features/Banners/Index.cs
@@ -3,6 +3,7 @@ namespace Pidp.Features.Banners;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using NodaTime;
+
 using Pidp.Data;
 using Pidp.Models;
 
@@ -24,15 +25,12 @@ public class Index
         public BannerStatus Status { get; set; }
         public string Header { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
-
     }
-
 
     public class QueryHandler(PidpDbContext context, IClock clock) : IQueryHandler<Query, List<Model>>
     {
         private readonly PidpDbContext context = context;
         private readonly IClock clock = clock;
-
 
         public async Task<List<Model>> HandleAsync(Query query)
         {
@@ -40,7 +38,8 @@ public class Index
                 .Where(banner => banner.Component == query.Component &&
                     banner.StartTime < this.clock.GetCurrentInstant() &&
                     banner.EndTime > this.clock.GetCurrentInstant())
-                .Select(banner => new Model {
+                .Select(banner => new Model
+                {
                     Component = banner.Component,
                     Status = banner.Status,
                     Header = banner.Header,
