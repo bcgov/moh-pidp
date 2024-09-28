83eab5463ed1b993b7db8f548e7e9f1ec182d91c
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2024-09-27 18:23:43 -0700

    new lines

--- backend/webapi/Models/Banner.cs
+++ backend/webapi/Models/Banner.cs
@@ -1,8 +1,8 @@
 namespace Pidp.Models;
 
+using NodaTime;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using NodaTime;
 
 public enum BannerStatus
 {
@@ -11,15 +11,22 @@ public enum BannerStatus
     Error = 3,
     Success = 4
 }
+
 [Table(nameof(Banner))]
 public class Banner : BaseAuditable
 {
     [Key]
     public int Id { get; set; }
+
     public string Component { get; set; } = string.Empty;
+
     public BannerStatus Status { get; set; }
+
     public string Header { get; set; } = string.Empty;
+
     public string Body { get; set; } = string.Empty;
+
     public Instant StartTime { get; set; }
+
     public Instant EndTime { get; set; }
 }
