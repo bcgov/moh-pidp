bfa27a7b84af04280138a2bedeb5bcef0c4d889f
Panos Hatzinikolaou <paahndev@gmail.com>
2024-10-17 13:10:09 -0700

    snowplow collector as a service

--- workspace/apps/pidp/src/assets/snowplow.js
+++ workspace/apps/pidp/src/assets/snowplow.js
@@ -20,19 +20,3 @@
   'https://www2.gov.bc.ca/StaticWebResources/static/sp/sp-2-14-0.js',
   'snowplow',
 );
-var collector = 'spm.apps.gov.bc.ca';
-window.snowplow('newTracker', 'rt', collector, {
-  appId: 'Snowplow_standalone',
-  cookieLifetime: 86400 * 548,
-  platform: 'web',
-  post: true,
-  forceSecureTracker: true,
-  contexts: {
-    webPage: true,
-    performanceTiming: true,
-  },
-});
-window.snowplow('enableActivityTracking', 30, 30); // Ping every 30 seconds after 30 seconds
-window.snowplow('enableLinkClickTracking');
-window.snowplow('trackPageView');
-// <!-- Snowplow stops plowing -->
