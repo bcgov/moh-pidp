78dede807e7fc9e4dfae1210b0ebe36bc16cf5ce
Aditya M <150046114+AdityaMantripragada@users.noreply.github.com>
2024-10-25 11:56:56 -0700

    lint fix2 import added back

--- workspace/apps/pidp/src/app/features/access/pages/bc-provider-edit/bc-provider-edit.page.spec.ts
+++ workspace/apps/pidp/src/app/features/access/pages/bc-provider-edit/bc-provider-edit.page.spec.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { randNumber, randTextRange } from '@ngneat/falso';
 import { Spy, createSpyFromClass, provideAutoSpy } from 'jest-auto-spies';
-
+import { NavigationService } from '@pidp/presentation';
 import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
 import { PartyService } from '@app/core/party/party.service';
 import { FormUtilsService } from '@app/core/services/form-utils.service';
@@ -53,6 +53,7 @@ describe('BcProviderEditPage', () => {
         },
         provideAutoSpy(HttpClient),
         provideAutoSpy(FormUtilsService),
+        provideAutoSpy(NavigationService),
         provideAutoSpy(Router),
       ],
     });
