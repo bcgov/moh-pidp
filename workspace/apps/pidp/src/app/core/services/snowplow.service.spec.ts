b3e7d2376b368d8b80037e195366a56a5866ea2a
Panos Hatzinikolaou <paahndev@gmail.com>
2024-11-07 11:50:15 -0800

    tests

--- workspace/apps/pidp/src/app/core/services/snowplow.service.spec.ts
+++ workspace/apps/pidp/src/app/core/services/snowplow.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
+import { provideAutoSpy } from 'jest-auto-spies';
+
+import { APP_CONFIG, APP_DI_CONFIG } from '@app/app.config';
+
 import { SnowplowService } from './snowplow.service';
+import { WindowRefService } from './window-ref.service';
 
 describe('SnowplowService', () => {
   let service: SnowplowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        SnowplowService,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG,
+        },
+        provideAutoSpy(WindowRefService),
+      ],
+    });
     service = TestBed.inject(SnowplowService);
   });
 
