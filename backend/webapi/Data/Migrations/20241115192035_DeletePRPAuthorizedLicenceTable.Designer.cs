6e9fd6303bf8c1334d06226c6e14c3cad799063b
vinod kumar kakarla <vinodkakarla564@gmail.com>
2024-11-21 16:26:28 -0800

    Pdip 1104 remove individual user from prp (#633)
    Pdip 1104 remove individual user from prp.

--- backend/webapi/Data/Migrations/20241115192035_DeletePRPAuthorizedLicenceTable.Designer.cs
+++ backend/webapi/Data/Migrations/20241115192035_DeletePRPAuthorizedLicenceTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Pidp.Data;
 namespace Pidp.Data.Migrations
 {
     [DbContext(typeof(PidpDbContext))]
-    partial class PidpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241115192035_DeletePRPAuthorizedLicenceTable")]
+    partial class DeletePRPAuthorizedLicenceTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
