918de83924ef11f22b41b537e3ce3cd69e510f0d
vinod kumar kakarla <vinodkakarla564@gmail.com>
2024-12-11 17:41:14 -0800

    unit tests fixed.

--- workspace/apps/pidp/src/app/shared/components/feedback-form/feedback-form.dialog.spec.ts
+++ workspace/apps/pidp/src/app/shared/components/feedback-form/feedback-form.dialog.spec.ts
@@ -1,17 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeedbackFormDialogComponent } from './feedback-form.dialog';
+import { FormUtilsService } from '@app/core/services/form-utils.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { provideAutoSpy, Spy } from 'jest-auto-spies';
+import { FeedbackSendComponent } from '../success-dialog/components/feedback-send.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('FeedbackFormDialogComponent', () => {
   let component: FeedbackFormDialogComponent;
   let fixture: ComponentFixture<FeedbackFormDialogComponent>;
+  let formUtilsServiceSpy: Spy<FormUtilsService>;
+  let matDialogRefSpy: Spy<MatDialogRef<FeedbackSendComponent>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FeedbackFormDialogComponent],
+      imports: [FeedbackFormDialogComponent, BrowserAnimationsModule],
+      providers: [
+        provideAutoSpy(FormUtilsService),
+        provideAutoSpy(MatDialogRef)
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(FeedbackFormDialogComponent);
     component = fixture.componentInstance;
+    formUtilsServiceSpy = TestBed.inject<any>(FormUtilsService);
+    matDialogRefSpy = TestBed.inject<any>(MatDialogRef);
     fixture.detectChanges();
   });
 
