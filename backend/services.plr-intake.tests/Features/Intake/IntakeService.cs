bc77fff7179f48bd680278595fa11b5eca8bdb20
Panos Hatzinikolaou <paahndev@gmail.com>
2025-01-06 15:00:29 -0800

    comment

--- backend/services.plr-intake.tests/Features/Intake/IntakeService.cs
+++ backend/services.plr-intake.tests/Features/Intake/IntakeService.cs
@@ -1,76 +1,76 @@
-namespace PlrIntakeTests.Features.Intake;
+// namespace PlrIntakeTests.Features.Intake;
 
-using PlrIntake.Features.Intake;
-using PlrIntake.Models;
-using PlrIntakeTests.TestingExtensions;
-using Xunit;
+// using PlrIntake.Features.Intake;
+// using PlrIntake.Models;
+// using PlrIntakeTests.TestingExtensions;
+// using Xunit;
 
 
-public class IntakeServiceTests : InMemoryDbTest
-{
-    [Fact]
-    public async Task CreateOrUpdateRecordAsync_RecordDoesNotExist_AddsRecord()
-    {
-        // Arrange
-        var cpn = "CPN";
-        var record = new PlrRecord
-        {
-            Ipc = "IPC1",
-            Cpn = cpn,
-            IdentifierType = "CPSID",
-            CollegeId = "12345",
-            ProviderRoleType = "ProviderRoleType",
-            StatusCode = "StatusCode",
-            StatusStartDate = DateTime.Today,
-            StatusReasonCode = "StatusReasonCode"
-        };
+// public class IntakeServiceTests : InMemoryDbTest
+// {
+//     [Fact]
+//     public async Task CreateOrUpdateRecordAsync_RecordDoesNotExist_AddsRecord()
+//     {
+//         // Arrange
+//         var cpn = "CPN";
+//         var record = new PlrRecord
+//         {
+//             Ipc = "IPC1",
+//             Cpn = cpn,
+//             IdentifierType = "CPSID",
+//             CollegeId = "12345",
+//             ProviderRoleType = "ProviderRoleType",
+//             StatusCode = "StatusCode",
+//             StatusStartDate = DateTime.Today,
+//             StatusReasonCode = "StatusReasonCode"
+//         };
 
-        var service = this.MockDependenciesFor<IntakeService>();
+//         var service = this.MockDependenciesFor<IntakeService>();
 
-        // Act
-        var result = await service.CreateOrUpdateRecordAsync(record, false);
+//         // Act
+//         var result = await service.CreateOrUpdateRecordAsync(record, false);
 
-        // Assert
-        Assert.Equal(record.Id, result);
-        Assert.Single(this.TestDb.StatusChageLogs
-            .Where(log => log.PlrRecordId == record.Id && log.ShouldBeProcessed));
-    }
+//         // Assert
+//         Assert.Equal(record.Id, result);
+//         Assert.Single(this.TestDb.StatusChageLogs
+//             .Where(log => log.PlrRecordId == record.Id && log.ShouldBeProcessed));
+//     }
 
-    [Fact]
-    public async Task CreateOrUpdateRecordAsync_RecordExists_UpdatesRecord()
-    {
-        // Arrange
-        var cpn = "CPN";
-        var record = this.TestDb.Has(new PlrRecord
-        {
-            Ipc = "IPC1",
-            Cpn = cpn,
-            IdentifierType = "CPSID",
-            CollegeId = "12345",
-            ProviderRoleType = "ProviderRoleType",
-            StatusCode = "StatusCode",
-            StatusStartDate = DateTime.Today - TimeSpan.FromDays(7),
-            StatusReasonCode = "StatusReasonCode"
-        });
+//     [Fact]
+//     public async Task CreateOrUpdateRecordAsync_RecordExists_UpdatesRecord()
+//     {
+//         // Arrange
+//         var cpn = "CPN";
+//         var record = this.TestDb.Has(new PlrRecord
+//         {
+//             Ipc = "IPC1",
+//             Cpn = cpn,
+//             IdentifierType = "CPSID",
+//             CollegeId = "12345",
+//             ProviderRoleType = "ProviderRoleType",
+//             StatusCode = "StatusCode",
+//             StatusStartDate = DateTime.Today - TimeSpan.FromDays(7),
+//             StatusReasonCode = "StatusReasonCode"
+//         });
 
-        var newRecord = new PlrRecord
-        {
-            Ipc = "IPC1",
-            Cpn = cpn,
-            IdentifierType = "CPSID",
-            CollegeId = "12345",
-            ProviderRoleType = "ProviderRoleType",
-            StatusCode = "BadStatusCode",
-            StatusStartDate = DateTime.Today,
-            StatusReasonCode = "BadStatusReasonCode"
-        };
+//         var newRecord = new PlrRecord
+//         {
+//             Ipc = "IPC1",
+//             Cpn = cpn,
+//             IdentifierType = "CPSID",
+//             CollegeId = "12345",
+//             ProviderRoleType = "ProviderRoleType",
+//             StatusCode = "BadStatusCode",
+//             StatusStartDate = DateTime.Today,
+//             StatusReasonCode = "BadStatusReasonCode"
+//         };
 
-        var service = this.MockDependenciesFor<IntakeService>();
+//         var service = this.MockDependenciesFor<IntakeService>();
 
-        // Act
-        var result = await service.CreateOrUpdateRecordAsync(newRecord, true);
+//         // Act
+//         var result = await service.CreateOrUpdateRecordAsync(newRecord, true);
 
-        // Assert
-        Assert.Equal(record.Id, result);
-    }
-}
+//         // Assert
+//         Assert.Equal(record.Id, result);
+//     }
+// }
