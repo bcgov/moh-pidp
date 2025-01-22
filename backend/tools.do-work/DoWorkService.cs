40849a7be15f81e070e5e120ac286a91786154a8
Panos Hatzinikolaou <paahndev@gmail.com>
2025-01-22 15:53:25 -0800

    first pass

--- backend/tools.do-work/DoWorkService.cs
+++ backend/tools.do-work/DoWorkService.cs
@@ -1,21 +1,56 @@
 namespace DoWork;
 
 using Microsoft.EntityFrameworkCore;
-
+using Pidp;
 using Pidp.Data;
-using Pidp.Infrastructure.HttpClients.Keycloak;
+using Pidp.Infrastructure.Auth;
+using Pidp.Infrastructure.HttpClients.BCProvider;
+using Pidp.Infrastructure.HttpClients.Plr;
 
-public class DoWorkService(IKeycloakAdministrationClient keycloakClient, PidpDbContext context) : IDoWorkService
+public class DoWorkService(
+    IBCProviderClient bcProviderClient,
+    PidpDbContext context,
+    IPlrClient plrClient,
+    PidpConfiguration config) : IDoWorkService
 {
-    private readonly IKeycloakAdministrationClient keycloakClient = keycloakClient;
+    private readonly IBCProviderClient bcProviderClient = bcProviderClient;
     private readonly PidpDbContext context = context;
+    private readonly IPlrClient plrClient = plrClient;
+    private readonly string clientId = config.BCProviderClient.ClientId;
+
 
     public async Task DoWorkAsync()
     {
-        var userId = await this.context.Parties
-            .Where(party => party.Id == 1001)
-            .Select(party => party.Credentials.First().UserId)
-            .SingleOrDefaultAsync();
-        await this.keycloakClient.GetUser(userId);
+        Console.WriteLine(">>>>Start!");
+        var parties = await this.context.Parties
+            .Include(party => party.Credentials)
+            .Take(1000)
+            .Where(party => party.Cpn != null
+                && party.Credentials.Any(credential => credential.IdentityProvider == IdentityProviders.BCProvider))
+            .ToListAsync();
+        Console.WriteLine(">>>>Parties loaded!");
+        Console.WriteLine(">>>>Parties count: " + parties.Count);
+        Console.WriteLine(">>>>Updating BCProvider attributes...");
+
+        foreach (var party in parties)
+        {
+            var userPrincipalName = party.Credentials
+                .Where(credential => credential.IdentityProvider == IdentityProviders.BCProvider)
+                .Select(credential => credential.IdpId)
+                .FirstOrDefault();
+
+            if (userPrincipalName == null)
+            {
+                return;
+            }
+
+            var plrStanding = await this.plrClient.GetStandingsDigestAsync(party.Cpn);
+
+            var attributes = new BCProviderAttributes(this.clientId)
+                .SetIsPharm(plrStanding.With(IdentifierType.Pharmacist).HasGoodStanding);
+
+            await this.bcProviderClient.UpdateAttributes(userPrincipalName, attributes.AsAdditionalData());
+        }
+        Console.WriteLine(">>>>End!");
     }
 }
