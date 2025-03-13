07118ae2ab6b1d907e1ed975927a37f6ba1ad3fc
Panos Hatzinikolaou <paahndev@gmail.com>
2025-03-12 19:13:33 -0700

    x2

--- backend/webapi/Startup.cs
+++ backend/webapi/Startup.cs
@@ -97,7 +97,6 @@ public class Startup(IConfiguration configuration)
     {
         var config = new PidpConfiguration();
         this.Configuration.Bind(config);
-        config.Keycloak.RealmUrl = "https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications";
 
         services.AddSingleton(config);
 
