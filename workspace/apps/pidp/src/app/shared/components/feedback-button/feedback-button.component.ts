4d8565a7dc78e3d58f4d9784610ae8c295dc3525
Panos Hatzinikolaou <paahndev@gmail.com>
2025-03-14 15:12:21 -0700

    formatting

--- workspace/apps/pidp/src/app/shared/components/feedback-button/feedback-button.component.ts
+++ workspace/apps/pidp/src/app/shared/components/feedback-button/feedback-button.component.ts
@@ -1,33 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FeedbackFormDialogComponent } from '../feedback-form/feedback-form.dialog';
-import { MatDialog } from '@angular/material/dialog';
+
 import { PidpViewport, ViewportService } from '@bcgov/shared/ui';
 
+import { FeedbackFormDialogComponent } from '../feedback-form/feedback-form.dialog';
+
 @Component({
-    selector: 'app-feedback-button',
-    templateUrl: './feedback-button.component.html',
-    styleUrl: './feedback-button.component.scss',
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule
-    ]
+  selector: 'app-feedback-button',
+  templateUrl: './feedback-button.component.html',
+  styleUrl: './feedback-button.component.scss',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
-export class FeedbackButtonComponent
+export class FeedbackButtonComponent {
   public dialogWidth = '360px';
   public isMobileView = false;
   public buttonClass = 'feedback-button-box';
 
   public constructor(
     public dialog: MatDialog,
-    viewportService: ViewportService
-  )
-  {
+    viewportService: ViewportService,
+  ) {
     viewportService.viewportBroadcast$.subscribe((viewport) =>
       this.onViewportChange(viewport),
     );
@@ -36,23 +37,21 @@ export class FeedbackButtonComponent
   public openDialog(): void {
     this.dialog.open(FeedbackFormDialogComponent, {
       position: {
-        right: "0px"
+        right: '0px',
       },
       width: this.dialogWidth,
     });
-
   }
 
   private onViewportChange(viewport: PidpViewport): void {
     if (viewport === PidpViewport.xsmall) {
-      this.dialogWidth = "315px";
+      this.dialogWidth = '315px';
       this.isMobileView = true;
-      this.buttonClass = "feedback-button-box-mobile";
+      this.buttonClass = 'feedback-button-box-mobile';
     } else {
-      this.dialogWidth ="360px";
-      this.isMobileView =false;
-      this.buttonClass = "feedback-button-box";
+      this.dialogWidth = '360px';
+      this.isMobileView = false;
+      this.buttonClass = 'feedback-button-box';
     }
   }
-
 }
