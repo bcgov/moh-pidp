51a486b977f8b1047aa7a29ff60a6c84aa7eb232
Panos Hatzinikolaou <paahndev@gmail.com>
2025-03-18 16:35:26 -0700

    add b2b subcard to routing

--- workspace/apps/pidp/src/app/features/access/pages/immsbc/immsbc-routing.routes.ts
+++ workspace/apps/pidp/src/app/features/access/pages/immsbc/immsbc-routing.routes.ts
@@ -1,15 +1,27 @@
 import { Routes } from '@angular/router';
 
+import { AccessRoutes } from '../../access.routes';
+import { B2bInformationPageComponent } from '../b2b-information-page/b2b-information-page.component';
 import { ImmsBCPage } from './immsbc.page';
 import { immsBCResolver } from './immsbc.resolver';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ImmsBCPage,
+    component: B2bInformationPageComponent,
     resolve: {
       immsBCStatusCode: immsBCResolver,
     },
+    data: {
+      title: 'Bring your own Account',
+      routes: {
+        root: '../../',
+      },
+    },
+  },
+  {
+    path: AccessRoutes.IMMSBC,
+    component: ImmsBCPage,
     data: {
       title: 'Immunizations BC and OneHealthID',
       routes: {
