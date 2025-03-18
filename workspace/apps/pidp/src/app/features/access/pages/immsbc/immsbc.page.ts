ea6bd8dee769de4efc55ce51bc76aaf1455e36c4
Panos Hatzinikolaou <paahndev@gmail.com>
2025-03-18 15:16:13 -0700

    init page

---
+++ workspace/apps/pidp/src/app/features/access/pages/immsbc/immsbc.page.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-immsbc',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './immsbc.page.html',
+  styleUrl: './immsbc.page.scss',
+})
+export class ImmsbcPage {}
