c067b90f92132a1c038565942810a206630cb764
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-03-24 14:41:18 -0700

    MessageData threshold and time to live configuration added.

--- backend/webapi/Infrastructure/Queue/MassTransitSetup.cs
+++ backend/webapi/Infrastructure/Queue/MassTransitSetup.cs
@@ -29,6 +29,12 @@ public static class MassTransitSetup
                 cfg.UseMessageRetry(r => r.Interval(2, TimeSpan.FromSeconds(5)));
                 cfg.UseInMemoryOutbox(context);
 
+                MessageDataDefaults.TimeToLive = TimeSpan.FromMinutes(20);
+                MessageDataDefaults.Threshold = 8192;
+                MessageDataDefaults.AlwaysWriteToRepository = false;
+
+                // Going with in-memory message data repository for now.
+                // TO-Do: Implement FileSystemMessageDataRepository with proper cleanup of data
                 IMessageDataRepository messageDataRepository = new InMemoryMessageDataRepository();
                 cfg.UseMessageData(messageDataRepository);
 
