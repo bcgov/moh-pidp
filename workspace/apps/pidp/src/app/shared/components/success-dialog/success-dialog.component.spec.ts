b88e4647c82cd145ac926a876a8e8ae0d5b97c5c
Rajesh Shrestha <rajesh.shrestha@nttdata.com>
2025-04-23 18:28:48 -0400

    OHID-293 - Liniting issue fixed in test

--- workspace/apps/pidp/src/app/shared/components/success-dialog/success-dialog.component.spec.ts
+++ workspace/apps/pidp/src/app/shared/components/success-dialog/success-dialog.component.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ViewContainerRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
