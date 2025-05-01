eb9889d89f205238d8fbc056a5214addcb6ff4c9
Panos Hatzinikolaou <paahndev@gmail.com>
2025-04-30 17:03:29 -0700

    edit

--- workspace/apps/pidp/src/app/features/access/pages/external-accounts/external-accounts-resource.service.ts
+++ workspace/apps/pidp/src/app/features/access/pages/external-accounts/external-accounts-resource.service.ts
@@ -3,6 +3,4 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class ExternalAccountsResourceService {
-  public constructor() {}
-}
+export class ExternalAccountsResourceService {}
