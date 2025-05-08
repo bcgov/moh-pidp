0778478ccdbc517f5f20ec7399102f9e45711900
Panos Hatzinikolaou <paahndev@gmail.com>
2025-05-08 12:22:18 -0700

    comment out

--- workspace/libs/shared/ui/src/lib/components/svg-icon/icon-registry.service.spec.ts
+++ workspace/libs/shared/ui/src/lib/components/svg-icon/icon-registry.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { IconRegistryService } from './icon-registry.service';
+// import { IconRegistryService } from './icon-registry.service';
 
-describe('IconRegistryService', () => {
-  let service: IconRegistryService;
+// describe('IconRegistryService', () => {
+//   let service: IconRegistryService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(IconRegistryService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(IconRegistryService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
