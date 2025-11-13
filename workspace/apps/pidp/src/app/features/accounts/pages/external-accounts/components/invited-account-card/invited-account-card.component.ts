639ea0c8e2423b422dc9b6c6f12f5399ea233f6a
Panos Hatzinikolaou <paahndev@gmail.com>
2025-11-13 09:20:20 -0800

    new comp

---
+++ workspace/apps/pidp/src/app/features/accounts/pages/external-accounts/components/invited-account-card/invited-account-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invited-account-card',
+  imports: [],
+  templateUrl: './invited-account-card.component.html',
+  styleUrl: './invited-account-card.component.scss'
+})
+export class InvitedAccountCardComponent {
+
+}
