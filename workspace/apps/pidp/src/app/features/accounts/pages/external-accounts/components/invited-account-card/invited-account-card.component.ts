620358893c9f2b9fc44ad52be16272886c7f22a6
Panos Hatzinikolaou <paahndev@gmail.com>
2025-11-14 14:04:07 -0800

    css directive

--- workspace/apps/pidp/src/app/features/accounts/pages/external-accounts/components/invited-account-card/invited-account-card.component.ts
+++ workspace/apps/pidp/src/app/features/accounts/pages/external-accounts/components/invited-account-card/invited-account-card.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
+import { InjectViewportCssClassDirective } from '@bcgov/shared/ui';
+
 @Component({
   selector: 'app-invited-account-card',
-  imports: [],
+  imports: [InjectViewportCssClassDirective],
   templateUrl: './invited-account-card.component.html',
-  styleUrl: './invited-account-card.component.scss'
+  styleUrl: './invited-account-card.component.scss',
 })
-export class InvitedAccountCardComponent {
-
-}
+export class InvitedAccountCardComponent {}
