8fbe4c8a5a54e498296c6e66102cf99aaa78e06a
Panos Hatzinikolaou <paahndev@gmail.com>
2025-12-10 14:16:11 -0800

    url

---
+++ workspace/apps/pidp/src/app/features/access/pages/immsbc/immsbc-constants.ts
@@ -0,0 +1,5 @@
+export const immsbcUATWebsite =
+  'https://bcpublicdigitalhealth--bcpubfundv.sandbox.my.salesforce.com/';
+
+export const immsbcCPWebsite =
+  'https://bcphsa--bcpubfundv.sandbox.my.site.com/bchvaxportal/s/';
