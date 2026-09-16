1b70a360d73d18a8f5a3f964418d515861312049
dwatson-cgi <dane.watson@cgi.com>
2026-09-16 00:46:45 -0700

    environment vars overrides

--- backend/tools.do-work/Program.cs
+++ backend/tools.do-work/Program.cs
@@ -56,7 +56,8 @@ static PidpConfiguration InitializeConfiguration(IServiceCollection services)
 {
     var builder = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
 
     var configuration = builder.Build();
 
